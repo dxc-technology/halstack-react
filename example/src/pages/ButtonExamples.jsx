483c9a9a6c416499df55f0b04de5988c2b004e0a
Suarez Gonzalez <jsuarezgonz2@dxc.com>
2019-06-12 16:48:16 +0200

    Slider component and new button styles

--- example/src/pages/ButtonExamples.jsx
+++ example/src/pages/ButtonExamples.jsx
@@ -11,20 +11,99 @@ function ButtonExamples() {
 
   return (
     <div>
+    <div>
       <h1>Button Component</h1>
-      <h2>Basic Example</h2>
+      <h2>Basic Example - Light Theme</h2>
       <DxcButton
         label="Basic"
         mode="basic"
         disabled={false}
         theme="light"
         disableRipple={false}
-        iconPosition="after"
+        iconPosition="before"
+        iconSrc="/images/thumbs-up.svg"
+        onClick={handleOnClick}
+      />
+      <DxcButton
+        label="Outlined"
+        mode="outlined"
+        disabled={false}
+        theme="light"
+        disableRipple={false}
+        iconPosition="before"
+        iconSrc="/images/run_icon.png"
+        onClick={handleOnClick}
+      />
+      <DxcButton
+        label="Flat"
+        mode="flat"
+        disabled={false}
+        theme="light"
+        disableRipple={false}
+        iconPosition="before"
+        iconSrc="/images/run_icon.png"
+        onClick={handleOnClick}
+      />
+      <DxcButton
+        label="Raised"
+        mode="raised"
+        disabled={false}
+        theme="light"
+        disableRipple={false}
+        iconPosition="before"
         iconSrc="/images/run_icon.png"
         onClick={handleOnClick}
       />
     </div>
+    <DarkDiv>
+      <h1>Button Component</h1>
+      <h2>Basic Example - Dark Theme</h2>
+      <DxcButton
+        label="Basic"
+        mode="basic"
+        disabled={false}
+        theme="dark"
+        disableRipple={true}
+        iconPosition="after"
+        iconSrc="/images/run_icon.png"
+        onClick={handleOnClick}
+      />
+      <DxcButton
+        label="Outlined"
+        mode="outlined"
+        disabled={false}
+        theme="dark"
+        disableRipple={false}
+        iconPosition="after"
+        iconSrc="/images/run_icon.png"
+        onClick={handleOnClick}
+      />
+      <DxcButton
+        label="Flat"
+        mode="flat"
+        disabled={false}
+        theme="dark"
+        disableRipple={false}
+        iconPosition="after"
+        iconSrc="/images/run_icon.png"
+        onClick={handleOnClick}
+      />
+      <DxcButton
+        label="Raised"
+        mode="raised"
+        disabled={false}
+        theme="dark"
+        disableRipple={false}
+        iconPosition="after"
+        iconSrc="/images/run_icon.png"
+        onClick={handleOnClick}
+      />
+    </DarkDiv>
+    </div>
   );
 }
+const DarkDiv = styled.div`
+  background-color: black;
+`;
 
 export default ButtonExamples;
