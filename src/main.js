0b6054d62f01936ea325a6951acc5e4c0d756de9
Cheng <jcheng42@dxc.com>
2019-08-06 09:01:07 +0200

    Dropdown

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import DxcButton from "./button/Button.jsx";
 import DxcCheckbox from "./checkbox/Checkbox.jsx";
+import DxcDropdown from "./dropdown/Dropdown.jsx";
 import DxcSwitch from "./switch/Switch.jsx";
 import DxcSlider from "./slider/Slider.jsx";
 import DxcRadio from "./radio/Radio.jsx";
 
-export { DxcButton, DxcCheckbox, DxcSwitch, DxcSlider, DxcRadio };
+export { DxcButton, DxcCheckbox, DxcDropdown, DxcSwitch, DxcSlider, DxcRadio };
