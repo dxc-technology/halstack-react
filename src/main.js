2659f063afb527bac7135cac10cfaaabe9e2ed94
Jairo Suarez Gonzalez <jsuarez81@gmail.com>
2019-09-02 13:08:58 +0200

    Adding components to main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import DxcButton from "./button/Button.jsx";
 import DxcCheckbox from "./checkbox/Checkbox.jsx";
+import DxcSelect from "./select/Select.jsx";
+import DxcInput from "./input-text/InputText.jsx"
 import DxcDropdown from "./dropdown/Dropdown.jsx";
 import DxcSwitch from "./switch/Switch.jsx";
 import DxcSlider from "./slider/Slider.jsx";
 import DxcRadio from "./radio/Radio.jsx";
 import DxcDate from "./date/Date.jsx";
 import DxcHeader from "./header/Header.jsx";
+import DxcTabs from "./tabs/Tabs.jsx";
 
-export { DxcButton, DxcCheckbox, DxcDropdown, DxcSwitch, DxcSlider, DxcRadio, DxcDate, DxcHeader };
+export { DxcButton, DxcCheckbox, DxcSelect, DxcInput, DxcDropdown, DxcSwitch, DxcSlider, DxcRadio, DxcDate, DxcHeader, DxcTabs };
