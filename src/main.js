96a65ec06d7fc56f6e065f5a4b3421d3b23a2089
Cheng <jcheng42@dxc.com>
2019-09-18 15:19:03 +0200

    Card Component

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import DxcButton from "./button/Button.jsx";
 import DxcCheckbox from "./checkbox/Checkbox.jsx";
-import DxcSelect from "./select/Select.jsx"
+import DxcSelect from "./select/Select.jsx";
 import DxcInput from "./input-text/InputText.jsx";
 import DxcDropdown from "./dropdown/Dropdown.jsx";
 import DxcSwitch from "./switch/Switch.jsx";
@@ -9,6 +9,21 @@ import DxcRadio from "./radio/Radio.jsx";
 import DxcDate from "./date/Date.jsx";
 import DxcHeader from "./header/Header.jsx";
 import DxcTabs from "./tabs/Tabs.jsx";
-import DxcDialog from "./dialog/Dialog.jsx"
+import DxcDialog from "./dialog/Dialog.jsx";
+import DxcCard from "./card/Card.jsx";
 
-export { DxcButton, DxcCheckbox, DxcSelect, DxcInput, DxcDropdown, DxcSwitch, DxcSlider, DxcRadio, DxcDate, DxcHeader, DxcTabs, DxcDialog };
+export {
+  DxcButton,
+  DxcCheckbox,
+  DxcSelect,
+  DxcInput,
+  DxcDropdown,
+  DxcSwitch,
+  DxcSlider,
+  DxcRadio,
+  DxcDate,
+  DxcHeader,
+  DxcTabs,
+  DxcDialog,
+  DxcCard
+};
