d2eaa71df0fb748636872373765ab68e90997a44
Jesus Suarez Ardid <jesussuarezardid@gmail.com>
2019-11-27 18:36:03 +0100

    Start app

---
+++ app/src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Router } from "@reach/router";
+import styled from "styled-components";
+
+import SideNav from "./SideNav";
+
+import paths from "./paths";
+
+function App() {
+  return (
+    <MainContainer>
+      <SideNav></SideNav>
+      <Content>
+        <Router>
+          {paths.map(path => (
+            <path.component path={path.path}></path.component>
+          ))}
+        </Router>
+      </Content>
+    </MainContainer>
+  );
+}
+
+const MainContainer = styled.div`
+  display: flex;
+  height: 100vh;
+  background: #efefef;
+`;
+
+const Content = styled.div`
+  margin: 50px;
+  background: white;
+`;
+
+export default App;
---
+++ app/src/pages/Checkbox.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { DxcButton } from "@diaas/dxc-react-cdk";
+
+function App() {
+  return (
+    <div>
+      <DxcButton label="Checkbox"></DxcButton>
+    </div>
+  );
+}
+
+export default App;
