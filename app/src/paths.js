b99a259df74713943190777495579581f7c701a5
Arrojo Lopez <rarrojolopez@dxc.com>
2019-12-05 13:56:19 +0100

    Change alert

--- app/src/paths.js
+++ app/src/paths.js
@@ -1,5 +1,6 @@
 import Button from "./pages/Button";
 import Checkbox from "./pages/Checkbox";
+import Alert from "./pages/Alert";
 
 export default [
   {
@@ -11,5 +12,10 @@ export default [
     path: "checkbox",
     name: "Checkbox",
     component: Checkbox
+  },
+  {
+    path: "alert",
+    name: "Alert",
+    component: Alert
   }
 ];
