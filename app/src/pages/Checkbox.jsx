142efb5fe9668d0a80757b83942da9fb8c940c6a
Cheng <jcheng42@dxc.com>
2019-12-05 15:43:51 +0100

    Checkbox added to app + control fixes

--- app/src/pages/Checkbox.jsx
+++ app/src/pages/Checkbox.jsx
@@ -1,10 +1,37 @@
-import React from "react";
-import { DxcButton } from "@diaas/dxc-react-cdk";
+import React, { useState } from "react";
+import { DxcCheckbox } from "@diaas/dxc-react-cdk";
 
 function App() {
+  const [checked, changeChecked] = useState(false);
+  const onChange = newValue => {
+    changeChecked(newValue);
+  };
   return (
     <div>
-      <DxcButton label="Checkbox"></DxcButton>
+
+      <div
+        style={{ width: "100%", display: "inline-flex", alignItems: "center" }}
+      >
+        <DxcCheckbox checked={checked} label="Controlled" onChange={onChange} />
+        <DxcCheckbox label="Uncontrolled" />
+      </div>
+
+      <div
+        style={{
+          width: "100%",
+          display: "inline-flex",
+          alignItems: "center",
+          background: "#666666"
+        }}
+      >
+        <DxcCheckbox
+          theme="dark"
+          checked={checked}
+          label="Controlled"
+          onChange={onChange}
+        />
+        <DxcCheckbox theme="dark" label="Uncontrolled" />
+      </div>
     </div>
   );
 }
