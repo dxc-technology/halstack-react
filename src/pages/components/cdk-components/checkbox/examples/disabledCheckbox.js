180de10600a0aa65d2e272864ef23c1b45a8f163
Cheng <jcheng42@dxc.com>
2019-12-10 15:32:30 +0100

    checkbox cases

--- src/pages/components/cdk-components/checkbox/examples/disabledCheckbox.js
+++ src/pages/components/cdk-components/checkbox/examples/disabledCheckbox.js
@@ -2,16 +2,10 @@ import { DxcCheckbox } from "@diaas/dxc-react-cdk";
 import { useState } from "react";
 
 const code = `() => {
-    const [checked, changeChecked] = useState(false);
-    const onChange = (newValue) => {
-        changeChecked(newValue);
-    };
-  
     return (
       <DxcCheckbox
-      checked={checked}
       label="Checkbox"
-      onChange={onChange}
+      disabled={true}
       />
     );
   }`;
