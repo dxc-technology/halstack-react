4029c108b0e4c5794e8b38788abb97572e8c47ea
Cheng <jcheng42@dxc.com>
2019-12-10 13:02:37 +0100

    Switch added + controllable

---
+++ app/src/pages/Switch.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { DxcSwitch } from "@diaas/dxc-react-cdk";
+
+function App() {
+  const [checked, changeChecked] = useState(false);
+  const onChange = newValue => {
+    changeChecked(newValue);
+  };
+  return (
+    <div>
+
+      <div
+        style={{ width: "100%", display: "inline-flex", alignItems: "center" }}
+      >
+        <DxcSwitch checked={checked} label="Controlled" onChange={onChange} />
+        <DxcSwitch label="Uncontrolled" />
+      </div>
+
+      <div
+        style={{
+          width: "100%",
+          display: "inline-flex",
+          alignItems: "center",
+          background: "#000000"
+        }}
+      >
+        <DxcSwitch
+          theme="dark"
+          checked={checked}
+          label="Controlled"
+        />
+        <DxcSwitch theme="dark" label="Uncontrolled" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
