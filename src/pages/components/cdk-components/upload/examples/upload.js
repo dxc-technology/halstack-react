8d5ba561ef1609874497994f845344720535334a
Arrojo Lopez <rarrojolopez@dxc.com>
2019-12-12 15:56:06 +0100

    Upload

--- src/pages/components/cdk-components/upload/examples/upload.js
+++ src/pages/components/cdk-components/upload/examples/upload.js
@@ -2,6 +2,7 @@ import { DxcUpload } from "@diaas/dxc-react-cdk";
 
 const code = `() => {
   async function callbackFunc(file) {
+    console.log(file);
     return new Promise(resolve => setTimeout(resolve, 1000));
   }
 
