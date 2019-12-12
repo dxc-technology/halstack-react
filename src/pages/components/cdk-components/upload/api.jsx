0392ba76debd0d263f1833434a0455a5d9b2ccee
Arrojo Lopez <rarrojolopez@dxc.com>
2019-12-12 12:41:44 +0100

    Changes upload

--- src/pages/components/cdk-components/upload/api.jsx
+++ src/pages/components/cdk-components/upload/api.jsx
@@ -17,7 +17,7 @@ const checkboxPropsTable = () => {
           for every file to be uploaded, we will send as a parameter the File
           object; apart from that this function should return one promise on
           which we should make 'then'(here we should show a Success alert) or
-          'catch' (in this case we would receive the error message as a string)
+          'catch' (in this case we would receive the error message as a string).
         </td>
       </tr>
     </PropsTable>
