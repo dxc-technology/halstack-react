aeb5045e8ba66aaca000a5f851af4a0cec828a81
Arrojo Lopez <rarrojolopez@dxc.com>
2019-12-13 09:32:27 +0100

    Upload

--- src/pages/components/paths.js
+++ src/pages/components/paths.js
@@ -3,11 +3,8 @@ import Checkbox from "./cdk-components/checkbox/Checkbox";
 import Switch from "./cdk-components/switch/Switch";
 import Alert from "./cdk-components/alert/Alert";
 import Radio from "./cdk-components/radio/Radio";
-<<<<<<< HEAD
 import Dropdown from "./cdk-components/dropdown/Dropdown";
 import Tabs from "./cdk-components/tabs/Tabs";
-=======
->>>>>>> 8d5ba561ef1609874497994f845344720535334a
 import Upload from "./cdk-components/upload/Upload";
 
 export default [
@@ -27,7 +24,6 @@ export default [
     component: Checkbox
   },
   {
-<<<<<<< HEAD
     path: "dropdown",
     name: "Dropdown",
     component: Dropdown
@@ -42,25 +38,12 @@ export default [
     name: "Switch",
     component: Switch
   },
-
   {
     path: "tabs",
     name: "Tabs",
     component: Tabs
   },
   {
-=======
-    path: "radio",
-    name: "Radio Button",
-    component: Radio
-  },
-  {
-    path: "switch",
-    name: "Switch",
-    component: Switch
-  },
-  {
->>>>>>> 8d5ba561ef1609874497994f845344720535334a
     path: "upload",
     name: "Upload",
     component: Upload
