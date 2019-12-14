7d708ef26e1285d505d4eaf7baf9533c779ba193
Jesus Suarez Ardid <jesussuarezardid@gmail.com>
2019-12-14 17:07:11 +0100

    Tabs for sections scrollspy

--- lib/src/tabs-for-sections/TabsForSections.jsx
+++ lib/src/tabs-for-sections/TabsForSections.jsx
@@ -1,12 +1,18 @@
 import React, { useRef } from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
+import useScrollSpy from "react-use-scrollspy";
 
 import DxcTabs from "../tabs/Tabs";
 
 const DxcTabsForSections = ({ tabsMode, tabsTheme, disableTabsRipple, sections }) => {
-  const tabs = sections.map(section => ({ label: section.tabLabel }));
+  const tabs = [{ label: "tab 1" }, { label: "tab 2" }, { label: "tab 3" }];
+  //const refs = [useRef(null), useRef(null), useRef(null)];
   const refs = sections.map(() => useRef(null));
+
+  const activeTab = useScrollSpy({
+    sectionElementRefs: refs
+  });
 
   const onTabClick = tabId =>
     refs[tabId].current.scrollIntoView({
@@ -21,8 +27,9 @@ const DxcTabsForSections = ({ tabsMode, tabsTheme, disableTabsRipple, sections }
           mode={tabsMode}
           theme={tabsTheme}
           disableRipple={disableTabsRipple}
-          onTabClick={onTabClick}
           tabs={tabs}
+          activeTabIndex={activeTab}
+          onTabClick={onTabClick}
         ></DxcTabs>
       </StyledTabs>
       {sections.map((section, i) => (
@@ -55,6 +62,6 @@ DxcTabsForSections.defaultProps = {
 
 const StyledTabs = styled.div`
   position: sticky;
-  top: 60px;
+  top: 0px;
 `;
 export default DxcTabsForSections;
