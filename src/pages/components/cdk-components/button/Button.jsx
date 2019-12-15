f707b6c15495ae5c0b43a51dcaa62f450ab6647f
Jesus Suarez Ardid <jesussuarezardid@gmail.com>
2019-12-15 20:33:22 +0100

    tabs for sections

--- src/pages/components/cdk-components/button/Button.jsx
+++ src/pages/components/cdk-components/button/Button.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import DxcTabsForSections from "./../../../../common/TabsForSections";
 
 import ComponentDoc from "../../common/ComponentDoc";
 import Section from "../../common/Section";
@@ -13,15 +14,31 @@ function Button() {
   return (
     <ComponentDoc>
       <DocTitle size={1}>Button</DocTitle>
-      <Section>
-        <DocTitle size={2}>Props</DocTitle>
-        <ButtonPropsTable />
-      </Section>
-      <Section>
-        <DocTitle size={2}>Elements</DocTitle>
-        <Example title="Default Button" example={modes}></Example>
-        <Example title="Disabled Button" example={disabled}></Example>
-      </Section>
+      <DxcTabsForSections
+        stickAtPx={64}
+        tabsMode="underlined"
+        sections={[
+          {
+            tabLabel: "Props",
+            section: () => (
+              <Section>
+                <DocTitle size={2}>Props</DocTitle>
+                <ButtonPropsTable />
+              </Section>
+            )
+          },
+          {
+            tabLabel: "Elements",
+            section: () => (
+              <Section>
+                <DocTitle size={2}>Elements</DocTitle>
+                <Example title="Default Button" example={modes}></Example>
+                <Example title="Disabled Button" example={disabled}></Example>
+              </Section>
+            )
+          }
+        ]}
+      ></DxcTabsForSections>
     </ComponentDoc>
   );
 }
