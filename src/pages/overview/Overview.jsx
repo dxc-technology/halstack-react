0303dd513278ed948de8508c961bc1448c899c0f
Jesus Suarez Ardid <jesussuarezardid@gmail.com>
2019-12-17 09:12:02 +0100

    Overview structure

--- src/pages/overview/Overview.jsx
+++ src/pages/overview/Overview.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import { DxcTabsForSections } from "@diaas/dxc-react-cdk";
+import styled from "styled-components";
 
-function Overview() {
-  return <div>Overview Page</div>;
+import DocTitle from "../../common/DocTitle";
+import Section from "../components/common/Section";
+
+function Alert() {
+  return (
+    <StyledOverview>
+      <DocTitle size={1}>Overview</DocTitle>
+      <DxcTabsForSections
+        stickAtPx={64}
+        tabsMode="underlined"
+        sections={[
+          {
+            tabLabel: "Getting Started",
+            section: () => (
+              <Section>
+                <DocTitle size={2}>Getting Started</DocTitle>
+                <p>Getting Started text</p>
+              </Section>
+            )
+          },
+          {
+            tabLabel: "Installation",
+            section: () => (
+              <Section>
+                <DocTitle size={2}>Installation</DocTitle>
+                <p>Installation text</p>
+              </Section>
+            )
+          },
+          {
+            tabLabel: "Using Components",
+            section: () => (
+              <Section>
+                <DocTitle size={2}>Using Components</DocTitle>
+                <p>Using Components text</p>
+              </Section>
+            )
+          }
+        ]}
+      ></DxcTabsForSections>
+    </StyledOverview>
+  );
 }
 
-export default Overview;
+const StyledOverview = styled.div`
+  max-width: 900px;
+  margin: 50px auto;
+`;
+
+export default Alert;
