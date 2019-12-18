45c6e726773cf2c9b23216f8e116dc397031b32b
Jairo Suarez Gonzalez <jsuarez81@gmail.com>
2019-12-18 11:12:18 +0100

    Spinner Component

---
+++ src/pages/components/cdk-components/spinner/Spinner.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { DxcTabsForSections } from "@diaas/dxc-react-cdk";
+
+import ComponentDoc from "../../common/ComponentDoc";
+import DocTitle from "../../../../common/DocTitle";
+import Example from "../../common/Example";
+import Section from "../../common/Section";
+
+import SpinnerPropsTable from "./api.jsx";
+
+import defaultSpinner from "./examples/defaultSpinner";
+import valueSpinner from "./examples/valueSpinner";
+import smallSpinner from "./examples/smallSpinner";
+import darkSpinner from "./examples/darkSpinner";
+import spinnerWithOverlay from "./examples/spinnerWithOverlay";
+
+function Spinner() {
+  return (
+    <ComponentDoc>
+      <DocTitle size={1}>Spinner</DocTitle>
+      <DxcTabsForSections
+        stickAtPx={64}
+        tabsMode="underlined"
+        sections={[
+          {
+            tabLabel: "Props",
+            section: () => (
+              <Section>
+                <DocTitle size={2}>Props</DocTitle>
+                <SpinnerPropsTable />
+              </Section>
+            )
+          },
+          {
+            tabLabel: "Examples",
+            section: () => (
+              <Section>
+                <DocTitle size={2}>Examples</DocTitle>
+                <Example title="Undetermined spinner" example={defaultSpinner}></Example>
+                <Example title="Determined spinner" example={valueSpinner}></Example>
+                <Example title="Small spinner" example={smallSpinner}></Example>
+                <Example title="Themed spinner" example={darkSpinner}></Example>
+                <Example title="Themed spinner" example={spinnerWithOverlay}></Example>
+              </Section>
+            )
+          }
+        ]}
+      ></DxcTabsForSections>
+    </ComponentDoc>
+  );
+}
+
+export default Spinner;
