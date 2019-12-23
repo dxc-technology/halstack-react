bc87747c8b3d0e87cf7653d942f8174606f7f749
Arrojo Lopez <rarrojolopez@dxc.com>
2019-12-23 11:19:22 +0100

    Format change

--- src/pages/components/cdk-components/switch/examples/labelPositionSwitch.js
+++ src/pages/components/cdk-components/switch/examples/labelPositionSwitch.js
@@ -3,11 +3,10 @@ import { useState } from "react";
 
 const code = `() => {
   const [checked, changeChecked] = useState(true);
+  const [checked1, changeChecked1] = useState(false);
   const onChange = newValue => {
     changeChecked(newValue);
   };
-
-  const [checked1, changeChecked1] = useState(false);
   const onChange1 = newValue => {
     changeChecked1(newValue);
   };
