2d6437397d817087053dfe0b9f5407a8590ab7f5
Jesus Suarez Ardid <jesussuarezardid@gmail.com>
2020-01-08 10:59:28 +0100

    Box examples

---
+++ src/pages/components/cdk-components/box/Box.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { DxcTabsForSections } from "@diaas/dxc-react-cdk";
+
+import ComponentDoc from "../../common/ComponentDoc";
+import Section from "../../common/Section";
+import DocTitle from "../../../../common/DocTitle";
+import Example from "../../common/Example";
+import BoxPropsTable from "./api.jsx";
+
+import basicExample from "./examples/basicExample";
+
+function Card() {
+  return (
+    <ComponentDoc>
+      <DocTitle size={1}>Box</DocTitle>
+      <DxcTabsForSections
+        stickAtPx={64}
+        tabsMode="underlined"
+        sections={[
+          {
+            tabLabel: "Props",
+            section: () => (
+              <Section>
+                <DocTitle size={2}>Props</DocTitle>
+                <BoxPropsTable />
+              </Section>
+            )
+          },
+          {
+            tabLabel: "Examples",
+            section: () => (
+              <Section>
+                <DocTitle size={2}>Examples</DocTitle>
+                <Example title="Basic Example" example={basicExample}></Example>
+              </Section>
+            )
+          }
+        ]}
+      ></DxcTabsForSections>
+    </ComponentDoc>
+  );
+}
+
+export default Card;
---
+++ src/pages/components/cdk-components/box/api.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { DxcTable } from "@diaas/dxc-react-cdk";
+
+const buttonPropsTable = () => {
+  return (
+    <DxcTable>
+      <tr>
+        <th>Name</th>
+        <th>Default</th>
+        <th>Description</th>
+      </tr>
+      <tr>
+        <td>shadowDepth: 0 | 1 | 2</td>
+        <td></td>
+        <td>The size of the shadow to be displayed around the box.</td>
+      </tr>
+      <tr>
+        <td>display: string</td>
+        <td>"inline-flex"</td>
+        <td>Changes the display CSS property of the Box div.</td>
+      </tr>
+      <tr>
+        <td>margin: string | object</td>
+        <td></td>
+        <td>
+          Size of the margin to be applied to the component ('xxsmall' |
+          'xsmall' | 'small' | 'medium' | 'large' | 'xlarge' | 'xxlarge'). You
+          can pass an object with 'top', 'bottom', 'left' and 'right' properties
+          in order to specify different margin sizes.
+        </td>
+      </tr>
+      <tr>
+        <td>padding: string | object</td>
+        <td></td>
+        <td>
+          Size of the padding to be applied to the component ('xxsmall' |
+          'xsmall' | 'small' | 'medium' | 'large' | 'xlarge' | 'xxlarge'). You
+          can pass an object with 'top', 'bottom', 'left' and 'right' properties
+          in order to specify different padding sizes.
+        </td>
+      </tr>
+    </DxcTable>
+  );
+};
+
+export default buttonPropsTable;
