7c51435583819251c5988ff9f3bcd4a356a6e9eb
Cheng <jcheng42@dxc.com>
2020-01-13 08:54:23 +0100

    box Sizes

--- app/src/pages/Box.jsx
+++ app/src/pages/Box.jsx
@@ -3,9 +3,22 @@ import { DxcBox } from "@diaas/dxc-react-cdk";
 
 function App() {
   return (
-    <DxcBox margin="medium" padding="medium" shadowDepth={1} display="block">
-      Hola que tal
-    </DxcBox>
+    <div>
+      <DxcBox margin="medium" padding="medium" shadowDepth={1} display="block" size ="medium">
+        Hola que tal
+      </DxcBox>
+      <DxcBox margin="medium" padding="medium" shadowDepth={1} display="block" size ="large">
+        Hola que tal
+      </DxcBox>
+      <DxcBox margin="medium" padding="medium" shadowDepth={1} display="block" size ="small">
+        Hola que tal
+      </DxcBox>
+      <div style={{width: "250px"}}>
+        <DxcBox margin="medium" padding="medium" shadowDepth={1} display="block" size ="fillParent">
+          Hola que tal
+        </DxcBox>
+      </div>
+    </div>
   );
 }
 
