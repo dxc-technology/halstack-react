fea24c5692a6d519a2a53e096208501c322c8c8b
Jesus Suarez Ardid <jesussuarezardid@gmail.com>
2020-01-16 17:05:54 +0100

    Add screenshot testing

--- app/cypress/integration/sample_spec.js
+++ app/cypress/integration/sample_spec.js
@@ -1,5 +1,25 @@
-describe("My First Test", function() {
-  it("Does not do much!", function() {
-    expect(true).to.equal(true);
+describe("Checkbox", () => {
+  it("should be publicly accessible", () => {
+    cy.visit("http://localhost:3000").then(() => {
+      const linksArray = Cypress.$("#links-list a");
+      if (linksArray.length) {
+        cy.wrap(linksArray).each(link => {
+          const componentName = link.text();
+          cy.wrap(link)
+            .click()
+            .then(() => {
+              const testCasesArray = Cypress.$("#tests-container .test-case");
+              if (testCasesArray.length) {
+                cy.wrap(testCasesArray).each(testCase => {
+                  const testCaseId = testCase.attr("id");
+                  cy.wrap(testCase).matchImageSnapshot(
+                    `${componentName} - ${testCaseId}`
+                  );
+                });
+              }
+            });
+        });
+      }
+    });
   });
 });
