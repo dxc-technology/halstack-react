c3d5fa53aad1aba090736e6178cabc8ee234b841
Jesus Suarez Ardid <jesussuarezardid@gmail.com>
2020-02-05 15:41:39 +0100

    Add date component

---
+++ src/pages/components/cdk-components/date/Date.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { DxcTabsForSections } from "@diaas/dxc-react-cdk";
+
+import ComponentDoc from "../../common/ComponentDoc";
+import DocTitle from "../../../../common/DocTitle";
+import Example from "../../common/Example";
+import DatePropsTable from "./api.jsx";
+import Section from "../../common/Section";
+import defaultDate from "./examples/default";
+
+
+function Accordion() {
+  return (
+    <ComponentDoc>
+      <DocTitle size={1}>Date</DocTitle>
+      <DxcTabsForSections
+        stickAtPx={64}
+        tabsMode="underlined"
+        sections={[
+          {
+            tabLabel: "Props",
+            section: () => (
+              <Section>
+                <DocTitle size={2}>Props</DocTitle>
+                <DatePropsTable />
+              </Section>
+            )
+          },
+          {
+            tabLabel: "Examples",
+            section: () => (
+              <Section>
+                <DocTitle size={2}>Examples</DocTitle>
+                <Example
+                  title="Default Date"
+                  example={defaultDate}
+                ></Example>
+              </Section>
+            )
+          }
+        ]}
+      ></DxcTabsForSections>
+    </ComponentDoc>
+  );
+}
+
+export default Accordion;
