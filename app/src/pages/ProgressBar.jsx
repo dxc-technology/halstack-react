8bd7dc9b27f9116b51ab7e7e4132c2c89bb45a07
Arrojo Lopez <rarrojolopez@dxc.com>
2020-02-05 09:11:32 +0100

    Test cases progress bar and snapshots

--- app/src/pages/ProgressBar.jsx
+++ app/src/pages/ProgressBar.jsx
@@ -1,8 +1,123 @@
-import React, { useState } from "react";
-import { DxcProgressBar, DxcButton } from "@diaas/dxc-react-cdk";
+import React from "react";
+import { DxcProgressBar } from "@diaas/dxc-react-cdk";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="test-case" id="light-theme">
+        <h4>Light theme</h4>
+        <DxcProgressBar
+          label="Loading..."
+          overlay={false}
+          showValue
+          value={50}
+        />
+      </div>
+
+      <div>
+        <h4>Dark theme</h4>
+        <div
+          style={{ background: "#000000" }}
+          className="test-case"
+          id="dark-theme"
+        >
+          <DxcProgressBar
+            label="Loading..."
+            theme="dark"
+            overlay={false}
+            showValue
+            value={40}
+          />
+        </div>
+      </div>
+
+      <div className="test-case" id="without-label">
+        <h4>Without label</h4>
+        <DxcProgressBar overlay={false} showValue value={50} />
+      </div>
+
+      <div>
+        <h4>Margins</h4>
+        <div className="test-case" id="xxsmall-margin">
+          <h5>xxsmall margin</h5>
+          <DxcProgressBar
+            label="Loading..."
+            theme="light"
+            overlay={false}
+            showValue
+            value={20}
+            margin="xxsmall"
+          />
+        </div>
+        <div className="test-case" id="xsmall-margin">
+          <h5>xsmall margin</h5>
+          <DxcProgressBar
+            label="Loading..."
+            theme="light"
+            overlay={false}
+            showValue
+            value={35}
+            margin="xsmall"
+          />
+        </div>
+        <div className="test-case" id="small-margin">
+          <h5>Small margin</h5>
+          <DxcProgressBar
+            label="Loading..."
+            theme="light"
+            overlay={false}
+            showValue
+            value={45}
+            margin="small"
+          />
+        </div>
+        <div className="test-case" id="medium-margin">
+          <h5>Medium margin</h5>
+          <DxcProgressBar
+            label="Loading..."
+            theme="light"
+            overlay={false}
+            showValue
+            value={60}
+            margin="medium"
+          />
+        </div>
+        <div className="test-case" id="large-margin">
+          <h5>Large margin</h5>
+          <DxcProgressBar
+            label="Loading..."
+            theme="light"
+            overlay={false}
+            showValue
+            value={75}
+            margin="large"
+          />
+        </div>
+        <div className="test-case" id="xlarge-margin">
+          <h5>xlarge margin</h5>
+          <DxcProgressBar
+            label="Loading..."
+            theme="light"
+            overlay={false}
+            showValue
+            value={85}
+            margin="xlarge"
+          />
+        </div>
+        <div className="test-case" id="xxlarge-margin">
+          <h5>xxlarge margin</h5>
+          <DxcProgressBar
+            label="Loading..."
+            theme="light"
+            overlay={false}
+            showValue
+            value={100}
+            margin="xxlarge"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
