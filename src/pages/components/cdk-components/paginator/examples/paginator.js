de1c507605e3e1dc462ad22cfe808f302fc3fbaa
Arrojo Lopez <rarrojolopez@dxc.com>
2020-02-20 16:12:45 +0100

    Paginator added to the site

---
+++ src/pages/components/cdk-components/paginator/examples/paginator.js
@@ -0,0 +1,38 @@
+import { DxcPaginator } from "@diaas/dxc-react-cdk";
+import { useState } from "react";
+
+const code = `() => {
+  const [page, changePage] = useState(1);
+
+  const prevClick = () => {
+    changePage(page - 1);
+  };
+  const firstClick = () => {
+    changePage(1);
+  };
+  const nextClick = () => {
+    changePage(page + 1);
+  };
+  const lastClick = currPage => {
+    changePage(currPage);
+  };
+
+  return (
+    <DxcPaginator
+      currentPage={page}
+      itemsPerPage={10}
+      totalItems={27}
+      prevFunction={prevClick}
+      firstFunction={firstClick}
+      nextFunction={nextClick}
+      lastFunction={lastClick}
+    ></DxcPaginator>
+  );
+}`;
+
+const scope = {
+  DxcPaginator,
+  useState
+};
+
+export default { code, scope };
