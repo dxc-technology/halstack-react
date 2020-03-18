10ce1da9ceaa2e8a4583c38b37bb4324d40b2d97
Arrojo Lopez <rarrojolopez@dxc.com>
2020-03-18 14:08:43 +0100

    Added props documentation to table

--- src/pages/components/cdk-components/table/Table.jsx
+++ src/pages/components/cdk-components/table/Table.jsx
@@ -5,6 +5,7 @@ import ComponentDoc from "../../common/ComponentDoc";
 import DocTitle from "../../../../common/DocTitle";
 import Section from "../../common/Section";
 import Example from "../../common/Example";
+import TablePropsTable from "./api.jsx";
 
 import defaultTable from "./examples/defaultTable";
 
@@ -17,14 +18,20 @@ function Tabs() {
         tabsMode="underlined"
         sections={[
           {
+            tabLabel: "Props",
+            section: () => (
+              <Section>
+                <DocTitle size={2}>Props</DocTitle>
+                <TablePropsTable />
+              </Section>
+            )
+          },
+          {
             tabLabel: "Examples",
             section: () => (
               <Section>
                 <DocTitle size={2}>Examples</DocTitle>
-                <Example
-                  title="Simple Table"
-                  example={defaultTable}
-                ></Example>
+                <Example title="Simple Table" example={defaultTable}></Example>
               </Section>
             )
           }
