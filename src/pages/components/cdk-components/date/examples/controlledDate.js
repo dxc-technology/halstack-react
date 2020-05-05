9a5eb390bd6a8e465f5b898fe14c6921bfdb1de1
Jairo Suarez Gonzalez <jsuarez81@gmail.com>
2020-05-05 13:22:22 +0200

    Updating cdk version and changes in Date component examples

--- src/pages/components/cdk-components/date/examples/controlledDate.js
+++ src/pages/components/cdk-components/date/examples/controlledDate.js
@@ -5,28 +5,31 @@ const code = `() => {
   const [inputValue, changeInput] = useState("1995/12/03");
   const [isInvalid, changeIsInvalid] = useState(false);
 
-  const onChange = newValue => {
-    changeInput(newValue);
-    if (!newValue.dateValue) {
-      changeIsInvalid(!isInvalid);
-    }
+  const onChange = ({ stringValue, dateValue }) => {
+    changeInput(stringValue);
+    changeIsInvalid(dateValue ? false : true);
+  };
+  const onBlur = (stringValue) => {
+    changeInput(stringValue);
   };
 
   return (
     <DxcDate
-      label="Input label"
-      value={inputValue}
-      margin="medium"
-      assistiveText="assistive text"
-      onInputChange={onChange}
-      invalid={isInvalid}
-    />
+          label="Input label"
+          assistiveText="assistive text"
+          value={inputValue}
+          invalid={isInvalid}
+          placeholder
+          format="MM-dd-yyyy"
+          onBlur={onBlur}
+          onChange={onChange}
+        />
   );
 }`;
 
 const scope = {
   DxcDate,
-  useState
+  useState,
 };
 
 export default { code, scope };
