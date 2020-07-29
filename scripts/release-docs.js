0aff140314a22aa88776d0acbbfb82164cf74d31
Jesus Suarez Ardid <jesussuarezardid@gmail.com>
2020-07-29 15:30:16 +0200

    Update release-docs.js adding redirection

--- scripts/release-docs.js
+++ scripts/release-docs.js
@@ -1,23 +1,87 @@
 const { exec } = require("child_process");
+const AWS = require("aws-sdk");
 
 const BUCKET_NAME = "design.site.test";
 const DIRECTORY = "tools/react/";
-const versionToDeploy = process.argv[2];
 
-const majorVersionToDeploy = Number(
-  versionToDeploy.substring(0, versionToDeploy.indexOf("."))
-);
+const processListObjectsResponse = (response) => {
+  return response.CommonPrefixes.map((commonPrefix) => {
+    const prefix = commonPrefix.Prefix;
+    return prefix.substring(
+      prefix.lastIndexOf(DIRECTORY) + DIRECTORY.length,
+      prefix.length - 1
+    );
+  })
+    .filter((version) => version !== "next" && version !== "latest")
+    .map((version) => Number(version));
+};
 
-exec(
-  `aws s3 rm s3://${BUCKET_NAME}/tools/react/${majorVersionToDeploy}/ --recursive &&
-    aws s3 cp ./docs/build/ s3://${BUCKET_NAME}/tools/react/${majorVersionToDeploy}/ --recursive`,
-  (error, stdout, stderr) => {
-    if (error) {
-      throw new Error(error.message)
-    }
-    if (stderr) {
-      throw new Error(stderr)
-    }
-    console.log(`stdout: ${stdout}`);
+const getVersionsInS3Bucket = async () => {
+  const params = {
+    Bucket: BUCKET_NAME,
+    Prefix: DIRECTORY,
+    Delimiter: "/",
+  };
+
+  return new Promise((resolve, reject) => {
+    new AWS.S3().listObjectsV2(params, (error, data) => {
+      if (error) {
+        reject(new Error(error));
+      } else {
+        resolve(processListObjectsResponse(data));
+      }
+    });
+  });
+};
+
+const moveToBucket = () => {
+  return new Promise((resolve, reject) => {
+    exec(
+      `aws s3 rm s3://${BUCKET_NAME}/${DIRECTORY}${majorVersionToDeploy}/ --recursive &&
+        aws s3 cp ./docs/build/ s3://${BUCKET_NAME}/${DIRECTORY}${majorVersionToDeploy}/ --recursive`,
+      (error, stdout, stderr) => {
+        if (error) {
+          reject(error.message);
+        }
+        if (stderr) {
+          reject(stderr);
+        }
+        resolve(stdout);
+      }
+    );
+  });
+};
+
+const updateRedirectionToLatest = (version) => {
+  const redirection = `window.location.replace("https://developer.dxc.com/tools/react/${version}");`;
+  return new Promise((resolve, reject) => {
+    exec(
+      `echo '${redirection}' | aws s3 cp - s3://${BUCKET_NAME}/${DIRECTORY}latest/index.html`,
+      (error, stdout, stderr) => {
+        if (error) {
+          reject(error.message);
+        }
+        if (stderr) {
+          reject(stderr);
+        }
+        resolve(stdout);
+      }
+    );
+  });
+};
+
+const deploy = async () => {
+  const versionToDeploy = process.argv[2];
+  const majorVersionToDeploy = Number(
+    versionToDeploy.substring(0, versionToDeploy.indexOf("."))
+  );
+  const existingVersionsInBucket = await getVersionsInS3Bucket();
+  const isNewLatest = existingVersionsInBucket.includes(majorVersionToDeploy);
+  await moveToBucket();
+  if (isNewLatest) {
+    await updateRedirectionToLatest(majorVersionToDeploy);
   }
-);
+  console.log(deployedVersions);
+};
+
+deploy();
