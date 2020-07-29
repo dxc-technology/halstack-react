619aee79992bd81e27a27123b185e99fb3f8b4ee
Jesus Suarez Ardid <jesussuarezardid@gmail.com>
2020-07-29 12:23:06 +0200

    Publish docs to s3 folder script

--- scripts/release-docs.js
+++ scripts/release-docs.js
@@ -1,61 +1,27 @@
-const AWS = require("aws-sdk");
-const fs = require('fs');
+const { exec } = require("child_process");
 
 const BUCKET_NAME = "design.site.test";
 const DIRECTORY = "tools/react/";
 const versionToDeploy = process.argv[2];
+
 const majorVersionToDeploy = Number(
   versionToDeploy.substring(0, versionToDeploy.indexOf("."))
 );
 
-const processListObjectsResponse = (response) => {
-  return response.CommonPrefixes.map((commonPrefix) => {
-    const prefix = commonPrefix.Prefix;
-    return prefix.substring(
-      prefix.lastIndexOf(DIRECTORY) + DIRECTORY.length,
-      prefix.length - 1
-    );
-  })
-    .filter((version) => version !== "next" && version !== "latest")
-    .map((version) => Number(version));
-};
-
-const getVersionsInS3Bucket = async () => {
-  const params = {
-    Bucket: BUCKET_NAME,
-    Prefix: DIRECTORY,
-    Delimiter: "/",
-  };
-
-  return new Promise((resolve, reject) => {
-    new AWS.S3().listObjectsV2(params, (error, data) => {
-      if (error) {
-        reject(new Error(error));
-      } else {
-        resolve(processListObjectsResponse(data));
-      }
-    });
-  });
-};
-const deployToExistingVersion = () => {
-    var files = fs.readdirSync('./docs/build');
-    console.log(files)
-};
-
-const deployToLatest = () => {};
-
-const moveLatestToPreviousVersion = () => {};
-
-const releaseDocs = async () => {
-  const versionsInBucket = await getVersionsInS3Bucket();
-  if (versionsInBucket.includes(majorVersionToDeploy)) {
-    deployToExistingVersion();
-  } else if (versionsInBucket.includes(majorVersionToDeploy - 1)) {
-    deployToLatest();
-  } else {
-    moveLatestToPreviousVersion();
-    deployToLatest();
+exec(
+  `aws s3 rm s3://${majorVersionToDeploy}/tools/react/${majorVersionToDeploy}/ --recursive &&
+    aws s3 cp ./docs/build/ s3://${majorVersionToDeploy}/tools/react/${majorVersionToDeploy}/ --recursive`,
+  (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
   }
-};
+);
 
 releaseDocs();
