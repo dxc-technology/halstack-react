3a8852ffdbb077d44e1589738bc62b0a2613cf88
Jesus Suarez Ardid <jesussuarezardid@gmail.com>
2020-08-04 16:07:23 +0200

    fix copy script

--- scripts/build/copy-readme.js
+++ scripts/build/copy-readme.js
@@ -1,3 +1,3 @@
 const fs = require('fs');
 
-fs.createReadStream('./README.md').pipe(fs.createWriteStream('./lib/dist/README.md'));
+fs.createReadStream('../README.md').pipe(fs.createWriteStream('./dist/README.md'));
