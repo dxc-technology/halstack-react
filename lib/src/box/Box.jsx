3e61328d69452f9f9945c5a94603ee9597e1b0b7
Arrojo Lopez <rarrojolopez@dxc.com>
2020-08-18 09:17:22 +0200

    Box tokens theming

--- lib/src/box/Box.jsx
+++ lib/src/box/Box.jsx
@@ -1,15 +1,22 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useContext, useMemo } from "react";
+import styled, { ThemeProvider } from "styled-components";
 import PropTypes from "prop-types";
-import { spaces } from "../common/variables.js";
+import { spaces, defaultTheme, theme } from "../common/variables.js";
 import { getMargin } from "../common/utils.js";
+import { getCustomTheme } from "../common/utils.js";
+import ThemeContext from "../ThemeContext.js";
 import "../common/OpenSans.css";
 
-const DxcBox = ({ shadowDepth, margin, padding, display = "inline-flex", children, size="fitContent" }) => {
+const DxcBox = ({ shadowDepth, margin, padding, display = "inline-flex", children, size = "fitContent" }) => {
+  const customTheme = useContext(ThemeContext);
+  const colorsTheme = useMemo(() => getCustomTheme(theme, getCustomTheme(defaultTheme, customTheme)), [customTheme]);
+
   return (
-    <StyledDxcBox shadowDepth={shadowDepth} display={display} margin={margin} padding={padding} size={size}>
-      {children}
-    </StyledDxcBox>
+    <ThemeProvider theme={colorsTheme.box}>
+      <StyledDxcBox shadowDepth={shadowDepth} display={display} margin={margin} padding={padding} size={size}>
+        {children}
+      </StyledDxcBox>
+    </ThemeProvider>
   );
 };
 
@@ -18,7 +25,7 @@ const sizes = {
   medium: "240px",
   large: "480px",
   fillParent: "100%",
-  fitContent: "unset"
+  fitContent: "unset",
 };
 
 const calculateWidth = (margin, size, padding) => {
@@ -34,8 +41,8 @@ const StyledDxcBox = styled.div`
   border-radius: 4px;
   overflow: hidden;
   font-family: "Open Sans", sans-serif;
-  width: ${props => calculateWidth(props.margin, props.size, props.padding)};
-
+  width: ${(props) => calculateWidth(props.margin, props.size, props.padding)};
+  background-color: ${(props) => props.theme.backgroundColor};
   box-shadow: ${({ shadowDepth }) =>
     shadowDepth === 1
       ? "0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)"
@@ -65,26 +72,26 @@ DxcBox.propTypes = {
       top: PropTypes.oneOf(Object.keys(spaces)),
       bottom: PropTypes.oneOf(Object.keys(spaces)),
       left: PropTypes.oneOf(Object.keys(spaces)),
-      right: PropTypes.oneOf(Object.keys(spaces))
+      right: PropTypes.oneOf(Object.keys(spaces)),
     }),
-    PropTypes.oneOf([...Object.keys(spaces)])
+    PropTypes.oneOf([...Object.keys(spaces)]),
   ]),
   padding: PropTypes.oneOfType([
     PropTypes.shape({
       top: PropTypes.oneOf(Object.keys(spaces)),
       bottom: PropTypes.oneOf(Object.keys(spaces)),
       left: PropTypes.oneOf(Object.keys(spaces)),
-      right: PropTypes.oneOf(Object.keys(spaces))
+      right: PropTypes.oneOf(Object.keys(spaces)),
     }),
-    PropTypes.oneOf([...Object.keys(spaces)])
-  ])
+    PropTypes.oneOf([...Object.keys(spaces)]),
+  ]),
 };
 
 DxcBox.defaultProps = {
   shadowDepth: 2,
   display: "inline-flex",
   margin: null,
-  padding: null
+  padding: null,
 };
 
 export default DxcBox;
