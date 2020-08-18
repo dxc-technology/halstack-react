2252892936012cd14b9a990dd69103071e15f87a
Arrojo Lopez <rarrojolopez@dxc.com>
2020-08-18 13:32:54 +0200

    Link tokens theming

--- docs/src/pages/components/cdk-components/link/api.jsx
+++ docs/src/pages/components/cdk-components/link/api.jsx
@@ -2,82 +2,75 @@ import React from "react";
 import { DxcTable } from "@dxc-technology/halstack-react";
 
 const linkPropsTable = () => {
-    return (
-        <DxcTable>
-            <tr>
-                <th>Name</th>
-                <th>Default</th>
-                <th>Description</th>
-            </tr>
-            <tr>
-                <td>underlined: boolean</td>
-                <td>
-                    <code>true</code>
-                </td>
-                <td>If true, the text is underlined.</td>
-            </tr>
-            <tr>
-                <td>disabled: boolean</td>
-                <td>
-                    <code>false</code>
-                </td>
-                <td>If true, the link is disabled.</td>
-            </tr>
-            <tr>
-                <td>inheritColor: boolean</td>
-                <td>
-                    <code>false</code>
-                </td>
-                <td>If true, the color is inherited from parent.</td>
-            </tr>
-            <tr>
-                <td>text: string</td>
-                <td></td>
-                <td>Link text.</td>
-            </tr>
-            <tr>
-                <td>iconSrc: string</td>
-                <td></td>
-                <td>Source of the icon.</td>
-            </tr>
-            <tr>
-                <td>iconPosition: 'before' | 'after'</td>
-                <td>
-                    <code>'before'</code>
-                </td>
-                <td>Indicates the position of the icon in the component.</td>
-            </tr>
-            <tr>
-                <td>href: string</td>
-                <td></td>
-                <td>Page to be opened when the user clicks on the link.</td>
-            </tr>
-            <tr>
-                <td>theme: 'light' | 'dark'</td>
-                <td>
-                    <code>'light'</code>
-                </td>
-                <td>Theme of the component.</td>
-            </tr>
-            <tr>
-                <td>newWindow: boolean</td>
-                <td>
-                    <code>false</code>
-                </td>
-                <td>If true, the page is opened in a new browser tab.</td>
-            </tr>
-            <tr>
-                <td>margin: string | object</td>
-                <td></td>
-                <td>
-                Size of the margin to be applied to the component ('xxsmall' |
-                'xsmall' | 'small' | 'medium' | 'large' | 'xlarge' | 'xxlarge'). You
-                can pass an object with 'top', 'bottom', 'left' and 'right' properties
-                in order to specify different margin sizes.
-                </td>
-            </tr>
-        </DxcTable>
-    );
-}
+  return (
+    <DxcTable>
+      <tr>
+        <th>Name</th>
+        <th>Default</th>
+        <th>Description</th>
+      </tr>
+      <tr>
+        <td>underlined: boolean</td>
+        <td>
+          <code>true</code>
+        </td>
+        <td>If true, the text is underlined.</td>
+      </tr>
+      <tr>
+        <td>disabled: boolean</td>
+        <td>
+          <code>false</code>
+        </td>
+        <td>If true, the link is disabled.</td>
+      </tr>
+      <tr>
+        <td>inheritColor: boolean</td>
+        <td>
+          <code>false</code>
+        </td>
+        <td>If true, the color is inherited from parent.</td>
+      </tr>
+      <tr>
+        <td>text: string</td>
+        <td></td>
+        <td>Link text.</td>
+      </tr>
+      <tr>
+        <td>iconSrc: string</td>
+        <td></td>
+        <td>Source of the icon.</td>
+      </tr>
+      <tr>
+        <td>iconPosition: 'before' | 'after'</td>
+        <td>
+          <code>'before'</code>
+        </td>
+        <td>Indicates the position of the icon in the component.</td>
+      </tr>
+      <tr>
+        <td>href: string</td>
+        <td></td>
+        <td>Page to be opened when the user clicks on the link.</td>
+      </tr>
+      <tr>
+        <td>newWindow: boolean</td>
+        <td>
+          <code>false</code>
+        </td>
+        <td>If true, the page is opened in a new browser tab.</td>
+      </tr>
+      <tr>
+        <td>margin: string | object</td>
+        <td></td>
+        <td>
+          Size of the margin to be applied to the component ('xxsmall' |
+          'xsmall' | 'small' | 'medium' | 'large' | 'xlarge' | 'xxlarge'). You
+          can pass an object with 'top', 'bottom', 'left' and 'right' properties
+          in order to specify different margin sizes.
+        </td>
+      </tr>
+    </DxcTable>
+  );
+};
 
 export default linkPropsTable;
--- lib/src/link/Link.jsx
+++ lib/src/link/Link.jsx
@@ -1,78 +1,81 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useContext, useMemo } from "react";
+import styled, { ThemeProvider } from "styled-components";
 import PropTypes from "prop-types";
 import { spaces } from "../common/variables.js";
 
+import { defaultTheme, theme } from "../common/variables.js";
+import { getCustomTheme } from "../common/utils.js";
+import ThemeContext from "../ThemeContext.js";
+
 const DxcLink = ({
-    underlined = true,
-    inheritColor = false,
-    disabled = false,
-    iconSrc,
-    iconPosition = "before",
-    href = "",
-    theme = "light",
-    newWindow = false,
-    text = "",
-    margin
+  underlined = true,
+  inheritColor = false,
+  disabled = false,
+  iconSrc,
+  iconPosition = "before",
+  href = "",
+  newWindow = false,
+  text = "",
+  margin,
 }) => {
-    return (
-      <LinkText 
+  const customTheme = useContext(ThemeContext);
+  const colorsTheme = useMemo(() => getCustomTheme(theme, getCustomTheme(defaultTheme, customTheme)), [customTheme]);
+
+  return (
+    <ThemeProvider theme={colorsTheme.link}>
+      <LinkText
         underlined={underlined}
         inheritColor={inheritColor}
         disabled={disabled}
         href={href}
-        brightness={theme}
         margin={margin}
         iconPosition={iconPosition}
-        target={newWindow? "_blank" : "_self"}
+        target={newWindow ? "_blank" : "_self"}
       >
         {text}
-        {
-          iconSrc ? 
-            <LinkIcon src={iconSrc} brightness={theme} iconPosition={iconPosition}></LinkIcon>
-          : ''
-        }
-        
+        {iconSrc ? <LinkIcon src={iconSrc} iconPosition={iconPosition}></LinkIcon> : ""}
       </LinkText>
-    );
+    </ThemeProvider>
+  );
 };
 
-
 const LinkText = styled.a`
-  margin: ${props => (props.margin && typeof props.margin !== "object" ? spaces[props.margin] : "0px")};
-  margin-top: ${props =>
+  margin: ${(props) => (props.margin && typeof props.margin !== "object" ? spaces[props.margin] : "0px")};
+  margin-top: ${(props) =>
     props.margin && typeof props.margin === "object" && props.margin.top ? spaces[props.margin.top] : ""};
-  margin-right: ${props =>
+  margin-right: ${(props) =>
     props.margin && typeof props.margin === "object" && props.margin.right ? spaces[props.margin.right] : ""};
-  margin-bottom: ${props =>
+  margin-bottom: ${(props) =>
     props.margin && typeof props.margin === "object" && props.margin.bottom ? spaces[props.margin.bottom] : ""};
-  margin-left: ${props =>
+  margin-left: ${(props) =>
     props.margin && typeof props.margin === "object" && props.margin.left ? spaces[props.margin.left] : ""};
 
-  ${props => props.underlined ? 
-      `text-decoration: none;
+  ${(props) =>
+    props.underlined
+      ? !props.disabled
+        ? `text-decoration: none;
       padding-bottom: 1px !important;
-      border-bottom: 1px solid;`
-      : 
-      `text-decoration: none`};
-  
-  color: ${ props => props.disabled ? props.brightness === "light" ? "#525252" : "#959595" : !props.inheritColor ?
-            props.brightness === "light" ? "#006BF6" : "#4797FF" 
-            : props.brightness === "dark" ? "#FFFFFF" : "inherit"} !important;
-  ${ props => !props.underlined ? "text-decoration-color: transparent;" :  ""}
-  ${ props => props.disabled ? "pointer-events: none;" : ""}
+      border-bottom: 1px solid ${props.theme.underlinedBackgroundColor}`
+        : `text-decoration: none;
+      padding-bottom: 1px !important;
+      border-bottom: 1px solid ${props.theme.disabledUnderlinedBackgroundColor}`
+      : `text-decoration: none`};
+
+  color: ${(props) => (!props.disabled ? props.theme.textColor : props.theme.disabledColor)};
+  ${(props) => (!props.underlined ? "text-decoration-color: transparent;" : "")}
+  ${(props) => (props.disabled ? "pointer-events: none;" : "")}
   
   display: inline-flex;
-  flex-direction: ${ props => props.iconPosition === "after" ?  "row" : "row-reverse"};
-  justify-content: ${ props => props.iconPosition === "after" ? "flex-start" : "flex-end"};
+  flex-direction: ${(props) => (props.iconPosition === "after" ? "row" : "row-reverse")};
+  justify-content: ${(props) => (props.iconPosition === "after" ? "flex-start" : "flex-end")};
   align-items: center;
- 
+
   max-width: 100%;
   font-size: 16px;
   padding-bottom: 2px;
 
   &:hover {
-    color: ${props=> props.brightness === "light" ? "#006BF6" : "#4797FF"} !important;
+    color: ${(props) => props.theme.hoverTextColor} !important;
     text-decoration: none;
     padding-bottom: 1px !important;
     border-bottom: 1px solid;
@@ -81,39 +84,39 @@ const LinkText = styled.a`
   }
 
   &:visited {
-    ${
-      props => !props.disabled ? 
-      `color: ${props.brightness === "light" ? "#8800F6" : "#C175FF"} !important; ` : ""
-    }
+    ${(props) =>
+      props.underlined
+        ? !props.disabled
+          ? `color: ${props.theme.visitedtextColor} !important; border-bottom: 1px solid ${props.theme.visitedUnderlinedBackgroundColor}`
+          : ""
+        : ""}
   }
 `;
 
 const LinkIcon = styled.img`
   width: 16px;
   height: 16px;
-  ${ props => props.iconPosition === "before" ? "margin-right" : "margin-left"}: 6px;
+  ${(props) => (props.iconPosition === "before" ? "margin-right" : "margin-left")}: 6px;
 `;
 
 DxcLink.propTypes = {
-    underlined: PropTypes.bool,
-    inheritColor: PropTypes.bool,
-    disabled: PropTypes.bool,
-    iconSrc: PropTypes.string,
-    iconPosition: PropTypes.oneOf(["after", "before"]),
-    href: PropTypes.string,
-    theme: PropTypes.oneOf(["light", "dark", ""]),
-    margin: PropTypes.oneOfType([
-      PropTypes.shape({
-        top: PropTypes.oneOf(Object.keys(spaces)),
-        bottom: PropTypes.oneOf(Object.keys(spaces)),
-        left: PropTypes.oneOf(Object.keys(spaces)),
-        right: PropTypes.oneOf(Object.keys(spaces))
-      }),
-      PropTypes.oneOf([...Object.keys(spaces)])
-    ]),
-    newWindow: PropTypes.bool,
-    text: PropTypes.string
+  underlined: PropTypes.bool,
+  inheritColor: PropTypes.bool,
+  disabled: PropTypes.bool,
+  iconSrc: PropTypes.string,
+  iconPosition: PropTypes.oneOf(["after", "before"]),
+  href: PropTypes.string,
+  margin: PropTypes.oneOfType([
+    PropTypes.shape({
+      top: PropTypes.oneOf(Object.keys(spaces)),
+      bottom: PropTypes.oneOf(Object.keys(spaces)),
+      left: PropTypes.oneOf(Object.keys(spaces)),
+      right: PropTypes.oneOf(Object.keys(spaces)),
+    }),
+    PropTypes.oneOf([...Object.keys(spaces)]),
+  ]),
+  newWindow: PropTypes.bool,
+  text: PropTypes.string,
 };
-
 
 export default DxcLink;
