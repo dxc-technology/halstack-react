5590ab307e1ca2433ba8d241d8d903d17cc36e0d
Arrojo Lopez <rarrojolopez@dxc.com>
2020-08-27 13:45:21 +0200

    Fixes heading

--- app/src/pages/Heading.jsx
+++ app/src/pages/Heading.jsx
@@ -22,55 +22,37 @@ function App() {
           <DxcHeading text="Hello world!" level={5} />
         </div>
       </div>
-      <div style={{ background: "#000000", padding: "25px 0px 25px 0px" }}>
-        <h4 style={{color: "#FFFFFF"}}>Dark headings</h4>
-        <div className="test-case" id="heading-level1-dark">
-          <DxcHeading text="Hello world!" level={1} theme="dark" />
-        </div>
-        <div className="test-case" id="heading-level2-dark">
-          <DxcHeading text="Hello world!" level={2} theme="dark" />
-        </div>
-        <div className="test-case" id="heading-level3-dark">
-          <DxcHeading text="Hello world!" level={3} theme="dark" />
-        </div>
-        <div className="test-case" id="heading-level4-dark">
-          <DxcHeading text="Hello world!" level={4} theme="dark" />
-        </div>
-        <div className="test-case" id="heading-level5-dark">
-          <DxcHeading text="Hello world!" level={5} theme="dark" />
-        </div>
-      </div>
       <div>
         <h4>Weights</h4>
         <div className="test-case" id="heading-level1-weight-normal">
-          <DxcHeading text="Hello world!" level={1} weight="normal"/>
+          <DxcHeading text="Hello world!" level={1} weight="normal" />
         </div>
         <div className="test-case" id="heading-level1-weight-bold">
-          <DxcHeading text="Hello world!" level={1} weight="bold"/>
+          <DxcHeading text="Hello world!" level={1} weight="bold" />
         </div>
         <div className="test-case" id="heading-level2-weight-light">
-          <DxcHeading text="Hello world!" level={2} weight="light"/>
+          <DxcHeading text="Hello world!" level={2} weight="light" />
         </div>
         <div className="test-case" id="heading-level2-weight-bold">
-          <DxcHeading text="Hello world!" level={2} weight="bold"/>
+          <DxcHeading text="Hello world!" level={2} weight="bold" />
         </div>
         <div className="test-case" id="heading-level3-weight-light">
-          <DxcHeading text="Hello world!" level={3} weight="light"/>
+          <DxcHeading text="Hello world!" level={3} weight="light" />
         </div>
         <div className="test-case" id="heading-level3-weight-bold">
-          <DxcHeading text="Hello world!" level={3} weight="bold"/>
+          <DxcHeading text="Hello world!" level={3} weight="bold" />
         </div>
         <div className="test-case" id="heading-level4-weight-light">
-          <DxcHeading text="Hello world!" level={4} weight="light"/>
+          <DxcHeading text="Hello world!" level={4} weight="light" />
         </div>
         <div className="test-case" id="heading-level4-weight-bold">
-          <DxcHeading text="Hello world!" level={4} weight="bold"/>
+          <DxcHeading text="Hello world!" level={4} weight="bold" />
         </div>
         <div className="test-case" id="heading-level5-weight-light">
-          <DxcHeading text="Hello world!" level={5} weight="light"/>
+          <DxcHeading text="Hello world!" level={5} weight="light" />
         </div>
         <div className="test-case" id="heading-level5-weight-bold">
-          <DxcHeading text="Hello world!" level={5} weight="bold"/>
+          <DxcHeading text="Hello world!" level={5} weight="bold" />
         </div>
       </div>
       <div>
