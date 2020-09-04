b27a26f86e51938dc574da04838057e87a194871
Jairo Suarez Gonzalez <jsuarez81@gmail.com>
2020-09-04 11:24:48 +0200

    Changes chip and fix for resultsetTable

--- lib/src/resultsetTable/ResultsetTable.jsx
+++ lib/src/resultsetTable/ResultsetTable.jsx
@@ -42,7 +42,7 @@ const DxcResultsetTable = ({ columns, rows, itemsPerPage = 5, margin }) => {
   const [sortColumnIndex, changeSortColumnIndex] = useState("");
   const [sortOrder, changeSortOrder] = useState("asc");
 
-  const minItemsPerPageIndex = useMemo(() => getMinItemsPerPageIndex(page, itemsPerPage, page), [page]); //Merece la pena sacar a una función para usar useMemo?
+  const minItemsPerPageIndex = useMemo(() => getMinItemsPerPageIndex(page, itemsPerPage, page), [page]);
   const maxItemsPerPageIndex = useMemo(() => getMaxItemsPerPageIndex(minItemsPerPageIndex, itemsPerPage, rows, page), [
     page,
   ]);
@@ -86,6 +86,10 @@ const DxcResultsetTable = ({ columns, rows, itemsPerPage = 5, margin }) => {
     () => sortedResultset && sortedResultset.slice(minItemsPerPageIndex, maxItemsPerPageIndex + 1),
     [page, sortColumnIndex, sortOrder, rows]
   );
+  console.log('JAIRO!!');
+  console.log(minItemsPerPageIndex);
+  console.log(maxItemsPerPageIndex +);
+
   return (
     <ThemeProvider theme={colorsTheme}>
       <DxcResultsetTableContainer margin={margin}>
