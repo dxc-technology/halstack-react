dd682e4ec699d4f38e6dfa0ee515b757aa44b968
Parrilla <m.parrilla@dxc.com>
2020-11-11 12:53:08 +0100

    New tokens name and values

--- docs/src/pages/components/cdk-components/toggleGroup/Tokens.jsx
+++ docs/src/pages/components/cdk-components/toggleGroup/Tokens.jsx
@@ -11,6 +11,34 @@ const toggleGroupTokensTable = () => {
         <th>Description</th>
       </tr>
       <tr>
+        <td>color</td>
+        <td>
+          <SampleComponent color="#D9D9D9"></SampleComponent>
+        </td>
+        <td>Applies to color.</td>
+      </tr>
+      <tr>
+        <td>hoverColor</td>
+        <td>
+          <SampleComponent color="#EEEEEE"></SampleComponent>
+        </td>
+        <td>Applies to hoverColor.</td>
+      </tr>
+      <tr>
+        <td>fontColor</td>
+        <td>
+          <SampleComponent color="#000000"></SampleComponent>
+        </td>
+        <td>Applies to fontColor.</td>
+      </tr>
+      <tr>
+        <td>hoverFontColor</td>
+        <td>
+          <SampleComponent color="#000000"></SampleComponent>
+        </td>
+        <td>Applies to hoverFontColor.</td>
+      </tr>
+      <tr>
         <td>selectedColor</td>
         <td>
           <SampleComponent color="#6F2C91"></SampleComponent>
@@ -37,34 +65,6 @@ const toggleGroupTokensTable = () => {
           <SampleComponent color="#FFFFFF"></SampleComponent>
         </td>
         <td>Applies to selectedHoverFontColor.</td>
-      </tr>
-      <tr>
-        <td>unselectedColor</td>
-        <td>
-          <SampleComponent color="#F6F6F6"></SampleComponent>
-        </td>
-        <td>Applies to unselectedColor.</td>
-      </tr>
-      <tr>
-        <td>unselectedHoverColor</td>
-        <td>
-          <SampleComponent color="#EEEEEE"></SampleComponent>
-        </td>
-        <td>Applies to unselectedHoverColor.</td>
-      </tr>
-      <tr>
-        <td>unselectedFontColor</td>
-        <td>
-          <SampleComponent color="#000000"></SampleComponent>
-        </td>
-        <td>Applies to unselectedFontColor.</td>
-      </tr>
-      <tr>
-        <td>unselectedHoverFontColor</td>
-        <td>
-          <SampleComponent color="#000000"></SampleComponent>
-        </td>
-        <td>Applies to unselectedHoverFontColor.</td>
       </tr>
     </DxcTable>
   );
