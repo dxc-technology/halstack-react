559eff7cc5fea5749affccdeb5f05ede922526f6
Parrilla <m.parrilla@dxc.com>
2020-12-17 13:15:55 +0100

    Using link component in overview

--- docs/src/pages/overview/Overview.jsx
+++ docs/src/pages/overview/Overview.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { DxcTabsForSections, DxcTag, DxcHeading } from "@dxc-technology/halstack-react";
+import {
+  DxcTabsForSections,
+  DxcTag,
+  DxcHeading,
+  DxcLink,
+} from "@dxc-technology/halstack-react";
 import styled from "styled-components";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
@@ -20,14 +25,17 @@ function Install() {
 
       <p>
         If you are creating a React application from scratch, we recommend using{" "}
-        <Link target="_blank" href="https://create-react-app.dev/">
-          create-react-app
-        </Link>{" "}
+        <DxcLink
+          href="https://create-react-app.dev/"
+          inheritColor
+          newWindow
+          text="create-react-app"
+        ></DxcLink>{" "}
         to set it up with minimum configuration.
       </p>
       <p>
-        DXC React Components is distributed as an npm package. styled-compoents is a peer dependency, so in order to use it in an
-        existing project:
+        DXC React Components is distributed as an npm package. styled-compoents
+        is a peer dependency, so in order to use it in an existing project:
       </p>
       <SyntaxHighlighter language="php" style={docco}>
         {`
@@ -48,9 +56,12 @@ function UseComponents() {
       <DxcHeading level={3} text="Use Components" />
       <p>
         A list of available components is available in the{" "}
-        <Link target="_blank" href="https://developer.dxc.com/design/components">
-          components screen
-        </Link>{" "}
+        <DxcLink
+          href="https://developer.dxc.com/design/components"
+          inheritColor
+          newWindow
+          text="components screen"
+        ></DxcLink>{" "}
         . The API documentation of every component is available in that screen,
         as well as a set of examples with a live code editor.
       </p>
@@ -228,9 +239,12 @@ function Overview() {
           DXC React Components is a library of reusable elements, made with the
           purpose of helping React developers with the task of implementing User
           Interfaces that follow the{" "}
-          <Link href="https://developer.dxc.com" target="_blank">
-            DXC Design Guidelines
-          </Link>{" "}
+          <DxcLink
+            href="https://developer.dxc.com"
+            inheritColor
+            newWindow
+            text="DXC Design Guidelines"
+          ></DxcLink>{" "}
           right out of the box.
         </p>
         <ul>
@@ -313,13 +327,6 @@ const Subtitle = styled.h2`
 const LinksSection = styled.div`
   display: flex;
   margin: 70px 0px 50px 0px;
-`;
-
-const Link = styled.a`
-  font-weight: bold;
-  text-decoration: underline;
-  color: black;
-  letter-spacing: initial;
 `;
 
 const Introduction = styled.div`
