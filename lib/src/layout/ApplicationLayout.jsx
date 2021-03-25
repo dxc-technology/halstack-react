51184f34da41a516d5b02ae5155ebe55defed6ec
Jairo Suárez González <jairosuarezgonzalez@MacBook-Pro-de-Jairo.local>
2021-03-25 09:55:23 +0100

    Changing Application Layout z-index

--- lib/src/layout/ApplicationLayout.jsx
+++ lib/src/layout/ApplicationLayout.jsx
@@ -190,7 +190,7 @@ const ApplicationLayoutContainer = styled.div`
 `;
 
 const HeaderContainer = styled.div`
-  z-index: 10;
+  z-index: 20;
   position: fixed;
   top: 0;
   left: 0;
@@ -236,7 +236,7 @@ const MainContent = styled.div`
 const SideNavArrowContainer = styled.div`
   display: flex;
   flex-direction: row;
-  z-index: 2;
+  z-index: 12;
   transform: ${(props) =>
     props.isSideNavVisible ? "translateX(0)" : !props.isSideNavVisible ? "translateX(-100%)" : ""};
   transition: transform 0.4s ease-in-out;
@@ -263,7 +263,7 @@ const ArrowTrigger = styled.div`
   border-radius: 50%;
   transform: ${(props) => (props.isSideNavVisible ? "rotate(-180deg)" : "rotate(0deg)")};
   transition: transform 0.4s ease-in-out;
-  z-index: 10;
+  z-index: 20;
   cursor: pointer;
   & > svg {
     fill: ${(props) => props.theme.arrowColor};
