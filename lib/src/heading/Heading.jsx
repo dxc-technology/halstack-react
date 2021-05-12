7d560c22b76d323b09c5e999cc1a9acd97f01f95
Parrilla <m.parrilla@dxc.com>
2021-05-12 09:43:21 +0200

    [Minor] Advanced tokens in heading component

--- lib/src/heading/Heading.jsx
+++ lib/src/heading/Heading.jsx
@@ -1,24 +1,28 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import PropTypes from "prop-types";
-
-import { spaces, componentTokens } from "../common/variables.js";
+import { spaces } from "../common/variables.js";
+import useTheme from "../useTheme.js";
 
 const DxcHeading = ({ level = 1, text = "", weight = "", margin }) => {
+  const colorsTheme = useTheme();
+
   return (
-    <HeadingContainer margin={margin}>
-      {level === 1 ? (
-        <HeadingLevel1 weight={weight}>{text}</HeadingLevel1>
-      ) : level === 2 ? (
-        <HeadingLevel2 weight={weight}>{text}</HeadingLevel2>
-      ) : level === 3 ? (
-        <HeadingLevel3 weight={weight}>{text}</HeadingLevel3>
-      ) : level === 4 ? (
-        <HeadingLevel4 weight={weight}>{text}</HeadingLevel4>
-      ) : (
-        <HeadingLevel5 weight={weight}>{text}</HeadingLevel5>
-      )}
-    </HeadingContainer>
+    <ThemeProvider theme={colorsTheme.heading}>
+      <HeadingContainer margin={margin}>
+        {level === 1 ? (
+          <HeadingLevel1 weight={weight}>{text}</HeadingLevel1>
+        ) : level === 2 ? (
+          <HeadingLevel2 weight={weight}>{text}</HeadingLevel2>
+        ) : level === 3 ? (
+          <HeadingLevel3 weight={weight}>{text}</HeadingLevel3>
+        ) : level === 4 ? (
+          <HeadingLevel4 weight={weight}>{text}</HeadingLevel4>
+        ) : (
+          <HeadingLevel5 weight={weight}>{text}</HeadingLevel5>
+        )}
+      </HeadingContainer>
+    </ThemeProvider>
   );
 };
 
@@ -32,74 +36,87 @@ const HeadingContainer = styled.div`
     props.margin && typeof props.margin === "object" && props.margin.bottom ? spaces[props.margin.bottom] : ""};
   margin-left: ${(props) =>
     props.margin && typeof props.margin === "object" && props.margin.left ? spaces[props.margin.left] : ""};
-  font-size: ${componentTokens?.heading?.fontSizeBase};
-
-  font-family: ${componentTokens?.heading?.fontFamily};
-  color: inherit;
+  font-family: ${(props) => props.theme.fontFamilyBase};
+  color: ${(props) => props.theme.fontColorBase};
 `;
 
 const HeadingLevel1 = styled.h1`
-  font-size: ${componentTokens?.heading?.fontSize60};
-  line-height: ${componentTokens?.heading?.fontHeightLevelCompact};
+  font-family: ${(props) => props.theme.level1FontFamily};
+  font-style: ${(props) => props.theme.level1FontStyle};
+  font-size: ${(props) => props.theme.level1FontSize};
+  line-height: ${(props) => props.theme.level1LineHeight};
   font-weight: ${(props) =>
-    props.weight === "light" || props.weight === ""
-      ? componentTokens?.heading?.fontWeightLight
-      : props.weight === "normal"
-      ? componentTokens?.heading?.fontWeight400
-      : componentTokens?.heading?.fontWeight600};
-  letter-spacing: ${componentTokens?.heading?.fontLetterSpacingTight01};
+    props.weight === "normal" || props.weight === ""
+      ? props.theme.level1NormalFontWeight
+      : props.weight === "light"
+      ? props.theme.level1LightFontWeight
+      : props.theme.level1BoldFontWeight};
+  letter-spacing: ${(props) => props.theme.level1LetterSpacing};
+  color: ${(props) => props.theme.level1FontColor};
   margin: 0;
 `;
 
 const HeadingLevel2 = styled.h2`
-  font-size: ${componentTokens?.heading?.fontSize48};
-  line-height: ${componentTokens?.heading?.fontHeightLevelNormal};
+  font-family: ${(props) => props.theme.level2FontFamily};
+  font-style: ${(props) => props.theme.level2FontStyle};
+  font-size: ${(props) => props.theme.level2FontSize};
+  line-height: ${(props) => props.theme.level2LineHeight};
   font-weight: ${(props) =>
     props.weight === "normal" || props.weight === ""
-      ? componentTokens?.heading?.fontWeight400
+      ? props.theme.level2NormalFontWeight
       : props.weight === "light"
-      ? componentTokens?.heading?.fontWeightLight
-      : componentTokens?.heading?.fontWeight600};
-  letter-spacing: ${componentTokens?.heading?.type_spacing_normal};
+      ? props.theme.level2LightFontWeight
+      : props.theme.level2BoldFontWeight};
+  letter-spacing: ${(props) => props.theme.level2LetterSpacing};
+  color: ${(props) => props.theme.level2FontColor};
   margin: 0;
 `;
 
 const HeadingLevel3 = styled.h3`
-  font-size: ${componentTokens?.heading?.fontSize34};
-  line-height: ${componentTokens?.heading?.fontHeightLevelCompact};
+  font-family: ${(props) => props.theme.level3FontFamily};
+  font-style: ${(props) => props.theme.level3FontStyle};
+  font-size: ${(props) => props.theme.level3FontSize};
+  line-height: ${(props) => props.theme.level3LineHeight};
   font-weight: ${(props) =>
     props.weight === "normal" || props.weight === ""
-      ? componentTokens?.heading?.fontWeight400
+      ? props.theme.level3NormalFontWeight
       : props.weight === "light"
-      ? componentTokens?.heading?.fontWeightLight
-      : componentTokens?.heading?.fontWeight600};
-  letter-spacing: ${componentTokens?.heading?.type_spacing_wide_01};
+      ? props.theme.level3LightFontWeight
+      : props.theme.level3BoldFontWeight};
+  letter-spacing: ${(props) => props.theme.level3LetterSpacing};
+  color: ${(props) => props.theme.level3FontColor};
   margin: 0;
 `;
 
 const HeadingLevel4 = styled.h4`
-  font-size: ${componentTokens?.heading?.fontSize24};
-  line-height: ${componentTokens?.heading?.fontHeightLevelNormal};
+  font-family: ${(props) => props.theme.level4FontFamily};
+  font-style: ${(props) => props.theme.level4FontStyle};
+  font-size: ${(props) => props.theme.level4FontSize};
+  line-height: ${(props) => props.theme.level4LineHeight};
   font-weight: ${(props) =>
     props.weight === "normal" || props.weight === ""
-      ? componentTokens?.heading?.fontWeight400
+      ? props.theme.level4NormalFontWeight
       : props.weight === "light"
-      ? componentTokens?.heading?.fontWeightLight
-      : componentTokens?.heading?.fontWeight600};
-  letter-spacing: ${componentTokens?.heading?.type_spacing_normal};
+      ? props.theme.level4LightFontWeight
+      : props.theme.level4BoldFontWeight};
+  letter-spacing: ${(props) => props.theme.level4LetterSpacing};
+  color: ${(props) => props.theme.level4FontColor};
   margin: 0;
 `;
 
 const HeadingLevel5 = styled.h5`
-  font-size: ${componentTokens?.heading?.fontSize20};
-  line-height: ${componentTokens?.heading?.fontHeightLevelNormal};
+  font-family: ${(props) => props.theme.level5FontFamily};
+  font-style: ${(props) => props.theme.level5FontStyle};
+  font-size: ${(props) => props.theme.level5FontSize};
+  line-height: ${(props) => props.theme.level5LineHeight};
   font-weight: ${(props) =>
     props.weight === "normal" || props.weight === ""
-      ? componentTokens?.heading?.fontWeight400
+      ? props.theme.level5NormalFontWeight
       : props.weight === "light"
-      ? componentTokens?.heading?.fontWeightLight
-      : componentTokens?.heading?.fontWeight600};
-  letter-spacing: ${componentTokens?.heading?.type_spacing_wide_01};
+      ? props.theme.level5LightFontWeight
+      : props.theme.level5BoldFontWeight};
+  letter-spacing: ${(props) => props.theme.level5LetterSpacing};
+  color: ${(props) => props.theme.level5FontColor};
   margin: 0;
 `;
 
