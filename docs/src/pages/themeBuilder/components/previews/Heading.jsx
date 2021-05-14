37af269319e55d855939f5c6100882e6317dc86e
Parrilla <m.parrilla@dxc.com>
2021-05-14 09:17:35 +0200

    [Minor] Theme builder update with new tokens

--- docs/src/pages/themeBuilder/components/previews/Heading.jsx
+++ docs/src/pages/themeBuilder/components/previews/Heading.jsx
@@ -10,77 +10,32 @@ const Heading = () => {
       <Mode text="Level 1">
         <DxcHeading
           level={1}
-          weight="light"
-          text="Main title"
-          margin={{ right: "small" }}
+          text="Title for main section"
         />
-        <DxcHeading
-          level={1}
-          weight="normal"
-          text="Main title"
-          margin={{ right: "small" }}
-        />
-        <DxcHeading level={1} weight="bold" text="Main title" />
       </Mode>
       <Mode text="Level 2">
         <DxcHeading
           level={2}
-          weight="light"
           text="Heading for sections"
-          margin={{ right: "small" }}
         />
-        <DxcHeading
-          level={2}
-          weight="normal"
-          text="Heading for sections"
-          margin={{ right: "small" }}
-        />
-        <DxcHeading level={2} weight="bold" text="Heading for sections" />
       </Mode>
       <Mode text="Level 3">
         <DxcHeading
           level={3}
-          weight="light"
-          text="Subtitle"
-          margin={{ right: "small" }}
+          text="Subtitle introducing a section"
         />
-        <DxcHeading
-          level={3}
-          weight="normal"
-          text="Subtitle"
-          margin={{ right: "small" }}
-        />
-        <DxcHeading level={3} weight="bold" text="Subtitle" />
       </Mode>
       <Mode text="Level 4">
         <DxcHeading
           level={4}
-          weight="light"
           text="Heading for a paragraph"
-          margin={{ right: "small" }}
         />
-        <DxcHeading
-          level={4}
-          weight="normal"
-          text="Heading for a paragraph"
-          margin={{ right: "small" }}
-        />
-        <DxcHeading level={4} weight="bold" text="Heading for a paragraph" />
       </Mode>
       <Mode text="Level 5">
         <DxcHeading
           level={5}
-          weight="light"
-          text="Heading to mix"
-          margin={{ right: "small" }}
+          text="Heading to mix with the rest of the headings"
         />
-        <DxcHeading
-          level={5}
-          weight="normal"
-          text="Heading to mix"
-          margin={{ right: "small" }}
-        />
-        <DxcHeading level={5} weight="bold" text="Heading to mix" />
       </Mode>
     </HeadingContainer>
   );
