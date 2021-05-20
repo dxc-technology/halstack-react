98c9ae1e21ea647c8e37c8bfd6242ca61c6afb4c
GomezIvann <UO258748@uniovi.es>
2021-05-20 17:58:12 +0200

    [Minor] Advanced tokens for Slider component

--- docs/src/pages/themeBuilder/components/previews/Slider.jsx
+++ docs/src/pages/themeBuilder/components/previews/Slider.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import { DxcSlider } from "@dxc-technology/halstack-react";
-
+import { DxcSlider, DxcHeading } from "@dxc-technology/halstack-react";
 import Mode from "../Mode";
+import { BackgroundColorProvider } from "@dxc-technology/halstack-react/dist/BackgroundColorContext";
 
 const Slider = () => {
-  
+  const [value, changeValue] = useState(0);
+  const onChange = newValue => {
+    changeValue(newValue);
+  };
+  const onChangeInput = (newValue) => {
+    changeValue(newValue);
+  };
+
   return (
     <SliderContainer>
       <Mode text="Default">
@@ -15,17 +22,6 @@ const Slider = () => {
           showLimitsValues={true}
           name="input"
           step={1}
-          margin="medium"
-        />
-      </Mode>
-      <Mode text="With marks">
-        <DxcSlider
-          minValue={0}
-          maxValue={100}
-          showLimitsValues={true}
-          name="input"
-          step={5}
-          marks={true}
           margin="medium"
         />
       </Mode>
@@ -41,6 +37,87 @@ const Slider = () => {
           margin="medium"
         />
       </Mode>
+      <Mode text="With marks">
+        <DxcSlider
+          minValue={0}
+          maxValue={100}
+          showLimitsValues={true}
+          name="input"
+          step={5}
+          marks={true}
+          margin="medium"
+        />
+      </Mode>
+      <Mode text="With input">
+        <DxcSlider
+          minValue={0}
+          maxValue={50}
+          showLimitsValues={true}
+          value={value}
+          showInput={true}
+          name="input"
+          step={5}
+          marks={true}
+          onChange={onChange}
+          onChangeInput={onChangeInput}
+          margin="medium"
+        />
+      </Mode>
+      <DxcHeading
+        text="Dark Mode"
+        level={5}
+        margin={{ top: "xsmall", bottom: "xxsmall" }}
+      />
+      <BackgroundColorProvider color="#000000">
+        <Mode mode="dark" text="Default">
+          <DxcSlider
+            minValue={0}
+            maxValue={100}
+            showLimitsValues={true}
+            name="input"
+            step={1}
+            margin="medium"
+          />
+        </Mode>
+        <Mode mode="dark" text="Disabled">
+          <DxcSlider
+            minValue={0}
+            maxValue={100}
+            showLimitsValues={true}
+            disabled
+            marks={true}
+            name="input"
+            step={10}
+            margin="medium"
+          />
+        </Mode>
+        <Mode mode="dark" text="With marks">
+          <DxcSlider
+            minValue={0}
+            maxValue={100}
+            showLimitsValues={true}
+            name="input"
+            step={5}
+            marks={true}
+            margin="medium"
+          />
+        </Mode>
+        <Mode mode="dark" text="With input">
+          <DxcSlider
+            minValue={0}
+            maxValue={50}
+            showLimitsValues={true}
+            value={value}
+            showInput={true}
+            name="input"
+            step={5}
+            marks={true}
+            onChange={onChange}
+            onChangeInput={onChangeInput}
+            margin="medium"
+          />
+        </Mode>
+      </BackgroundColorProvider>
     </SliderContainer>
   );
 };
