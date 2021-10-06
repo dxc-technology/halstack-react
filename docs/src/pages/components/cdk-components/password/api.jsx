cb29c281b433d7dcb31df6bf3ff40ebc3b808bad
GomezIvann <UO258748@uniovi.es>
2021-10-06 10:44:32 +0200

    [Minor] Clearable for date, doc improvements & error icon container fixed

--- docs/src/pages/components/cdk-components/password/api.jsx
+++ docs/src/pages/components/cdk-components/password/api.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { DxcTable } from "@dxc-technology/halstack-react";
 
-const passwordPropsTable = () => {
+const PasswordPropsTable = () => {
   return (
     <DxcTable>
       <tr>
@@ -13,7 +13,7 @@ const passwordPropsTable = () => {
         <td>value: string</td>
         <td></td>
         <td>
-          Value of the password. If undefined, the component will be
+          Value of the input element. If undefined, the component will be
           uncontrolled and the value will be managed internally by the
           component.
         </td>
@@ -26,7 +26,7 @@ const passwordPropsTable = () => {
       <tr>
         <td>name: string</td>
         <td></td>
-        <td>Name attribute of the password element.</td>
+        <td>Name attribute of the input element.</td>
       </tr>
       <tr>
         <td>helperText: string</td>
@@ -34,62 +34,51 @@ const passwordPropsTable = () => {
         <td>Helper text to be placed above the password.</td>
       </tr>
       <tr>
-        <td>error: string</td>
-        <td></td>
-        <td>
-          If it is defined, the component will change its appearance, showing
-          the error below the password component. If it is not defined, the
-          error messages will be created internally by the component.
-        </td>
-      </tr>
-      <tr>
         <td>clearable: boolean</td>
         <td>
           <code>false</code>
         </td>
         <td>
-          If true, the input will have an action to clear the value entered in
-          the password.
+          If true, the password will have an action to clear the entered value.
         </td>
       </tr>
       <tr>
         <td>onChange: function</td>
         <td></td>
         <td>
-          This function will be called when the user types within the password.
-          The new value will be passed as a parameter.
+          This function will be called when the user types within the input
+          element of the component. The new value will be passed as a parameter.
         </td>
       </tr>
       <tr>
         <td>onBlur: function</td>
         <td></td>
         <td>
-          This function will be called when the password loses the focus. An
-          object including the value and the error (if the value entered is not
-          valid) will be passed to this function. An example of this object is:{" "}
-          {"{ "}
+          This function will be called when the input element loses the focus.
+          An object including the value and the error (if the value entered is
+          not valid) will be passed to this function. An example of this object
+          is: {"{ "}
           <code>value: value, error: error</code>
           {" }"}. If there is no error, error will be null.
         </td>
       </tr>
       <tr>
-        <td>margin: function</td>
+        <td>error: string</td>
         <td></td>
         <td>
-          Size of the margin to be applied to the component ('xxsmall' |
-          'xsmall' | 'small' | 'medium' | 'large' | 'xlarge' | 'xxlarge'). You
-          can pass an object with 'top', 'bottom', 'left' and 'right' properties
-          in order to specify different margin sizes.
+          If it is defined, the component will change its appearance, showing
+          the error below the password component. If it is not defined, the
+          error messages will be created and managed internally.
         </td>
       </tr>
       <tr>
-        <td>pattern: string </td>
+        <td>pattern: string</td>
         <td></td>
         <td>
           Regular expression that defines the valid format allowed by the
-          password. This will be checked when the password loses the focus. If
-          the value entered does not match the pattern, the onBlur function will
-          be called with the value entered and the error informing that the
+          password. This will be checked when the input element loses the focus.
+          If the value entered does not match the pattern, the onBlur function
+          will be called with the value entered and the error informing that the
           value does not match the pattern as parameters. If the pattern is
           accomplished, the error parameter will be null.
         </td>
@@ -106,6 +95,16 @@ const passwordPropsTable = () => {
           called with the value entered and the error informing that the value
           does not comply the length as parameters. If the length is
           accomplished, the error parameter will be null.
+        </td>
+      </tr>
+      <tr>
+        <td>margin: string | object</td>
+        <td></td>
+        <td>
+          Size of the margin to be applied to the component ('xxsmall' |
+          'xsmall' | 'small' | 'medium' | 'large' | 'xlarge' | 'xxlarge'). You
+          can pass an object with 'top', 'bottom', 'left' and 'right' properties
+          in order to specify different margin sizes.
         </td>
       </tr>
       <tr>
@@ -127,10 +126,10 @@ const passwordPropsTable = () => {
       <tr>
         <td>ref: object</td>
         <td></td>
-        <td>Reference to the password.</td>
+        <td>Reference to the component.</td>
       </tr>
     </DxcTable>
   );
 };
 
-export default passwordPropsTable;
+export default PasswordPropsTable;
