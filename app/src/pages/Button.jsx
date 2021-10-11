dd831e151cdef3afdb6fdcd0acb9d271dc9d97dd
raquelarrojo <arrojoraquel@gmail.com>
2021-10-11 09:31:45 +0200

    [Patch] Fix #385 Review button specs

--- app/src/pages/Button.jsx
+++ app/src/pages/Button.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { DxcButton, ThemeProvider } from "@dxc-technology/halstack-react";
+import {
+  DxcButton,
+  ThemeProvider,
+  BackgroundColorProvider,
+} from "@dxc-technology/halstack-react";
+import styled from "styled-components";
 import homeLogo from "../images/home.svg";
 
 const colors = {
@@ -54,6 +59,84 @@ function App() {
           disabled
           size="large"
           margin="small"
+        />
+      </div>
+      <div>
+        <h4>Primary - only icon</h4>
+        <DxcButton
+          mode="primary"
+          onClick={onClick}
+          icon={
+            <svg
+              x="0px"
+              y="0px"
+              width="24px"
+              height="24px"
+              viewBox="0 0 24 24"
+              enable-background="new 0 0 24 24"
+              fill="currentColor"
+            >
+              <g id="Bounding_Box">
+                <rect fill="none" width="24" height="24" />
+              </g>
+              <g id="Master">
+                <path d="M19,9.3V4h-3v2.6L12,3L2,12h3v8h5v-6h4v6h5v-8h3L19,9.3z M10,10c0-1.1,0.9-2,2-2s2,0.9,2,2H10z" />
+              </g>
+            </svg>
+          }
+          margin="xsmall"
+        />
+      </div>
+      <div>
+        <h4>Secondary - only icon</h4>
+        <DxcButton
+          mode="secondary"
+          onClick={onClick}
+          icon={
+            <svg
+              x="0px"
+              y="0px"
+              width="24px"
+              height="24px"
+              viewBox="0 0 24 24"
+              enable-background="new 0 0 24 24"
+              fill="currentColor"
+            >
+              <g id="Bounding_Box">
+                <rect fill="none" width="24" height="24" />
+              </g>
+              <g id="Master">
+                <path d="M19,9.3V4h-3v2.6L12,3L2,12h3v8h5v-6h4v6h5v-8h3L19,9.3z M10,10c0-1.1,0.9-2,2-2s2,0.9,2,2H10z" />
+              </g>
+            </svg>
+          }
+          margin="xsmall"
+        />
+      </div>
+      <div>
+        <h4>Text - only icon</h4>
+        <DxcButton
+          mode="text"
+          onClick={onClick}
+          icon={
+            <svg
+              x="0px"
+              y="0px"
+              width="24px"
+              height="24px"
+              viewBox="0 0 24 24"
+              enable-background="new 0 0 24 24"
+              fill="currentColor"
+            >
+              <g id="Bounding_Box">
+                <rect fill="none" width="24" height="24" />
+              </g>
+              <g id="Master">
+                <path d="M19,9.3V4h-3v2.6L12,3L2,12h3v8h5v-6h4v6h5v-8h3L19,9.3z M10,10c0-1.1,0.9-2,2-2s2,0.9,2,2H10z" />
+              </g>
+            </svg>
+          }
+          margin="xsmall"
         />
       </div>
       <div>
@@ -478,8 +561,161 @@ function App() {
           </div>
         </ThemeProvider>
       </div>
+      <BackgroundColorProvider color="#000000">
+        <Mode mode="dark" text="Default">
+          <DxcButton
+            mode="primary"
+            label="Custom Button"
+            onClick={onClick}
+            size="large"
+            margin="small"
+          />
+        </Mode>
+        <Mode mode="dark" text="Primary">
+          <DxcButton
+            mode="primary"
+            onClick={onClick}
+            icon={
+              <svg
+                x="0px"
+                y="0px"
+                width="24px"
+                height="24px"
+                viewBox="0 0 24 24"
+                enable-background="new 0 0 24 24"
+                fill="currentColor"
+              >
+                <g id="Bounding_Box">
+                  <rect fill="none" width="24" height="24" />
+                </g>
+                <g id="Master">
+                  <path d="M19,9.3V4h-3v2.6L12,3L2,12h3v8h5v-6h4v6h5v-8h3L19,9.3z M10,10c0-1.1,0.9-2,2-2s2,0.9,2,2H10z" />
+                </g>
+              </svg>
+            }
+            margin="xsmall"
+          />
+        </Mode>
+        <Mode mode="dark" text="Primary">
+          <DxcButton
+            mode="primary"
+            label="Custom Button"
+            onClick={onClick}
+            size="large"
+            margin="small"
+            disabled
+          />
+        </Mode>
+        <Mode mode="dark" text="Secondary">
+          <DxcButton
+            mode="secondary"
+            label="Custom Button"
+            onClick={onClick}
+            size="large"
+            margin="small"
+          />
+        </Mode>
+        <Mode mode="dark" text="Secondary">
+          <DxcButton
+            mode="secondary"
+            onClick={onClick}
+            icon={
+              <svg
+                x="0px"
+                y="0px"
+                width="24px"
+                height="24px"
+                viewBox="0 0 24 24"
+                enable-background="new 0 0 24 24"
+                fill="currentColor"
+              >
+                <g id="Bounding_Box">
+                  <rect fill="none" width="24" height="24" />
+                </g>
+                <g id="Master">
+                  <path d="M19,9.3V4h-3v2.6L12,3L2,12h3v8h5v-6h4v6h5v-8h3L19,9.3z M10,10c0-1.1,0.9-2,2-2s2,0.9,2,2H10z" />
+                </g>
+              </svg>
+            }
+            margin="xsmall"
+          />
+        </Mode>
+        <Mode mode="dark" text="Secondary">
+          <DxcButton
+            mode="secondary"
+            label="Custom Button"
+            onClick={onClick}
+            size="large"
+            margin="small"
+            disabled
+          />
+        </Mode>
+        <Mode mode="dark" text="Text">
+          <DxcButton
+            mode="text"
+            label="Custom Button"
+            onClick={onClick}
+            size="large"
+            margin="small"
+          />
+        </Mode>
+        <Mode mode="dark" text="Text">
+          <DxcButton
+            mode="text"
+            onClick={onClick}
+            icon={
+              <svg
+                x="0px"
+                y="0px"
+                width="24px"
+                height="24px"
+                viewBox="0 0 24 24"
+                enable-background="new 0 0 24 24"
+                fill="currentColor"
+              >
+                <g id="Bounding_Box">
+                  <rect fill="none" width="24" height="24" />
+                </g>
+                <g id="Master">
+                  <path d="M19,9.3V4h-3v2.6L12,3L2,12h3v8h5v-6h4v6h5v-8h3L19,9.3z M10,10c0-1.1,0.9-2,2-2s2,0.9,2,2H10z" />
+                </g>
+              </svg>
+            }
+            margin="xsmall"
+          />
+        </Mode>
+        <Mode mode="dark" text="Text">
+          <DxcButton
+            mode="text"
+            label="Custom Button"
+            onClick={onClick}
+            size="large"
+            margin="small"
+            disabled
+          />
+        </Mode>
+      </BackgroundColorProvider>
     </div>
   );
 }
+
+const Mode = ({ mode, children }) => {
+  return (
+    <ModeContainer mode={mode}>
+      <PreviewsContainer>{children}</PreviewsContainer>
+    </ModeContainer>
+  );
+};
+
+const ModeContainer = styled.div`
+  background-color: ${(props) =>
+    props.mode === "dark" ? "#000000" : "transparent"};
+  display: flex;
+  flex-flow: row wrap;
+`;
+
+const PreviewsContainer = styled.div`
+  flex: 100%;
+`;
 
 export default App;
