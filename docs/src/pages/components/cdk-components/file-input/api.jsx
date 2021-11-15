e5fe37c7854a555039aa216e83e3aacc4cf4ddd6
raquelarrojo <arrojoraquel@gmail.com>
2021-11-15 12:48:10 +0100

    Change in file input's documentation

--- docs/src/pages/components/cdk-components/file-input/api.jsx
+++ docs/src/pages/components/cdk-components/file-input/api.jsx
@@ -102,7 +102,9 @@ const fileInputPropsTable = () => {
           <td></td>
           <td>
             This function will be called when the user selects or drops a file.
-            The list of files will be sent as a parameter.
+            The list of files will be sent as a parameter. In this function, the
+            files can be updated or returned as they come. This files must be
+            passed to the value in order to be shown.
           </td>
         </tr>
         <tr>
