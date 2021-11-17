47777f92dfc6dd7fa7960b32cee0b95a53daf453
raquelarrojo <arrojoraquel@gmail.com>
2021-11-17 12:45:04 +0100

    [Patch] Added default theme to file input

--- lib/src/file-input/FileInput.jsx
+++ lib/src/file-input/FileInput.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import styled, { ThemeProvider } from "styled-components";
