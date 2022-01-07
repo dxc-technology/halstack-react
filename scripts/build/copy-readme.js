098cc84e7afd674a69d28a5b424bc67ab69c70c1
Jesús Suárez Ardid <jesussuarezardid@gmail.com>
2022-01-07 14:45:58 +0100

    Move dist folder to root

--- scripts/build/copy-readme.js
+++ scripts/build/copy-readme.js
@@ -1,3 +1,3 @@
 const fs = require('fs');
 
-fs.createReadStream('../README.md').pipe(fs.createWriteStream('./README.md'));
+fs.createReadStream('./package.json').pipe(fs.createWriteStream('../dist/package.json'));
