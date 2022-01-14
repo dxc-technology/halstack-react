25e2f61b680cbadbb5fab11bbd7f20531562b064
Jairo Suárez González <jairosuarezgonzalez@MacBook-Pro-de-Jairo.local>
2022-01-14 13:53:31 +0100

    Types added to the button component

--- lib/src/button/index.d.ts
+++ lib/src/button/index.d.ts
@@ -6,18 +6,55 @@ type Margin = {
   left?: Space;
   right?: Space;
 };
+type SVG = string | (HTMLElement & SVGElement);
+
 
 type Props = {
+  /**
+   * Text to be placed next to the button.
+   */
   label?: string;
+  /**
+   * Uses one of the available button modes.
+   */
   mode?: "primary" | "secondary" | "text";
+  /**
+   * If true, the component will be disabled.
+   */
   disabled?: boolean;
+  /**
+   * Whether the icon should appear after or before the label.
+   */
   iconPosition?: "before" | "after";
+  /**
+   * This prop corresponds to the 'type' prop of the button in html.
+   */
   type?: "button" | "reset" | "submit";
+
+  /**
+   * DEPRECATED. URL of the icon that will be placed next to the button label.
+   */
   iconSrc?: string;
-  icon?: any;
+  /**
+   * Element used as the icon that will be placed next to the button label.
+   */
+  icon?: SVG;
+  /**
+   * This function will be called when the user clicks the button. The event object will be passed as a parameter.
+   */
   onClick?: void;
+  /**
+   * Size of the margin to be applied to the component ('xxsmall' | 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge' | 'xxlarge'). 
+   * You can pass an object with 'top', 'bottom', 'left' and 'right' properties in order to specify different margin sizes.
+   */
   margin?: Space | Margin;
+  /**
+   * Size of the component ('small' | 'medium' | 'large' | 'fillParent' | 'fitContent').
+   */
   size?: Size;
+  /**
+   * Value of the tabindex.
+   */
   tabIndex?: number;
 };
 
