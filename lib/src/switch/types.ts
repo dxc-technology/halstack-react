7fcd8debcef740e68c8417b7fe5a6d07560fc79c
GomezIvann <UO258748@uniovi.es>
2022-01-19 14:36:57 +0100

    [Minor] Adding types to Switch component

---
+++ lib/src/switch/types.ts
@@ -0,0 +1,57 @@
+type Space = "xxsmall" | "xsmall" | "small" | "medium" | "large" | "xlarge" | "xxlarge";
+type Margin = {
+  top?: Space;
+  bottom?: Space;
+  left?: Space;
+  right?: Space;
+};
+
+type Props = {
+  /**
+   * If true, the component is checked. If undefined, the component will be uncontrolled and the value will be managed internally by the component.
+   */
+  checked?: boolean;
+  /**
+   * Will be passed to the value attribute of the html input element. When inside a form, this value will be only submitted if the switch is checked.
+   */
+  value?: string;
+  /**
+   * Text to be placed next to the switch.
+   */
+  label?: string;
+  /**
+   * Whether the label should appear after or before the switch.
+   */
+  labelPosition?: "before" | "after";t
+  /**
+   * Name attribute of the input element.
+   */
+  name?: string,
+  /**
+   * If true, the component will be disabled.
+   */
+  disabled?: boolean;
+  /**
+   * This function will be called when the user clicks the radio. The new value will be passed as a parameter.
+   */
+  onChange?: (checked: boolean) => void;
+  /**
+   * If true, the switch will change its appearence, showing that the value is required.
+   */
+  required?: boolean;
+  /**
+   * Size of the margin to be applied to the component ('xxsmall' | 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge' | 'xxlarge'). 
+   * You can pass an object with 'top', 'bottom', 'left' and 'right' properties in order to specify different margin sizes.
+   */
+  margin?: Space | Margin;
+  /**
+   * Size of the component ('small' | 'medium' | 'large' | 'fillParent' | 'fitContent').
+   */
+  size?: "small" | "medium" | "large" | "fillParent" | "fitContent";
+  /**
+   * Value of the tabindex.
+   */
+  tabIndex?: number;
+};
+
+export default Props;
