62c1e22d69e06e205c14ef6d5a4bf3455d230aca
agonzalez97 <aidamaria.gonzalez@dxc.com>
2022-02-01 15:30:30 +0100

    [Minor] Types for Table component

---
+++ lib/src/table/types.ts
@@ -0,0 +1,22 @@
+type Space = "xxsmall" | "xsmall" | "small" | "medium" | "large" | "xlarge" | "xxlarge";
+type Margin = {
+  top?: Space;
+  bottom?: Space;
+  left?: Space;
+  right?: Space;
+};
+
+type Props = {
+  /**
+   * Size of the margin to be applied to the component ('xxsmall' | 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge' | 'xxlarge').
+   * You can pass an object with 'top', 'bottom', 'left' and 'right' properties in order to specify different margin sizes.
+   */
+  margin?: Space | Margin;
+  /**
+   * The center section of the table. Can be used to render custom
+   * content in this area.
+   */
+  children?: React.ReactNode;
+};
+
+export default Props;
