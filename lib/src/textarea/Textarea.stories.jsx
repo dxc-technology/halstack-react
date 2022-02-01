77e3c4eea988db1990734382624be6b375d79594
Jairo Suárez González <jairosuarezgonzalez@MacBook-Pro-de-Jairo.local>
2022-02-01 15:12:51 +0100

    Added tests for margins, disabled labels (Optional and value) and for 'manual' grow

--- lib/src/textarea/Textarea.stories.jsx
+++ lib/src/textarea/Textarea.stories.jsx
@@ -23,7 +23,17 @@ export const Chromatic = () => (
       </ExampleContainer>
       <ExampleContainer>
         <Title title="Disabled" theme="light" level={4} />
-        <DxcTextarea label="Disabled" helperText="Sample text" placeholder="Enter your text here..." disabled />
+        <DxcTextarea
+          label="Disabled"
+          optional
+          helperText="Sample text"
+          placeholder="Enter your text here..."
+          disabled
+        />
+      </ExampleContainer>
+      <ExampleContainer>
+        <Title title="Disabled with value" theme="light" level={4} />
+        <DxcTextarea label="Disabled" value="Example text" disabled />
       </ExampleContainer>
       <ExampleContainer>
         <Title title="With error" theme="light" level={4} />
@@ -35,56 +45,39 @@ export const Chromatic = () => (
         />
       </ExampleContainer>
       <ExampleContainer>
-        <Title title="With helper text" theme="light" level={4} />
-        <DxcTextarea label="Fill parent" helperText="Sample text" />
-      </ExampleContainer>
-      <ExampleContainer>
-        <Title title="Optional" theme="light" level={4} />
-        <DxcTextarea label="Optional" optional />
-      </ExampleContainer>
-      <ExampleContainer>
-        <Title title="Helper text and optional" theme="light" level={4} />
-        <DxcTextarea label="Helper & optional" helperText="Sample text" optional />
+        <Title title="Helper text and optional with value" theme="light" level={4} />
+        <DxcTextarea label="Helper & optional" value="Some text" helperText="Sample text" optional />
       </ExampleContainer>
       <ExampleContainer>
         <Title title="Resizable" theme="light" level={4} />
         <DxcTextarea label="With resizer" helperText="Helper text" verticalGrow="manual" />
       </ExampleContainer>
       <ExampleContainer>
-        <Title title="With value" theme="light" level={4} />
-        <DxcTextarea label="Textarea with value" value="Example text" helperText="Sample text" />
-      </ExampleContainer>
-      <Title title="Sizes" theme="light" level={2} />
-      <ExampleContainer>
-        <DxcTextarea label="Small" size="small" />
-      </ExampleContainer>
-      <ExampleContainer>
-        <DxcTextarea label="Medium" size="medium" />
-      </ExampleContainer>
-      <ExampleContainer>
-        <DxcTextarea label="Large" size="large" />
-      </ExampleContainer>
-      <ExampleContainer>
-        <DxcTextarea label="Fill parent" size="fillParent" />
+        <Title title="Grow manual" theme="light" level={4} />
+        <DxcTextarea
+          label="Manual vertical grow"
+          verticalGrow="manual"
+          value="Long textttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt"
+        />
       </ExampleContainer>
     </>
     <BackgroundColorProvider color="#333333">
       <DarkContainer>
         <>
-          <Title title="Helper text and optional" theme="dark" level={4} />
-          <DxcTextarea label="Helper & optional" margin="medium" helperText="Sample text" optional />
+          <Title title="Helper text and optional with value" theme="dark" level={4} />
+          <DxcTextarea label="Helper & optional" value="Some text" margin="medium" helperText="Sample text" optional />
 
           <Title title="Disabled" theme="dark" level={4} />
           <DxcTextarea
             label="Disabled"
             margin="medium"
+            optional
             helperText="Sample text"
             placeholder="Enter your text here..."
             disabled
           />
-          <Title title="With value" theme="dark" level={4} />
-          <DxcTextarea label="Textarea with value" margin="medium" value="Example text" helperText="Sample text" />
-
+          <Title title="Disabled with value" theme="dark" level={4} />
+          <DxcTextarea label="Disabled" margin="medium" value="Example text" disabled />
           <Title title="With error" theme="dark" level={4} />
           <DxcTextarea
             label="Textarea with error"
@@ -96,5 +89,47 @@ export const Chromatic = () => (
         </>
       </DarkContainer>
     </BackgroundColorProvider>
+    <Title title="Sizes" theme="light" level={2} />
+    <ExampleContainer>
+      <DxcTextarea label="Small" size="small" />
+    </ExampleContainer>
+    <ExampleContainer>
+      <DxcTextarea label="Medium" size="medium" />
+    </ExampleContainer>
+    <ExampleContainer>
+      <DxcTextarea label="Large" size="large" />
+    </ExampleContainer>
+    <ExampleContainer>
+      <DxcTextarea label="Fill parent" size="fillParent" />
+    </ExampleContainer>
+    <Title title="Margins" theme="light" level={2} />
+    <ExampleContainer>
+      <Title title="Xxsmall margin" theme="light" level={4} />
+      <DxcTextarea label="Xxsmmall" margin="xxsmall" />
+    </ExampleContainer>
+    <ExampleContainer>
+      <Title title="Xsmall margin" theme="light" level={4} />
+      <DxcTextarea label="xsmmall" margin="xsmall" />
+    </ExampleContainer>
+    <ExampleContainer>
+      <Title title="Small margin" theme="light" level={4} />
+      <DxcTextarea label="smmall" margin="small" />
+    </ExampleContainer>
+    <ExampleContainer>
+      <Title title="Medium margin" theme="light" level={4} />
+      <DxcTextarea label="medium" margin="medium" />
+    </ExampleContainer>
+    <ExampleContainer>
+      <Title title="Large margin" theme="light" level={4} />
+      <DxcTextarea label="Large" margin="large" />
+    </ExampleContainer>
+    <ExampleContainer>
+      <Title title="Xlarge margin" theme="light" level={4} />
+      <DxcTextarea label="Xlarge" margin="xlarge" />
+    </ExampleContainer>
+    <ExampleContainer>
+      <Title title="Xxlarge margin" theme="light" level={4} />
+      <DxcTextarea label="Xxlarge" margin="xxlarge" />
+    </ExampleContainer>
   </>
 );
