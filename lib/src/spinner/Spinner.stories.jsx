c9501a041e6e4f84d371c10517bfdda4440c2643
agonzalez97 <aidamaria.gonzalez@dxc.com>
2022-02-04 11:17:44 +0100

    Edited examples

--- lib/src/spinner/Spinner.stories.jsx
+++ lib/src/spinner/Spinner.stories.jsx
@@ -12,54 +12,56 @@ export const Chromatic = () => (
   <>
     <ExampleContainer>
       <Title title="With label" theme="light" level={4} />
-      <DxcSpinner label="Label"></DxcSpinner>
-      <Title title="With value" theme="light" level={4} />
-      <DxcSpinner value="50"></DxcSpinner>
+      <DxcSpinner label="Label" value="50"></DxcSpinner>
       <Title title="With value label" theme="light" level={4} />
       <DxcSpinner value="50" showValue></DxcSpinner>
       <Title title="With label and value label" theme="light" level={4} />
       <DxcSpinner label="Label" value="50" showValue></DxcSpinner>
+      <Title title="With 100%" theme="light" level={4} />
+      <DxcSpinner label="Label" value="100" showValue></DxcSpinner>
     </ExampleContainer>
     <Title title="Modes" theme="light" level={2} />
     <ExampleContainer>
       <Title title="Mode large" theme="light" level={4} />
-      <DxcSpinner mode="large"></DxcSpinner>
+      <DxcSpinner mode="large" value="50"></DxcSpinner>
       <Title title="Mode small" theme="light" level={4} />
-      <DxcSpinner mode="small"></DxcSpinner>
+      <DxcSpinner mode="small" value="50"></DxcSpinner>
+      <Title title="Mode small with 100%" theme="light" level={4} />
+      <DxcSpinner mode="small" value="100" showValue></DxcSpinner>
     </ExampleContainer>
     <Title title="Margins with large mode" theme="light" level={2} />
     <ExampleContainer>
       <Title title="Xxsmall margin" theme="light" level={4} />
-      <DxcSpinner margin="xxsmall"></DxcSpinner>
+      <DxcSpinner margin="xxsmall" value="75"></DxcSpinner>
       <Title title="Xsmall margin" theme="light" level={4} />
-      <DxcSpinner margin="xsmall"></DxcSpinner>
+      <DxcSpinner margin="xsmall" value="75"></DxcSpinner>
       <Title title="Small margin" theme="light" level={4} />
-      <DxcSpinner margin="small"></DxcSpinner>
+      <DxcSpinner margin="small" value="75"></DxcSpinner>
       <Title title="Medium margin" theme="light" level={4} />
-      <DxcSpinner margin="medium"></DxcSpinner>
+      <DxcSpinner margin="medium" value="75"></DxcSpinner>
       <Title title="Large margin" theme="light" level={4} />
-      <DxcSpinner margin="large"></DxcSpinner>
+      <DxcSpinner margin="large" value="75"></DxcSpinner>
       <Title title="Xlarge margin" theme="light" level={4} />
-      <DxcSpinner margin="xlarge"></DxcSpinner>
+      <DxcSpinner margin="xlarge" value="75"></DxcSpinner>
       <Title title="Xxlarge margin" theme="light" level={4} />
-      <DxcSpinner margin="xxlarge"></DxcSpinner>
+      <DxcSpinner margin="xxlarge" value="75"></DxcSpinner>
     </ExampleContainer>
     <Title title="Margins with small mode" theme="light" level={2} />
     <ExampleContainer>
       <Title title="Xxsmall margin" theme="light" level={4} />
-      <DxcSpinner margin="xxsmall" mode="small"></DxcSpinner>
+      <DxcSpinner margin="xxsmall" mode="small" value="75"></DxcSpinner>
       <Title title="Xsmall margin" theme="light" level={4} />
-      <DxcSpinner margin="xsmall" mode="small"></DxcSpinner>
+      <DxcSpinner margin="xsmall" mode="small" value="75"></DxcSpinner>
       <Title title="Small margin" theme="light" level={4} />
-      <DxcSpinner margin="small" mode="small"></DxcSpinner>
+      <DxcSpinner margin="small" mode="small" value="75"></DxcSpinner>
       <Title title="Medium margin" theme="light" level={4} />
-      <DxcSpinner margin="medium" mode="small"></DxcSpinner>
+      <DxcSpinner margin="medium" mode="small" value="75"></DxcSpinner>
       <Title title="Large margin" theme="light" level={4} />
-      <DxcSpinner margin="large" mode="small"></DxcSpinner>
+      <DxcSpinner margin="large" mode="small" value="75"></DxcSpinner>
       <Title title="Xlarge margin" theme="light" level={4} />
-      <DxcSpinner margin="xlarge" mode="small"></DxcSpinner>
+      <DxcSpinner margin="xlarge" mode="small" value="75"></DxcSpinner>
       <Title title="Xxlarge margin" theme="light" level={4} />
-      <DxcSpinner margin="xxlarge" mode="small"></DxcSpinner>
+      <DxcSpinner margin="xxlarge" mode="small" value="75"></DxcSpinner>
     </ExampleContainer>
   </>
 );
