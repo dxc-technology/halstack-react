af50af5527b9b1a1093a0065d7bfde16f8e6fce8
GomezIvann <UO258748@uniovi.es>
2022-02-15 16:06:38 +0100

    Empty spaces removed from types in ToggleGroup

--- lib/src/toggle-group/types.ts
+++ lib/src/toggle-group/types.ts
@@ -1,4 +1,3 @@
-
 type Space = "xxsmall" | "xsmall" | "small" | "medium" | "large" | "xlarge" | "xxlarge";
 type Margin = {
   top?: Space;
@@ -7,35 +6,36 @@ type Margin = {
   right?: Space;
 };
 type SVG = React.SVGProps<SVGSVGElement> | React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+
 type OptionCommons = {
-      /**
-    * Number with the option inner value.
-    */
-       value: string;
-    /**
-    * @deprecated URL of the icon that will be placed. IconSrc and label can't be used at same time.
-    */
-      iconSrc?: string;
+  /**
+   * Number with the option inner value.
+   */
+  value: string;
+  /**
+   * @deprecated URL of the icon that will be placed. IconSrc and label can't be used at same time.
+   */
+  iconSrc?: string;
 };
 type OptionIcon = OptionCommons & {
-    /**
-    * String with the option display value.
-    */
-     label?: string;
-     /**
-     * Element used as the icon. Icon and label can't be used at same time.
-     */
-     icon: SVG;
+  /**
+   * String with the option display value.
+   */
+  label?: string;
+  /**
+   * Element used as the icon. Icon and label can't be used at same time.
+   */
+  icon: SVG;
 };
-type OptionLabel= OptionCommons & {
-    /**
-    * String with the option display value.
-    */
-    label: string;
-    /**
-    * Element used as the icon. Icon and label can't be used at same time.
-    */
-    icon?: SVG;
+type OptionLabel = OptionCommons & {
+  /**
+   * String with the option display value.
+   */
+  label: string;
+  /**
+   * Element used as the icon. Icon and label can't be used at same time.
+   */
+  icon?: SVG;
 };
 type Option = OptionIcon | OptionLabel;
 
@@ -49,17 +49,17 @@ type Props = {
    */
   helperText?: string;
   /**
-   * The key(s) of the selected value(s). If the toggle group component doesn't allow multiple selection, 
-   * it must be one unique value. If the component allows multiple selection, value must be an array. 
+   * The key(s) of the selected value(s). If the toggle group component doesn't allow multiple selection,
+   * it must be one unique value. If the component allows multiple selection, value must be an array.
    * If undefined, the component will be uncontrolled and the value will be managed internally by the component.
    */
   value?: string | string[];
   /**
-   * This function will be called every time the selection changes. The number with the key of the selected 
-   * value will be passed as a parameter to this function. 
+   * This function will be called every time the selection changes. The number with the key of the selected
+   * value will be passed as a parameter to this function.
    * If multiple selection is allowed, an array of keys will be passed.
    */
-  onChange?:(optionIndex: number | number[]) => void;
+  onChange?: (optionIndex: number | number[]) => void;
   /**
    * If true, the component will be disabled.
    */
@@ -73,7 +73,7 @@ type Props = {
    */
   multiple?: boolean;
   /**
-   * Size of the margin to be applied to the component ('xxsmall' | 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge' | 'xxlarge'). 
+   * Size of the margin to be applied to the component ('xxsmall' | 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge' | 'xxlarge').
    * You can pass an object with 'top', 'bottom', 'left' and 'right' properties in order to specify different margin sizes.
    */
   margin?: Space | Margin;
