8caf886e7098f91cf1c7bf49c49de2ad331b98da
marcialfps <m.parrilla@dxc.com>
2022-03-29 10:05:19 +0200

    Scrollbar styles

--- lib/.storybook/components/ExampleContainer.tsx
+++ lib/.storybook/components/ExampleContainer.tsx
@@ -17,7 +17,25 @@ const DivContainer = styled.div`
   margin: 15px;
   ${(props) => props.expanded && "height: 100vh;"}
   * {
-    overflow: scroll;
+    /* width */
+    ::-webkit-scrollbar {
+      width: 10px !important;
+    }
+
+    /* Track */
+    ::-webkit-scrollbar-track {
+      background: #f1f1f1 !important;
+    }
+
+    /* Handle */
+    ::-webkit-scrollbar-thumb {
+      background: #888 !important;
+    }
+
+    /* Handle on hover */
+    ::-webkit-scrollbar-thumb:hover {
+      background: #555 !important;
+    }
   }
 `;
 
