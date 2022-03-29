3426e67bc29abece9de154e71f43db8907fdabd8
marcialfps <m.parrilla@dxc.com>
2022-03-29 14:40:06 +0200

    Add again decorator

--- lib/src/table/Table.stories.jsx
+++ lib/src/table/Table.stories.jsx
@@ -7,6 +7,13 @@ import ExampleContainer from "../../.storybook/components/ExampleContainer";
 export default {
   title: "Table",
   component: DxcTable,
+  decorators: [
+    (Story) => (
+      <Decorator>
+        <Story />
+      </Decorator>
+    ),
+  ],
 };
 
 const Table = () => (
@@ -276,6 +283,28 @@ const Table = () => (
     </ExampleContainer>
   </>
 );
+
+const Decorator = styled.div`
+  * {
+    overflow: auto !important;
+    /* width */
+    ::-webkit-scrollbar {
+      width: 10px !important;
+    }
+    /* Track */
+    ::-webkit-scrollbar-track {
+      background: #f1f1f1 !important;
+    }
+    /* Handle */
+    ::-webkit-scrollbar-thumb {
+      background: #888 !important;
+    }
+    /* Handle on hover */
+    ::-webkit-scrollbar-thumb:hover {
+      background: #555 !important;
+    }
+  }
+`;
 
 export const Chromatic = Table.bind({});
 Chromatic.args = {
