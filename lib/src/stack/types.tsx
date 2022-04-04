ffebc859d3ee01541d8d3788eceb296b178717b7
marcialfps <m.parrilla@dxc.com>
2022-04-04 16:48:37 +0200

    Doc in types

--- lib/src/stack/types.tsx
+++ lib/src/stack/types.tsx
@@ -1,4 +1,7 @@
 type Props = {
+  /**
+   * Space applied between each child.
+   */
   gutter?:
     | "none"
     | "xxxsmall"
@@ -10,9 +13,21 @@ type Props = {
     | "xlarge"
     | "xxlarge"
     | "xxxlarge";
+  /**
+   * If true a divider is shown between children.
+   */
   divider?: boolean;
+  /**
+   * Alignment applied to children.
+   */
   align?: "start" | "center" | "end" | "baseline" | "stretch";
+  /**
+   * Component type or primitive that is rendered as the type of the root element.
+   */
   as?: React.ElementType;
+  /**
+   * Custom content inside the stack.
+   */
   children: React.ReactNode;
 };
 
