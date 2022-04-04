dd59ff2f8d5155f02da5eeadd222bf6015dc727a
raquelarrojo <arrojoraquel@gmail.com>
2022-04-04 13:26:37 +0200

    Change tag test

--- lib/src/tag/Tag.stories.tsx
+++ lib/src/tag/Tag.stories.tsx
@@ -124,6 +124,7 @@ export const Chromatic = () => (
       <Title title="FitContent size" theme="light" level={4} />
       <DxcTag label="FitContent" size="fitContent" icon={icon} />
     </ExampleContainer>
+    <hr />
   </>
 );
 
