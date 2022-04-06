681e41467a0ddf90f48cfcb1926053716dd723be
marcialfps <m.parrilla@dxc.com>
2022-04-06 16:15:14 +0200

    Bleed component documentation

---
+++ docs/src/pages/components/cdk-components/bleed/examples/horizontal.js
@@ -0,0 +1,25 @@
+import { DxcStack, DxcBleed } from "@dxc-technology/halstack-react";
+import Placeholder from "../../../common/Placeholder";
+
+const code = `() => {
+  return (
+    <div
+      style={{
+        background: "#f2eafa",
+        padding: "2rem"
+      }}
+    >
+        <DxcStack gutter="medium">
+            <Placeholder></Placeholder>
+            <DxcBleed horizontal="medium">
+                <Placeholder></Placeholder>
+            </DxcBleed>
+            <Placeholder></Placeholder>
+        </DxcStack>
+    </div>
+  );
+}`;
+
+const scope = { DxcStack, DxcBleed, Placeholder };
+
+export default { code, scope };
