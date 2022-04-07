b6aa679ca3b40b3661e52338ee6a4c74c847c42b
marcialfps <m.parrilla@dxc.com>
2022-04-07 13:43:48 +0200

    Gutter browser support warning

--- docs/src/pages/components/cdk-components/row/Row.jsx
+++ docs/src/pages/components/cdk-components/row/Row.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { DxcHeading } from "@dxc-technology/halstack-react";
+import { DxcAlert, DxcHeading, DxcLink } from "@dxc-technology/halstack-react";
 import ComponentDoc from "../../common/ComponentDoc";
 import Section from "../../common/Section";
 import Example from "../../common/Example";
@@ -9,11 +9,24 @@ import basicUsage from "./examples/basicUsage";
 import alignAndJustify from "./examples/alignAndJustify";
 import gutterAndReverse from "./examples/gutterAndReverse";
 import wrap from "./examples/wrap";
+import Code from "../../common/Code";
 
 function Row() {
   return (
     <ComponentDoc>
       <ComponentHeader title="Row" status="experimental"></ComponentHeader>
+      <DxcAlert type="warning" margin={{ bottom: "small" }} size="fillParent">
+        Some older browsers may not support <Code>gutter</Code> prop. Please
+        check{" "}
+        <DxcLink
+          href="https://caniuse.com/flexbox-gap"
+          underlined={false}
+          inheritedColor={true}
+          newWindow
+          text="here"
+        ></DxcLink>{" "}
+        that it works in your target browsers.
+      </DxcAlert>
       <Section>
         <DxcHeading level={3} text="Props" margin={{ bottom: "small" }} />
         <RowPropsTable />
