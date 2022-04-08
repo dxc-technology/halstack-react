ebba95d6cceac661a3da93c0d0cbdd56090bb9f1
GomezIvann <UO258748@uniovi.es>
2022-04-08 10:35:29 +0200

    New aria attributes for Wizard component

--- lib/src/wizard/Wizard.tsx
+++ lib/src/wizard/Wizard.tsx
@@ -72,56 +72,57 @@ const DxcWizard = ({
 
   return (
     <ThemeProvider theme={colorsTheme.wizard}>
-      <StepsContainer mode={mode} margin={margin}>
-        {steps?.map((step, i) => {
-          return (
-            <StepContainer key={`step${i}`} mode={mode} lastStep={i === steps?.length - 1}>
-              <Step
-                tabIndex={tabIndex}
-                onClick={() => handleStepClick(i)}
-                mode={mode}
-                disabled={step.disabled}
-                first={i === 0}
-                last={i === steps?.length - 1}
-              >
-                <StepHeader>
-                  <IconContainer current={i === renderedCurrent} visited={i < renderedCurrent} disabled={step.disabled}>
-                    {step.icon ? (
-                      <StepIconContainer disabled={step.disabled}>
-                        {typeof step.icon === "string" ? <Icon src={step.icon}></Icon> : step.icon}
-                      </StepIconContainer>
-                    ) : (
-                      <Number disabled={step.disabled} current={i === renderedCurrent}>
-                        {i + 1}
-                      </Number>
-                    )}
-                  </IconContainer>
-                  {step.valid !== undefined &&
-                    (step.valid ? (
-                      <ValidityIconContainer>{icons.validIcon}</ValidityIconContainer>
-                    ) : (
-                      <ValidityIconContainer>{icons.invalidIcon}</ValidityIconContainer>
-                    ))}
-                </StepHeader>
-                {(step.label || step.description) && (
-                  <InfoContainer>
-                    {step.label && (
-                      <Label disabled={step.disabled} visited={i <= innerCurrent}>
-                        {step.label}
-                      </Label>
-                    )}
-                    {step.description && (
-                      <Description disabled={step.disabled} visited={i <= innerCurrent}>
-                        {step.description}
-                      </Description>
-                    )}
-                  </InfoContainer>
-                )}
-              </Step>
-              {i === steps.length - 1 ? "" : <StepSeparator mode={mode} />}
-            </StepContainer>
-          );
-        })}
+      <StepsContainer mode={mode} margin={margin} role="group">
+        {steps?.map((step, i) => (
+          <StepContainer key={`step${i}`} mode={mode} lastStep={i === steps?.length - 1}>
+            <Step
+              onClick={() => {
+                handleStepClick(i);
+              }}
+              disabled={step.disabled}
+              mode={mode}
+              first={i === 0}
+              last={i === steps?.length - 1}
+              aria-current={renderedCurrent === i ? "step" : "false"}
+              tabIndex={tabIndex}
+            >
+              <StepHeader>
+                <IconContainer current={i === renderedCurrent} visited={i < renderedCurrent} disabled={step.disabled}>
+                  {step.icon ? (
+                    <StepIconContainer disabled={step.disabled}>
+                      {typeof step.icon === "string" ? <Icon src={step.icon}></Icon> : step.icon}
+                    </StepIconContainer>
+                  ) : (
+                    <Number disabled={step.disabled} current={i === renderedCurrent}>
+                      {i + 1}
+                    </Number>
+                  )}
+                </IconContainer>
+                {step.valid !== undefined &&
+                  (step.valid ? (
+                    <ValidityIconContainer>{icons.validIcon}</ValidityIconContainer>
+                  ) : (
+                    <ValidityIconContainer>{icons.invalidIcon}</ValidityIconContainer>
+                  ))}
+              </StepHeader>
+              {(step.label || step.description) && (
+                <InfoContainer>
+                  {step.label && (
+                    <Label disabled={step.disabled} visited={i <= innerCurrent}>
+                      {step.label}
+                    </Label>
+                  )}
+                  {step.description && (
+                    <Description disabled={step.disabled} visited={i <= innerCurrent}>
+                      {step.description}
+                    </Description>
+                  )}
+                </InfoContainer>
+              )}
+            </Step>
+            {i === steps.length - 1 ? "" : <StepSeparator mode={mode} />}
+          </StepContainer>
+        ))}
       </StepsContainer>
     </ThemeProvider>
   );
