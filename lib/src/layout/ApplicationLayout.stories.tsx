f5a4dd807016e028f0194af88279308937b4433e
Jairo Suárez González <jairosuarezgonzalez@MacBook-Pro-de-Jairo.local>
2022-04-27 10:51:44 +0200

    Changing viewport size

--- lib/src/layout/ApplicationLayout.stories.tsx
+++ lib/src/layout/ApplicationLayout.stories.tsx
@@ -111,7 +111,7 @@ ApplicationLayoutResponsive.args = {
   with: "props",
 };
 ApplicationLayoutResponsive.parameters = {
-  chromatic: { viewports: [720] },
+  chromatic: { viewports: [320] },
 };
 export const ApplicationLayoutWithCustomHeader = () => (
   <>
