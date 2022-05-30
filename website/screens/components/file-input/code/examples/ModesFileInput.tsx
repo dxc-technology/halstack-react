904d5e9ba70ff2ef5a0ca92e44ae711d98bb9831
agonzalez97 <aidamaria.gonzalez@dxc.com>
2022-05-30 13:28:49 +0200

    Formated code

--- website/screens/components/file-input/code/examples/ModesFileInput.tsx
+++ website/screens/components/file-input/code/examples/ModesFileInput.tsx
@@ -5,32 +5,28 @@ import {
 } from "@dxc-technology/halstack-react";
 
 const code = `() => {
-          return (
-              <DxcInset space="large">
-                <DxcStack gutter="large">
-                      <div style={{ display: "flex", flexWrap: "wrap" }}>
-                          <DxcFileInput
-                            label="File"
-                            helperText="Helper Text"
-                            margin="small"
-                          />
-                          <DxcFileInput
-                            label="Filedrop"
-                            mode="filedrop"
-                            helperText="Helper Text"
-                            margin="small"
-                          />
-                          <DxcFileInput
-                            label="Dropzone"
-                            mode="dropzone"
-                            helperText="Helper Text"
-                            margin="small"
-                          />
-                      </div>
-                </DxcStack>
-              </DxcInset>
-          );
-      }`;
+  return (
+    <DxcInset space="large">
+      <DxcStack gutter="large">
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
+          <DxcFileInput label="File" helperText="Helper Text" margin="small" />
+          <DxcFileInput
+            label="Filedrop"
+            mode="filedrop"
+            helperText="Helper Text"
+            margin="small"
+          />
+          <DxcFileInput
+            label="Dropzone"
+            mode="dropzone"
+            helperText="Helper Text"
+            margin="small"
+          />
+        </div>
+      </DxcStack>
+    </DxcInset>
+  );
+}`;
 
 const scope = {
   DxcFileInput,
