ecdfa93706b0dded595c68b5381f3f591aca8e15
Jialecl <jialestrabajos@gmail.com>
2022-06-02 12:33:04 +0200

    Sidenav redesign and child components

--- lib/src/sidenav/types.ts
+++ lib/src/sidenav/types.ts
@@ -1,10 +1,35 @@
-type Space = "xxsmall" | "xsmall" | "small" | "medium" | "large" | "xlarge" | "xxlarge";
+type SVG = React.SVGProps<SVGSVGElement>;
 
-type Padding = {
-  top?: Space;
-  bottom?: Space;
-  left?: Space;
-  right?: Space;
+type SidenavPropsType = {
+  /**
+   * The area inside the sidenav. This area can be used to render custom content.
+   */
+  children: React.ReactNode;
+};
+
+export type SidenavTitlePropsType = {
+  /**
+   * The area inside the sidenav title. This area can be used to render custom content.
+   */
+  children: React.ReactNode;
+  /**
+   * The icon to be displayed in the sidenav title.
+   */
+  icon?: SVG;
+};
+
+export type SidenavSectionPropsType = {
+  /**
+   * The area inside the sidenav section. This area can be used to render sidenav groups or links.
+   */
+  children: React.ReactNode;
+};
+
+export type SidenavGroupPropsType = {
+  children: React.ReactNode;
+  title: string;
+  collapsable: boolean;
+  icon?: SVG;
 };
 
 export type SidenavLinkPropsType = {
@@ -17,37 +42,19 @@ export type SidenavLinkPropsType = {
    */
   href?: string;
   /**
-   * This function will be called when the user clicks the link.
+   * If true, the page is opened in a new browser tab.
    */
-  onClick?: () => void;
+  newWindow?: boolean;
+  /**
+   * Element or path used as the icon that will be placed to the left of the link text.
+   */
+  icon?: SVG;
+  /**
+   * If true, the link will be marked as selected. This can also affect the group if it is closed to indicate that one of its links is selected.
+   */
+  selected?: boolean;
   /**
    * The area inside the sidenav link. This area can be used to render custom content.
-   */
-  children: React.ReactNode;
-};
-
-export type SidenavTitlePropsType = {
-  /**
-   * The area inside the sidenav title. This area can be used to render custom content.
-   */
-  children: React.ReactNode;
-};
-
-export type SidenavSubtitlePropsType = {
-  /**
-   * The area inside the sidenav subtitle. This area can be used to render custom content.
-   */
-  children: React.ReactNode;
-};
-
-type SidenavPropsType = {
-  /**
-   * Size of the padding to be applied to the custom area ('xxsmall' | 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge' | 'xxlarge').
-   * You can pass an object with 'top', 'bottom', 'left' and 'right' properties in order to specify different padding sizes.
-   */
-  padding?: Space | Padding;
-  /**
-   * The area inside the sidenav. This area can be used to render custom content.
    */
   children: React.ReactNode;
 };
