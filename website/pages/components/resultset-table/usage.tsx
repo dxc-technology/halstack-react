23e2d6dd63a0ad7e75fbb03ebe71c2ee1884dfbb
agonzalez97 <aidamaria.gonzalez@dxc.com>
2022-06-07 13:21:05 +0200

    Add Code tab to Resultset Table page

--- website/pages/components/resultset-table/usage.tsx
+++ website/pages/components/resultset-table/usage.tsx
@@ -1,15 +1,15 @@
 import Head from "next/head";
 import type { ReactElement } from "react";
-import ResultsetTableCodePage from "../../../screens/components/resultset-table/code/ResultsetTableCodePage";
 import ResultsetTablePageLayout from "../../../screens/components/resultset-table/ResultsetTablePageLayout";
+import ResultsetTableUsagePage from "../../../screens/components/resultset-table/usage/ResultsetTableUsagePage";
 
 const Usage = () => {
   return (
     <>
       <Head>
-        <title>Resultset Table — Halstack Design System</title>
+        <title>ResultsetTable Usage — Halstack Design System</title>
       </Head>
-      <ResultsetTableCodePage></ResultsetTableCodePage>
+      <ResultsetTableUsagePage></ResultsetTableUsagePage>
     </>
   );
 };
---
+++ website/screens/components/resultset-table/code/examples/basicUsage.ts
@@ -0,0 +1,72 @@
+import {
+  DxcResultsetTable,
+  DxcButton,
+  DxcInset,
+} from "@dxc-technology/halstack-react";
+import { deleteIcon } from "./Icons";
+
+const code = `() => {
+  const columns = [
+    { displayValue: "Id", isSortable: false },
+    { displayValue: "Name", isSortable: true },
+    { displayValue: "City", isSortable: false },
+    { displayValue: "Actions", isSortable: true },
+  ];
+
+  const rows = [
+    [
+      { displayValue: "001", sortValue: "001" },
+      { displayValue: "Peter", sortValue: "Peter" },
+      { displayValue: "Miami", sortValue: "Miami" },
+      { displayValue: <DxcButton icon={deleteIcon} /> },
+    ],
+    [
+      { displayValue: "002", sortValue: "002" },
+      { displayValue: "Louis", sortValue: "Louis" },
+      { displayValue: "London", sortValue: "London" },
+      { displayValue: "" },
+    ],
+    [
+      { displayValue: "003", sortValue: "003" },
+      { displayValue: "Lana", sortValue: "Lana" },
+      { displayValue: "Amsterdam", sortValue: "Amsterdam" },
+      { displayValue: <DxcButton icon={deleteIcon} /> },
+    ],
+    [
+      { displayValue: "004", sortValue: "004" },
+      { displayValue: "Rick", sortValue: "Rick" },
+      { displayValue: "London", sortValue: "London" },
+      { displayValue: <DxcButton icon={deleteIcon} /> },
+    ],
+    [
+      { displayValue: "005", sortValue: "005" },
+      { displayValue: "Mark", sortValue: "Mark" },
+      { displayValue: "Miami", sortValue: "Miami" },
+      { displayValue: <DxcButton icon={deleteIcon} /> },
+    ],
+    [
+      { displayValue: "006", sortValue: "006" },
+      { displayValue: "Cris", sortValue: "Cris" },
+      { displayValue: "Paris", sortValue: "Paris" },
+      { displayValue: "" },
+    ],
+  ];
+
+  return (
+    <DxcInset space="large">
+      <DxcResultsetTable
+        columns={columns}
+        rows={rows}
+      ></DxcResultsetTable>
+    </DxcInset>
+  );
+}`;
+
+const scope = {
+  DxcResultsetTable,
+  DxcButton,
+  DxcInset,
+  deleteIcon,
+};
+
+export default { code, scope };
