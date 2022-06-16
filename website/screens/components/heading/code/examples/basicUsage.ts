a95116b85d68d8aa2d3a0d690fd2a62e794ac168
raquelarrojo <arrojoraquel@gmail.com>
2022-06-16 13:47:29 +0200

    Review Heading pages

--- website/screens/components/heading/code/examples/basicUsage.ts
+++ website/screens/components/heading/code/examples/basicUsage.ts
@@ -2,8 +2,7 @@ import { DxcHeading, DxcInset } from "@dxc-technology/halstack-react";
 const code = `() => {
   return (
     <DxcInset space="large">
-      <DxcHeading text="Title for main section" />
-      <DxcHeading level={2} text="Heading for sections" />
+      <DxcHeading level={2} text="Introduction" />
     </DxcInset>
   );
 }`;
