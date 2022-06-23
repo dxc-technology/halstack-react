48e0d99838e825364652eb4774977c99ec581bb0
Jialecl <jialestrabajos@gmail.com>
2022-06-23 17:06:43 +0200

    Removing className and changing docs sidenav

--- lib/src/sidenav/Sidenav.tsx
+++ lib/src/sidenav/Sidenav.tsx
@@ -13,6 +13,27 @@ import SidenavPropsType, {
 } from "./types.js";
 import DxcStack from "../stack/Stack";
 
+const collapsableIcon = (
+  <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor">
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" />
+  </svg>
+);
+
+const collapsedIcon = (
+  <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor">
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M12 8l-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14z" />
+  </svg>
+);
+
+const externalLinkIcon = (
+  <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" fill="currentColor">
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z" />
+  </svg>
+);
+
 const DxcSidenav = ({ children }: SidenavPropsType): JSX.Element => {
   const colorsTheme = useTheme();
   return (
@@ -44,40 +65,26 @@ const Title = ({ children, icon }: SidenavTitlePropsType): JSX.Element => (
   </SidenavTitle>
 );
 
-const Section = ({ children }: SidenavSectionPropsType): JSX.Element => (
-  <div>
-    <DxcStack gutter="small">{children}</DxcStack>
-  </div>
-);
+const Section = ({ children }: SidenavSectionPropsType): JSX.Element => <DxcStack gutter="small">{children}</DxcStack>;
 
 const Group = ({ children, title, collapsable = false, icon }: SidenavGroupPropsType): JSX.Element => {
   const [collapsed, setCollapsed] = useState(false);
   return (
     <SidenavGroup>
       {collapsable && title ? (
-        <button className="sidenav-title" aria-expanded={collapsed} onClick={() => setCollapsed(!collapsed)}>
+        <SidenavGroupTitleButton aria-expanded={collapsed} onClick={() => setCollapsed(!collapsed)}>
           <span>
             {icon}
             {title}
           </span>{" "}
-          {collapsed ? (
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor">
-              <path d="M0 0h24v24H0z" fill="none" />
-              <path d="M12 8l-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14z" />
-            </svg>
-          ) : (
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor">
-              <path d="M0 0h24v24H0z" fill="none" />
-              <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" />
-            </svg>
-          )}
-        </button>
+          {collapsed ? collapsedIcon : collapsableIcon}
+        </SidenavGroupTitleButton>
       ) : (
         title && (
-          <div className="sidenav-title">
+          <SidenavGroupTitle>
             {icon}
             {title}
-          </div>
+          </SidenavGroupTitle>
         )
       )}
       {(!collapsable || (collapsable && !collapsed)) && children}
@@ -118,12 +125,7 @@ const Link = forwardRef(
           {icon}
           {children}
         </span>
-        {newWindow && (
-          <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
-            <path d="M0 0h24v24H0z" fill="none" />
-            <path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z" />
-          </svg>
-        )}
+        {newWindow && externalLinkIcon}
       </SidenavLink>
     );
   }
@@ -176,43 +178,59 @@ const SidenavTitle = styled.div`
 
 const SidenavGroup = styled.div`
   width: 100%;
-
-  button.sidenav-title {
-    all: unset;
-    cursor: pointer;
-    justify-content: space-between;
-    span {
-      display: flex;
-      align-items: center;
-    }
-    &:focus {
-      outline: 2px solid ${(props) => props.theme.linkFocusColor};
-      outline-offset: -2px;
-    }
-  }
-  .sidenav-title,
-  button.sidenav-title {
-    svg {
-      width: 16px;
-      fill: currentColor;
-      margin-right: 8px;
-    }
-    box-sizing: border-box;
-    width: 100%;
-
-    font-family: ${(props) => props.theme.groupTitleFontFamily};
-    font-style: ${(props) => props.theme.groupTitleFontStyle};
-    font-weight: ${(props) => props.theme.groupTitleFontWeight};
-    font-size: ${(props) => props.theme.groupTitleFontSize};
-    line-height: 19px;
-
-    display: flex;
-    align-items: center;
-    margin: 0px;
-    padding: 7px 24px;
-  }
   a {
     padding: 7px 24px 7px 36px;
+  }
+`;
+
+const SidenavGroupTitle = styled.span`
+  svg {
+    width: 16px;
+    margin-right: 8px;
+  }
+  box-sizing: border-box;
+  width: 100%;
+
+  font-family: ${(props) => props.theme.groupTitleFontFamily};
+  font-style: ${(props) => props.theme.groupTitleFontStyle};
+  font-weight: ${(props) => props.theme.groupTitleFontWeight};
+  font-size: ${(props) => props.theme.groupTitleFontSize};
+  line-height: 19px;
+
+  display: flex;
+  align-items: center;
+  margin: 0px;
+  padding: 7px 24px;
+`;
+
+const SidenavGroupTitleButton = styled.button`
+  all: unset;
+  cursor: pointer;
+  justify-content: space-between;
+  box-sizing: border-box;
+  width: 100%;
+
+  font-family: ${(props) => props.theme.groupTitleFontFamily};
+  font-style: ${(props) => props.theme.groupTitleFontStyle};
+  font-weight: ${(props) => props.theme.groupTitleFontWeight};
+  font-size: ${(props) => props.theme.groupTitleFontSize};
+  line-height: 19px;
+
+  display: flex;
+  align-items: center;
+  margin: 0px;
+  padding: 7px 24px;
+  span {
+    display: flex;
+    align-items: center;
+    svg {
+      width: 16px;
+      margin-right: 8px;
+    }
+  }
+  &:focus {
+    outline: 2px solid ${(props) => props.theme.linkFocusColor};
+    outline-offset: -2px;
   }
 `;
 
