bdd4943ec92f81a52a58544598295f06febfa887
marcialfps <m.parrilla@dxc.com>
2022-07-05 14:28:11 +0200

    Use HeaderDescriptionCell

--- website/screens/components/inline/code/InlineCodePage.tsx
+++ website/screens/components/inline/code/InlineCodePage.tsx
@@ -6,6 +6,7 @@ import QuickNavContainerLayout from "@/common/QuickNavContainerLayout";
 import Example from "@/common/example/Example";
 import basicUsage from "./examples/basicUsage";
 import alignment from "./examples/alignment";
+import HeaderDescriptionCell from "@/common/HeaderDescriptionCell";
 
 const sections = [
   {
@@ -15,7 +16,7 @@ const sections = [
         <tr>
           <th>Name</th>
           <th>Default</th>
-          <th>Description</th>
+          <HeaderDescriptionCell>Description</HeaderDescriptionCell>
         </tr>
         <tr>
           <td>alignX: 'start' | 'end' | 'center'</td>
