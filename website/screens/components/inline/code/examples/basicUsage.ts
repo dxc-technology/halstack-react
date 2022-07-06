97d55dbf47a39ac8928c20e7769375cb8f4c517d
marcialfps <m.parrilla@dxc.com>
2022-07-06 14:19:28 +0200

    Add inset to examples

--- website/screens/components/inline/code/examples/basicUsage.ts
+++ website/screens/components/inline/code/examples/basicUsage.ts
@@ -1,23 +1,26 @@
-import { DxcInline } from "@dxc-technology/halstack-react";
+import { DxcInline, DxcInset } from "@dxc-technology/halstack-react";
 import Placeholder from "./Placeholder";
 
 const code = `() => {
   return (
-    <DxcInline gutter="0.5rem" divider>
-      <Placeholder width="medium" height="large" />
-      <Placeholder width="large" height="medium" />
-      <Placeholder width="medium" height="medium" />
-      <Placeholder width="small" height="medium" />
-      <Placeholder width="medium" height="small" />
-      <Placeholder width="medium" height="large" />
-      <Placeholder width="large" height="medium" />
-      <Placeholder width="medium" height="medium" />
-    </DxcInline>
+    <DxcInset space="2rem">
+      <DxcInline gutter="0.5rem" divider>
+        <Placeholder width="medium" height="large" />
+        <Placeholder width="large" height="medium" />
+        <Placeholder width="medium" height="medium" />
+        <Placeholder width="small" height="medium" />
+        <Placeholder width="medium" height="small" />
+        <Placeholder width="medium" height="large" />
+        <Placeholder width="large" height="medium" />
+        <Placeholder width="medium" height="medium" />
+      </DxcInline>
+    </DxcInset>
   );
 }`;
 
 const scope = {
   DxcInline,
+  DxcInset,
   Placeholder,
 };
 
