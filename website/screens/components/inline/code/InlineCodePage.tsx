ba223e60cafccf1f401f2be5f11005e8692d31e4
marcialfps <m.parrilla@dxc.com>
2022-07-06 14:19:05 +0200

    New gutter values

--- website/screens/components/inline/code/InlineCodePage.tsx
+++ website/screens/components/inline/code/InlineCodePage.tsx
@@ -52,7 +52,7 @@ const sections = [
         <tr>
           <td>
             gutter: '0rem' | '0.125rem' | '0.25rem' | '0.5rem' | '0.75rem' |
-            '1rem' | '1.5rem' | '2rem'
+            '1rem' | '1.5rem' | '2rem' | '3rem' | '4rem' | '5rem'
           </td>
           <td>
             <Code>'0rem'</Code>
