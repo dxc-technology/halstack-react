f5592fa3b16486803f5b6f6fc52d7dc109fb26c0
marcialfps <m.parrilla@dxc.com>
2022-07-13 14:51:01 +0200

    Fix gutter values

--- website/screens/components/rows/code/RowsCodePage.tsx
+++ website/screens/components/rows/code/RowsCodePage.tsx
@@ -48,7 +48,7 @@ const RowsPropsTable = () => (
     <tr>
       <td>
         gutter: '0rem' | '0.125rem' | '0.25rem' | '0.5rem' | '0.75rem' | '1rem'
-        | '1.5rem' | '2rem'
+        | '1.5rem' | '2rem' | '3rem' | '4rem' | '5rem'
       </td>
       <td>
         <Code>'0rem'</Code>
