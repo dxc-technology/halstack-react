652f92b461a2412407b8c3cf415427334b2d7c81
agonzalez97 <aidamaria.gonzalez@dxc.com>
2022-07-15 12:30:29 +0200

    Added documentation for bleed component

---
+++ website/pages/components/bleed/index.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import type { ReactElement } from "react";
+import BleedPageLayout from "../../../screens/components/bleed/BleedPageLayout";
+import BleedCodePage from "../../../screens/components/bleed/code/BleedCodePage";
+
+const Index = () => {
+  return (
+    <>
+      <Head>
+        <title>Bleed — Halstack Design System</title>
+      </Head>
+      <BleedCodePage></BleedCodePage>
+    </>
+  );
+};
+
+Index.getLayout = function getLayout(page: ReactElement) {
+  return <BleedPageLayout>{page}</BleedPageLayout>;
+};
+
+export default Index;
