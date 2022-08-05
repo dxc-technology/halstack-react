aa6a7a933b44a3bc06f205c0011730dee1cb8f52
GomezIvann <UO258748@uniovi.es>
2022-08-05 09:45:56 +0200

    First version of the Flex component doc

---
+++ website/screens/components/flex/FlexPageLayout.tsx
@@ -0,0 +1,28 @@
+import { DxcText, DxcFlex } from "@dxc-technology/halstack-react";
+import PageHeading from "@/common/PageHeading";
+import TabsPageHeading from "@/common/TabsPageLayout";
+import ComponentHeading from "@/common/ComponentHeading";
+
+const FlexPageHeading = ({ children }: { children: React.ReactNode }) => {
+  const tabs = [{ label: "Code", path: "/components/flex" }];
+
+  return (
+    <DxcFlex direction="column" gap="3rem">
+      <PageHeading>
+        <DxcFlex direction="column" gap="2rem">
+          <ComponentHeading name="Flex" status="Experimental" />
+          <DxcText as="p">
+            Flex allows users to build Flexible Box Module based layouts. It
+            serves as a technical component that abstracts users from working
+            directly with CSS Flexbox and helps them write more semantic
+            layouts.
+          </DxcText>
+          <TabsPageHeading tabs={tabs}></TabsPageHeading>
+        </DxcFlex>
+      </PageHeading>
+      {children}
+    </DxcFlex>
+  );
+};
+
+export default FlexPageHeading;
