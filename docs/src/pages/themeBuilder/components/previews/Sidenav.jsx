f380d3e8bba154779255161fa388e11d1c2f368a
GomezIvann <UO258748@uniovi.es>
2022-08-05 12:03:21 +0200

    Updating Sidenav examples in the Theme Builder

--- docs/src/pages/themeBuilder/components/previews/Sidenav.jsx
+++ docs/src/pages/themeBuilder/components/previews/Sidenav.jsx
@@ -6,31 +6,23 @@ import Mode from "../Mode";
 
 const SideNav = () => {
   return (
-    <SideNavContainer>
+    <SidenavContainer>
       <Mode text="Default">
         <DxcSidenav title={<DxcSidenav.Title>My sidenav</DxcSidenav.Title>}>
           <DxcSidenav.Section>
             <p>Lorem ipsum</p>
             <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
             <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
-            <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
-            <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
             <DxcSidenav.Group title="Group title" collapsable>
-              <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
-              <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
               <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
               <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
               <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
             </DxcSidenav.Group>
             <DxcSidenav.Group title="Group title" collapsable>
-              <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
-              <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
-              <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
               <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
               <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
             </DxcSidenav.Group>
           </DxcSidenav.Section>
-
           <DxcSidenav.Section>
             <p>Lorem ipsum</p>
             <DxcSidenav.Group title="Group title">
@@ -38,22 +30,70 @@ const SideNav = () => {
               <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
               <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
               <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
-              <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
             </DxcSidenav.Group>
             <DxcSidenav.Group title="Group title" collapsable>
-              <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
-              <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
-              <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
-              <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
               <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
             </DxcSidenav.Group>
           </DxcSidenav.Section>
         </DxcSidenav>
       </Mode>
-    </SideNavContainer>
+      <Mode text="With scroll">
+        <SidenavWithScroll>
+          <DxcSidenav title={<DxcSidenav.Title>My sidenav</DxcSidenav.Title>}>
+            <DxcSidenav.Section>
+              <p>Lorem ipsum</p>
+              <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
+              <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
+              <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
+              <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
+              <DxcSidenav.Group title="Group title" collapsable>
+                <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
+                <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
+                <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
+                <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
+                <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
+              </DxcSidenav.Group>
+              <DxcSidenav.Group title="Group title" collapsable>
+                <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
+                <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
+                <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
+                <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
+                <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
+              </DxcSidenav.Group>
+            </DxcSidenav.Section>
+            <DxcSidenav.Section>
+              <p>Lorem ipsum</p>
+              <DxcSidenav.Group title="Group title">
+                <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
+                <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
+                <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
+                <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
+                <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
+              </DxcSidenav.Group>
+              <DxcSidenav.Group title="Group title" collapsable>
+                <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
+                <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
+                <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
+                <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
+                <DxcSidenav.Link href="#">Lorem ipsum</DxcSidenav.Link>
+              </DxcSidenav.Group>
+            </DxcSidenav.Section>
+          </DxcSidenav>
+        </SidenavWithScroll>
+      </Mode>
+    </SidenavContainer>
   );
 };
 
-const SideNavContainer = styled.div``;
+const SidenavContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const SidenavWithScroll = styled.div`
+  height: 500px;
+  margin-bottom: 50px;
+`;
 
 export default SideNav;
