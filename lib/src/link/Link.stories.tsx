2c6f90d2f52b09ab7de212906c80161bd8fd6026
marcialfps <m.parrilla@dxc.com>
2022-08-25 12:10:32 +0200

    Add icon after Link in story

--- lib/src/link/Link.stories.tsx
+++ lib/src/link/Link.stories.tsx
@@ -75,7 +75,11 @@ export const Chromatic = () => (
       ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
       aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
       consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit{" "}
+      <DxcLink href="https://www.google.com" icon={icon} iconPosition="after">
+        Test
+      </DxcLink>{" "}
+      anim id est laborum.
     </ExampleContainer>
     <ExampleContainer pseudoState="pseudo-focus">
       <Title title="Long text with focus" theme="light" level={4} />
