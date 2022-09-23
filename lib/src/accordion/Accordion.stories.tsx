0e3be1628d1dfaea973101bd2b610e11dd27d93b
raquelarrojo <arrojoraquel@gmail.com>
2022-09-23 12:51:23 +0200

    Test accordion

--- lib/src/accordion/Accordion.stories.tsx
+++ lib/src/accordion/Accordion.stories.tsx
@@ -314,5 +314,5 @@ export const FocusedAccordion = () => (
 );
 
 FocusedAccordion.parameters = {
-  chromatic: { diffThreshold: 0.2, diffIncludeAntiAliasing: true },
+  chromatic: { diffThreshold: 0.1 },
 };
