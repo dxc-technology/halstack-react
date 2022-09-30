39d9abd16b37aecb4ed68233b3ca404beda7766e
agonzalez97 <aidamaria.gonzalez@dxc.com>
2022-09-30 11:23:00 +0200

    Added router for redirecting

--- website/pages/overview/introduction.tsx
+++ website/pages/overview/introduction.tsx
@@ -1,7 +1,16 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import IntroductionPage from "../../screens/overview/introduction/IntroductionPage";
 
 const Introduction = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const { asPath } = router;
+    asPath === "/" && router.push("/overview/introduction");
+  }, [router]);
+
   return (
     <>
       <Head>
