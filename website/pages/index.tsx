a37685caedbc6007547aa231b3b437373bbdde9a
agonzalez97 <aidamaria.gonzalez@dxc.com>
2022-10-06 09:05:39 +0200

    Removed spinner

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,7 +1,5 @@
-import { DxcSpinner } from "@dxc-technology/halstack-react";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import styled from "styled-components";
 
 export default function Index() {
   const { asPath, push } = useRouter();
@@ -10,15 +8,5 @@ export default function Index() {
     asPath === "/" && push("/overview/introduction");
   }, [asPath]);
 
-  return (
-    <LoadingContainer>
-      {/* <DxcSpinner mode="overlay" label="Loading..."></DxcSpinner> */}
-    </LoadingContainer>
-  );
+  return <></>;
 }
-
-const LoadingContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
