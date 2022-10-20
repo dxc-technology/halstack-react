89eae41ba812b299a10bddfe9746e96008ff7c58
Iván Gómez Pinta <44321109+GomezIvann@users.noreply.github.com>
2022-10-20 09:46:14 +0200

    Update types.ts

--- lib/src/box/types.ts
+++ lib/src/box/types.ts
@@ -34,7 +34,6 @@ type Props = {
    * @deprecated This prop will be removed shortly, consider using the Inset component for this purpose.
    * Size of the padding to be applied to the custom area ('xxsmall' | 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge' | 'xxlarge').
    * You can pass an object with 'top', 'bottom', 'left' and 'right' properties in order to specify different padding sizes.
-   * The prop will be removed, use Inset instead.
    */
   padding?: Space | Padding;
   /**
