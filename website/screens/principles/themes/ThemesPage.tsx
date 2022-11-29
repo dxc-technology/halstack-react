73c1336353a81e1cc2496833d7b299505b1570db
agonzalez97 <aidamaria.gonzalez@dxc.com>
2022-11-29 16:34:23 +0100

    Updated theme inputs for select component

--- website/screens/principles/themes/ThemesPage.tsx
+++ website/screens/principles/themes/ThemesPage.tsx
@@ -633,9 +633,6 @@ const sections = [
               <tr>
                 <td>Font color</td>
                 <td>
-                  <Code>listOptionFontColor</Code>
-                  <br />
-                  <br />
                   <Code>valueFontColor</Code>
                   <br />
                   <br />
@@ -649,6 +646,12 @@ const sections = [
                   <br />
                   <br />
                   <Code>collapseIndicatorColor</Code>
+                </td>
+              </tr>
+              <tr>
+                <td>Option Font color</td>
+                <td>
+                  <Code>listOptionFontColor</Code>
                 </td>
               </tr>
             </tbody>
