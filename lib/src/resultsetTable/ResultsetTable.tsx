6efd68f016ad109563eba2d17f5e0fdf0438e562
GomezIvann <UO258748@uniovi.es>
2022-12-28 13:56:35 +0100

    Fixes in Resultset table

--- lib/src/resultsetTable/ResultsetTable.tsx
+++ lib/src/resultsetTable/ResultsetTable.tsx
@@ -1,18 +1,17 @@
-// @ts-nocheck
 import React, { useState, useMemo, useEffect } from "react";
 import styled, { ThemeProvider } from "styled-components";
 import { spaces } from "../common/variables.js";
 import DxcTable from "../table/Table";
 import DxcPaginator from "../paginator/Paginator";
 import useTheme from "../useTheme";
-import ResultsetTablePropsType from "./types";
+import ResultsetTablePropsType, { Margin, Space } from "./types";
+import icons from "./Icons";
+import { getMargin } from "../common/utils.js";
 
-function normalizeSortValue(sortValue) {
-  return typeof sortValue === "string" ? sortValue.toUpperCase() : sortValue;
-}
+const normalizeSortValue = (sortValue) => (typeof sortValue === "string" ? sortValue.toUpperCase() : sortValue);
 
-function sortArray(index, order, resultset) {
-  return resultset.slice().sort((element1, element2) => {
+const sortArray = (index, order, resultset) =>
+  resultset.slice().sort((element1, element2) => {
     const sortValueA = normalizeSortValue(element1[index].sortValue || element1[index].displayValue);
     const sortValueB = normalizeSortValue(element2[index].sortValue || element2[index].displayValue);
     let comparison = 0;
@@ -25,35 +24,14 @@ function sortArray(index, order, resultset) {
     } else if (sortValueA < sortValueB) {
       comparison = -1;
     }
-    return order === "desc" ? comparison * -1 : comparison;
+    return order === "descending" ? comparison * -1 : comparison;
   });
-}
+
 const getMinItemsPerPageIndex = (currentPageInternal, itemsPerPage, page) =>
   currentPageInternal === 1 ? 0 : itemsPerPage * (page - 1);
 
 const getMaxItemsPerPageIndex = (minItemsPerPageIndex, itemsPerPage, resultset, page) =>
   minItemsPerPageIndex + itemsPerPage > resultset.length ? resultset.length : itemsPerPage * page - 1;
-
-const ArrowUp = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" fill="currentColor">
-    <path d="M0 0h24v24H0V0z" fill="none" />
-    <path d="M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z" />
-  </svg>
-);
-
-const ArrowDown = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" fill="currentColor">
-    <path d="M0 0h24v24H0V0z" fill="none" />
-    <path d="M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z" />
-  </svg>
-);
-
-const BothArrows = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" fill="currentColor">
-    <path d="M0 0h24v24H0z" fill="none" />
-    <path d="M12 5.83L15.17 9l1.41-1.41L12 3 7.41 7.59 8.83 9 12 5.83zm0 12.34L8.83 15l-1.41 1.41L12 21l4.59-4.59L15.17 15 12 18.17z" />
-  </svg>
-);
 
 const DxcResultsetTable = ({
   columns,
@@ -67,39 +45,16 @@ const DxcResultsetTable = ({
 }: ResultsetTablePropsType): JSX.Element => {
   const colorsTheme = useTheme();
   const [page, changePage] = useState(1);
-  const [sortColumnIndex, changeSortColumnIndex] = useState("");
-  const [sortOrder, changeSortOrder] = useState("asc");
+  const [sortColumnIndex, changeSortColumnIndex] = useState(-1);
+  const [sortOrder, changeSortOrder] = useState<"ascending" | "descending">("ascending");
 
   const minItemsPerPageIndex = useMemo(() => getMinItemsPerPageIndex(page, itemsPerPage, page), [itemsPerPage, page]);
   const maxItemsPerPageIndex = useMemo(
     () => getMaxItemsPerPageIndex(minItemsPerPageIndex, itemsPerPage, rows, page),
     [itemsPerPage, minItemsPerPageIndex, page, rows]
   );
-
-  const goToPage = (newPage) => {
-    changePage(newPage);
-  };
-  const changeSorting = (columnIndex) => {
-    changePage(1);
-    changeSortColumnIndex(columnIndex);
-    changeSortOrder(
-      sortColumnIndex === "" || sortColumnIndex !== columnIndex ? "asc" : sortOrder === "asc" ? "desc" : "asc"
-    );
-  };
-  const getIconForSortableColumn = (clickedColumnIndex) => {
-    return sortColumnIndex === clickedColumnIndex ? sortOrder === "asc" ? <ArrowUp /> : <ArrowDown /> : <BothArrows />;
-  };
-
-  useEffect(() => {
-    if (rows.length > 0) {
-      changePage(1);
-    } else {
-      changePage(0);
-    }
-  }, [rows.length, itemsPerPage]);
-
   const sortedResultset = useMemo(
-    () => (sortColumnIndex !== "" ? sortArray(sortColumnIndex, sortOrder, rows) : rows),
+    () => (sortColumnIndex !== -1 ? sortArray(sortColumnIndex, sortOrder, rows) : rows),
     [sortColumnIndex, sortOrder, rows]
   );
   const filteredResultset = useMemo(
@@ -107,107 +62,90 @@ const DxcResultsetTable = ({
     [sortedResultset, minItemsPerPageIndex, maxItemsPerPageIndex]
   );
 
+  const goToPage = (newPage: number) => {
+    changePage(newPage);
+  };
+  
+  const changeSorting = (columnIndex: number) => {
+    changePage(1);
+    changeSortColumnIndex(columnIndex);
+    changeSortOrder(
+      sortColumnIndex === -1 || sortColumnIndex !== columnIndex
+        ? "ascending"
+        : sortOrder === "ascending"
+        ? "descending"
+        : "ascending"
+    );
+  };
+
+  useEffect(() => {
+    rows.length > 0 ? changePage(1) : changePage(0);
+  }, [rows]);
+
   return (
     <ThemeProvider theme={colorsTheme.table}>
       <DxcResultsetTableContainer margin={margin}>
-        <TableContainer>
-          <DxcTable>
-            <HeaderRow>
-              <tr>
-                {columns.map((column, index) => (
-                  <TableHeader key={`tableHeader_${index}`}>
-                    <HeaderContainer
-                      key={`headerContainer_${index}`}
-                      onClick={() => column.isSortable && changeSorting(index)}
-                      tabIndex={column.isSortable ? tabIndex : -1}
-                      isSortable={column.isSortable}
-                    >
-                      <TitleDiv isSortable={column.isSortable}>{column.displayValue}</TitleDiv>
-                      {column.isSortable && <SortIcon>{getIconForSortableColumn(index)}</SortIcon>}
-                    </HeaderContainer>
-                  </TableHeader>
+        <DxcTable>
+          <thead>
+            <tr>
+              {columns.map((column, index) => (
+                <th
+                  key={`tableHeader_${index}`}
+                  aria-sort={column.isSortable ? (sortColumnIndex === index ? sortOrder : "none") : undefined}
+                >
+                  <HeaderContainer
+                    role={column.isSortable ? "button" : undefined}
+                    key={`headerContainer_${index}`}
+                    onClick={() => {
+                      column.isSortable && changeSorting(index);
+                    }}
+                    tabIndex={column.isSortable ? tabIndex : -1}
+                    isSortable={column.isSortable}
+                  >
+                    <span>{column.displayValue}</span>
+                    {column.isSortable && (
+                      <SortIcon>
+                        {sortColumnIndex === index
+                          ? sortOrder === "ascending"
+                            ? icons.arrowUp
+                            : icons.arrowDown
+                          : icons.bothArrows}
+                      </SortIcon>
+                    )}
+                  </HeaderContainer>
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {filteredResultset.map((cells, index) => (
+              <tr key={`resultSetTableCell_${index}`}>
+                {cells.map((cellContent, index) => (
+                  <td key={`resultSetTableCellContent_${index}`}>{cellContent.displayValue}</td>
                 ))}
               </tr>
-            </HeaderRow>
-            <TableRowGroup>
-              {filteredResultset.map((cells, index) => (
-                <tr key={`resultSetTableCell_${index}`}>
-                  {cells.map((cellContent, index) => (
-                    <td key={`resultSetTableCellContent_${index}`}>{cellContent.displayValue}</td>
-                  ))}
-                </tr>
-              ))}
-            </TableRowGroup>
-          </DxcTable>
-        </TableContainer>
-        <PaginatorContainer>
-          <DxcPaginator
-            totalItems={rows.length}
-            itemsPerPage={itemsPerPage}
-            itemsPerPageOptions={itemsPerPageOptions}
-            itemsPerPageFunction={itemsPerPageFunction}
-            currentPage={page}
-            showGoToPage={showGoToPage}
-            onPageChange={goToPage}
-            tabIndex={tabIndex}
-          />
-        </PaginatorContainer>
+            ))}
+          </tbody>
+        </DxcTable>
+        <DxcPaginator
+          totalItems={rows.length}
+          itemsPerPage={itemsPerPage}
+          itemsPerPageOptions={itemsPerPageOptions}
+          itemsPerPageFunction={itemsPerPageFunction}
+          currentPage={page}
+          showGoToPage={showGoToPage}
+          onPageChange={goToPage}
+          tabIndex={tabIndex}
+        />
       </DxcResultsetTableContainer>
     </ThemeProvider>
   );
 };
-const TableContainer = styled.div`
-  & table {
-    table-layout: auto;
-  }
-`;
-const PaginatorContainer = styled.div``;
-const TableRowGroup = styled.tbody`
-  > div:nth-child(1) {
-    position: absolute;
-    left: calc(50% - 68.5px);
-    bottom: calc(50% - 68.5px - 30px);
-  }
-`;
-const SortIcon = styled.div`
-  top: 409px;
-  left: 390px;
-  height: 14px;
-  cursor: pointer;
-  color: ${(props) => props.theme.sortIconColor};
 
-  svg {
-    height: 100%;
-    width: 100%;
-  }
-`;
+const calculateWidth = (margin) => `calc(100% - ${getMargin(margin, "left")} - ${getMargin(margin, "right")})`;
 
-const TitleDiv = styled.div`
-  cursor: ${(props) => (props.isSortable && "pointer") || "default"};
-`;
-const TableHeader = styled.th``;
-const HeaderContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: ${(props) =>
-    props.theme.headerTextAlign === "center"
-      ? "center"
-      : props.theme.headerTextAlign === "right"
-      ? "flex-end"
-      : "flex-start"};
-  width: fit-content;
-  :focus {
-    ${(props) =>
-      props.isSortable &&
-      `outline: #0095ff solid 2px; 
-        outline-offset: 4px;`}
-  }
-`;
-const HeaderRow = styled.thead`
-  height: 60px;
-`;
-const DxcResultsetTableContainer = styled.div`
-  font-size: ${(props) => props.theme.fontSizeBase};
+const DxcResultsetTableContainer = styled.div<{ margin: Space | Margin }>`
+  width: ${(props) => calculateWidth(props.margin)};
   margin: ${(props) => (props.margin && typeof props.margin !== "object" ? spaces[props.margin] : "0px")};
   margin-top: ${(props) =>
     props.margin && typeof props.margin === "object" && props.margin.top ? spaces[props.margin.top] : ""};
@@ -217,6 +155,41 @@ const DxcResultsetTableContainer = styled.div`
     props.margin && typeof props.margin === "object" && props.margin.bottom ? spaces[props.margin.bottom] : ""};
   margin-left: ${(props) =>
     props.margin && typeof props.margin === "object" && props.margin.left ? spaces[props.margin.left] : ""};
+`;
+
+const HeaderContainer = styled.span<{ isSortable: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: ${(props) =>
+    props.theme.headerTextAlign === "center"
+      ? "center"
+      : props.theme.headerTextAlign === "right"
+      ? "flex-end"
+      : "flex-start"};
+  gap: 8px;
+  width: fit-content;
+  border: 1px solid transparent;
+  border-radius: 2px;
+  padding: 3px;
+  cursor: ${(props) => (props.isSortable ? "pointer" : "default")};
+
+  ${(props) =>
+    props.isSortable &&
+    `&:focus {
+      outline: #0095ff solid 2px;
+    }`}
+`;
+
+const SortIcon = styled.span`
+  display: flex;
+  height: 14px;
+  width: 14px;
+  color: ${(props) => props.theme.sortIconColor};
+
+  svg {
+    height: 100%;
+    width: 100%;
+  }
 `;
 
 export default DxcResultsetTable;
