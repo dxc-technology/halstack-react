14206af0e00564acd6f9037c80d76e9c5a8ae074
GomezIvann <UO258748@uniovi.es>
2023-01-18 16:42:21 +0100

    Updating the icons of the Paginator

---
+++ lib/src/paginator/Icons.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const firstIcon = (
+  <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" fill="currentColor">
+    <path d="M6 18V6h2v12Zm11 0-6-6 6-6 1.4 1.4-4.6 4.6 4.6 4.6Z" />
+  </svg>
+);
+
+export const previousIcon = (
+  <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" fill="currentColor">
+    <path d="m14 18-6-6 6-6 1.4 1.4-4.6 4.6 4.6 4.6Z" />
+  </svg>
+);
+
+export const nextIcon = (
+  <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" fill="currentColor">
+    <path d="M9.4 18 8 16.6l4.6-4.6L8 7.4 9.4 6l6 6Z" />
+  </svg>
+);
+
+export const lastIcon = (
+  <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" fill="currentColor">
+    <path d="m7 18-1.4-1.4 4.6-4.6-4.6-4.6L7 6l6 6Zm9 0V6h2v12Z" />
+  </svg>
+);
