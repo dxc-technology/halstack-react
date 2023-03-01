5e7dadbf72c0668d58f187a95eeb1c5dea5025b1
agonzalez97 <aidamaria.gonzalez@dxc.com>
2023-03-01 16:24:52 +0100

    Testing function

--- scripts/create-catalog.js
+++ scripts/create-catalog.js
@@ -49,18 +49,16 @@ const componentsLinks = [
 ];
 
 const setCatalog = () => {
-  console.log("creating catalog");
   let catalog = [];
   componentsLinks.forEach((el) => {
     let component = {
-      key: el.split("/")[2],
+      key: el.path.split("/")[2],
       name: el.label,
     };
     catalog.push(component);
   });
-  console.log("catalog: ", catalog);
   const jsonData = JSON.stringify(catalog);
-  fs.writeFile("./catalog/catalog.json", jsonData, (err) => {
+  fs.writeFile("../catalog/catalog.json", jsonData, (err) => {
     if (err) throw err;
     console.log("Data written to file");
   });
