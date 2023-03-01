e73bccd4c7889fb18954e545e5f49d045750aea1
agonzalez97 <aidamaria.gonzalez@dxc.com>
2023-03-01 17:44:27 +0100

    Changed directory script

---
+++ website/scripts/create-catalog.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+
+const componentsLinks = require("../screens/common/pagesList").componentsLinks;
+
+const setCatalog = () => {
+  let catalog = [];
+  componentsLinks.forEach((el) => {
+    let component = {
+      key: el.path.split("/")[2],
+      name: el.label,
+    };
+    catalog.push(component);
+  });
+  const jsonData = JSON.stringify(catalog);
+  fs.writeFile("catalog/catalog.json", jsonData, (err) => {
+    if (err) throw err;
+    console.log("Data written to file");
+  });
+};
+
+setCatalog();
