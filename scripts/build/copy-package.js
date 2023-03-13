b169f8e230ae4ec00125c0dc574445d56fc999a2
Jialecl <jialestrabajos@gmail.com>
2023-03-13 12:28:03 +0100

    Adding transform to copy package

--- scripts/build/copy-package.js
+++ scripts/build/copy-package.js
@@ -1,5 +1,15 @@
 const fs = require("fs");
+const { Transform } = require("stream");
 
-fs.createReadStream("./package.json").pipe(
-  fs.createWriteStream("../../dist/package.json")
-);
+const uppercase = new Transform({
+  transform(chunk, encoding, callback) {
+    const content = JSON.parse(chunk);
+    content.main = "./main.js";
+    content.types = "./main.d.ts";
+    callback(null, JSON.stringify(content, null, 2));
+  },
+});
+
+fs.createReadStream("./package.json")
+  .pipe(uppercase)
+  .pipe(fs.createWriteStream("../../dist/package.json"));
