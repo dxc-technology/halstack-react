4a3bcf2ea5187e942b897466dd5bf37d3c30952c
agonzalez97 <aidamaria.gonzalez@dxc.com>
2023-03-22 13:25:24 +0100

    Removed array

--- scripts/create-version.js
+++ scripts/create-version.js
@@ -2,11 +2,9 @@ const fs = require("fs");
 
 const setVersion = () => {
   const versionToDeploy = process.argv[2];
-  const object = [
-    {
-      version: versionToDeploy,
-    },
-  ];
+  const object = {
+    version: versionToDeploy,
+  };
   const jsonData = JSON.stringify(object);
   fs.writeFile("catalog/version/version.json", jsonData, (err) => {
     if (err) throw err;
