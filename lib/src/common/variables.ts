e4725058b1ad0943a248af72ecd275099da3f3f7
Iván Gómez Pinta <44321109+GomezIvann@users.noreply.github.com>
2023-09-15 15:06:37 +0200

    Fixing variables issue

--- lib/src/common/variables.ts
+++ lib/src/common/variables.ts
@@ -492,11 +492,11 @@ export const componentTokens = {
   },
   image: {
     captionFontColor: CORE_TOKENS.color_grey_900,
-    captionFontFamily: CORE_TOKENS.font_family_sans,
-    captionFontSize: CORE_TOKENS.font_scale_02,
-    captionFontStyle: CORE_TOKENS.font_style_normal,
-    captionFontWeight: CORE_TOKENS.font_weight_regular,
-    captionLineHeight: CORE_TOKENS.font_leading_normal,
+    captionFontFamily: CORE_TOKENS.type_sans,
+    captionFontSize: CORE_TOKENS.type_scale_02,
+    captionFontStyle: CORE_TOKENS.type_normal,
+    captionFontWeight: CORE_TOKENS.type_regular,
+    captionLineHeight: CORE_TOKENS.type_leading_normal,
   },
   link: {
     fontColor: CORE_TOKENS.color_blue_800,
