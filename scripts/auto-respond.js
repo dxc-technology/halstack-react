8e5c04d826d786a2eb4496db82dc9d68d035dd5c
Mil4n0r <morenocarmonaenrique@gmail.com>
2024-01-25 16:37:32 +0100

    Fixed octokit function

--- scripts/auto-respond.js
+++ scripts/auto-respond.js
@@ -1,15 +1,16 @@
-const { context, Octokit } = require('@actions/github');
+const { context, getOctokit } = require('@actions/github');
 
 const autoRespond = async () => {
   try {
-    const token = process.env.GITHUB_TOKEN;
-    const octokit = new Octokit({ auth: token });
+    const octokit = getOctokit(process.env.GITHUB_TOKEN);
+
     const issueComment =
       `Thank you for opening an issue! 🚀
       
       Our team will review it as soon as possible. In the meantime, please make sure that you've provided all the necessary details to help us understand and address the issue effectively.
       
       Feel free to contribute and participate in discussions!`;
+
     await octokit.issues.createComment({
       owner: context.repo.owner,
       repo: context.repo.repo,
