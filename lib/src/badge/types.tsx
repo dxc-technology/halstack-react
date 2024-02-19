bc9e154fa32b7b70e1d0547515a33f80fb7e7b09
Raquel Arrojo Lopez <rarrojolopez@dxc.com>
2024-02-19 09:40:06 +0100

    Change un badge types

--- lib/src/badge/types.tsx
+++ lib/src/badge/types.tsx
@@ -1,24 +1,5 @@
 type SVG = React.ReactNode & React.SVGProps<SVGSVGElement>;
 
-type NotificationProps = {
-  /**
-   * Text to be placed in the badge.
-   */
-  label?: number;
-  /**
-   * The available badge modes.
-   */
-  mode: "notification";
-  /**
-   * In notification mode, if the number entered as label is greater that this notification limit, +99 will be shown. If not, the entered text will be shown as label.
-   */
-  notificationLimit?: number;
-  /**
-   * Affects the visual style of the badge. It can be used following semantic purposes or not.
-   */
-  color?: never;
-};
-
 type ContextualProps = {
   /**
    * Text to be placed in the badge.
@@ -36,6 +17,25 @@ type ContextualProps = {
    * Affects the visual style of the badge. It can be used following semantic purposes or not.
    */
   color?: "grey" | "blue" | "green" | "orange" | "red" | "yellow" | "purple";
+};
+
+type NotificationProps = {
+  /**
+   * Text to be placed in the badge.
+   */
+  label?: number;
+  /**
+   * The available badge modes.
+   */
+  mode: "notification";
+  /**
+   * In notification mode, if the number entered as label is greater that this notification limit, +99 will be shown. If not, the entered text will be shown as label.
+   */
+  notificationLimit?: number;
+  /**
+   * Affects the visual style of the badge. It can be used following semantic purposes or not.
+   */
+  color?: never;
 };
 
 type CommonProps = {
