1cafebacf67e6c1b0fce371b4b2cf3fb6ecbff6f
Mil4n0r <morenocarmonaenrique@gmail.com>
2024-02-28 14:49:12 +0100

    Fixed styled components dynamic warning

--- lib/.storybook/components/Title.tsx
+++ lib/.storybook/components/Title.tsx
@@ -8,14 +8,17 @@ type Props = {
 };
 
 const Title = ({ title, theme, level = 4 }: Props): JSX.Element => {
-  const HeadingLevel = `h${level}`;
-
-  const SectionTitle = styled[HeadingLevel]`
-    font-family: Open Sans, sans-serif;
-    color: ${(props) => (props.theme === "dark" ? "#FFFFFF" : "#000000")};
-  `;
-
-  return <SectionTitle theme={theme}>{title}</SectionTitle>;
+  const headingLevel = `h${level}` as keyof JSX.IntrinsicElements;
+  return (
+    <SectionTitle theme={theme} as={headingLevel}>
+      {title}
+    </SectionTitle>
+  );
 };
+
+const SectionTitle = styled.div`
+  font-family: Open Sans, sans-serif;
+  color: ${(props) => (props.theme === "dark" ? "#FFFFFF" : "#000000")};
+`;
 
 export default Title;
