15cb0a3f41abae6fd31d603292dc366c9876e715
Mil4n0r <morenocarmonaenrique@gmail.com>
2024-02-28 14:40:18 +0100

    Fixed Title component for storybook

--- lib/.storybook/components/Title.tsx
+++ lib/.storybook/components/Title.tsx
@@ -2,27 +2,20 @@ import React from "react";
 import styled from "styled-components";
 
 type Props = {
-  title?: string,
-  theme?: string,
-  level?: number,
+  title?: string;
+  theme?: string;
+  level?: number;
 };
 
-const Title = ({ title, theme, level }: Props): JSX.Element => {
-  return level === 2 ? (
-    <SectionTitle theme={theme}>{title}</SectionTitle>
-  ) : (
-    <ExampleTitle theme={theme}>{title}</ExampleTitle>
-  );
+const Title = ({ title, theme, level = 4 }: Props): JSX.Element => {
+  const HeadingLevel = `h${level}`;
+
+  const SectionTitle = styled[HeadingLevel]`
+    font-family: Open Sans, sans-serif;
+    color: ${(props) => (props.theme === "dark" ? "#FFFFFF" : "#000000")};
+  `;
+
+  return <SectionTitle theme={theme}>{title}</SectionTitle>;
 };
-
-const SectionTitle = styled.h2`
-  font-family: Open Sans, sans-serif;
-  color: ${(props) => (props.theme === "dark" ? "#FFFFFF" : "#000000")};
-`;
-
-const ExampleTitle = styled.h4`
-  font-family: Open Sans, sans-serif;
-  color: ${(props) => (props.theme === "dark" ? "#FFFFFF" : "#000000")};
-`;
 
 export default Title;
