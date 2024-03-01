b31799b7a8d0d4f941129df968c2419a067bfdf2
Mil4n0r <morenocarmonaenrique@gmail.com>
2024-03-01 14:46:51 +0100

    Updated test-runner config to match global config

--- lib/.storybook/test-runner.ts
+++ lib/.storybook/test-runner.ts
@@ -1,12 +1,23 @@
-import { injectAxe, checkA11y } from "axe-playwright";
+import { injectAxe, checkA11y, configureAxe } from "axe-playwright";
 
-import type { TestRunnerConfig } from "@storybook/test-runner";
+import { getStoryContext, type TestRunnerConfig } from "@storybook/test-runner";
 
 const a11yConfig: TestRunnerConfig = {
   async preVisit(page) {
     await injectAxe(page);
   },
-  async postVisit(page) {
+  async postVisit(page, context) {
+    // Get the entire context of a story, including parameters, args, argTypes, etc.
+    const storyContext = await getStoryContext(page, context);
+    // Do not run a11y tests on disabled stories.
+    if (storyContext.parameters?.a11y?.disable) {
+      return;
+    }
+    // Apply story-level a11y rules
+    await configureAxe(page, {
+      rules: storyContext.parameters?.a11y?.config?.rules,
+    });
+
     await checkA11y(page, "#storybook-root", {
       detailedReport: true,
       detailedReportOptions: {
