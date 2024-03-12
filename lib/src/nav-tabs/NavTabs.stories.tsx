a65101981563e081a8bdf2e617303a755f7faec7
Aitor Echevarría <aechevarriaf@dxc.com>
2024-03-12 17:37:09 +0100

    Keeping an SVG example

--- lib/src/nav-tabs/NavTabs.stories.tsx
+++ lib/src/nav-tabs/NavTabs.stories.tsx
@@ -9,6 +9,14 @@ export default {
   component: DxcNavTabs,
 };
 
+const iconSVG = (
+  <svg viewBox="0 0 24 24" fill="currentColor">
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+  </svg>
+);
+
+
 const favoriteIcon = 'filled_Favorite'
 
 const pinIcon = 'Location_On';
@@ -94,10 +102,10 @@ export const Chromatic = () => (
     <ExampleContainer>
       <Title title="With icon position top" theme="light" level={4} />
       <DxcNavTabs>
-        <DxcNavTabs.Tab href="#" active icon={favoriteIcon}>
+        <DxcNavTabs.Tab href="#" active icon={iconSVG}>
           Tab 1
         </DxcNavTabs.Tab>
-        <DxcNavTabs.Tab href="#" disabled icon={favoriteIcon}>
+        <DxcNavTabs.Tab href="#" disabled icon={iconSVG}>
           Tab 2
         </DxcNavTabs.Tab>
         <DxcNavTabs.Tab href="#" icon={pinIcon}>
