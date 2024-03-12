a5d4b72f4d623b052496108db3402d78eb6349e0
Mil4n0r <morenocarmonaenrique@gmail.com>
2024-03-12 10:45:33 +0100

    Refactoring accessibility testing settings

--- lib/.storybook/preview.tsx
+++ lib/.storybook/preview.tsx
@@ -2,6 +2,8 @@ import { HalstackProvider } from "../src/HalstackContext";
 import styled from "styled-components";
 import type { Preview } from "@storybook/react";
 import React from "react";
+import { disabledRules } from "../test/accessibility/disabledRules";
+
 
 const preview: Preview = {
   parameters: {
@@ -14,18 +16,8 @@ const preview: Preview = {
     },
     a11y: {
       config: {
-        rules: [
-          {
-            // Disable heading order rule to prevent errors from using h2 and h4 in the titles of the stories
-            id: "heading-order",
-            enabled: false,
-          },
-          {
-            // Disable autocomplete valid rule to prevent errors from "nope" which is used on purpose as an invalid autocomplete value
-            id: "autocomplete-valid",
-            enabled: false,
-          },
-        ],
+        rules: 
+        disabledRules.map(ruleId => ({id: ruleId, enabled: false}))
       },
       options: {},
     },
