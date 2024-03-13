ff864d6a5b831078c4f3a45c4815367a03aec1dc
Iván Gómez Pinta <44321109+GomezIvann@users.noreply.github.com>
2024-03-13 15:59:41 +0100

    More tests added to the Contextual Menu

--- lib/src/contextual-menu/GroupItem.tsx
+++ lib/src/contextual-menu/GroupItem.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useMemo, useState } from "react";
 import styled from "styled-components";
 import CoreTokens from "../common/coreTokens";
 import { GroupItemProps } from "./types";
@@ -7,28 +7,33 @@ import ItemAction from "./ItemAction";
 import { ContextualMenuContext } from "./ContextualMenu";
 import DxcIcon from "../icon/Icon";
 
-const isGroupSelected = (items: GroupItemProps["items"], selectedItemId: number) =>
+const isGroupSelected = (items: GroupItemProps["items"], selectedItemId: number): boolean =>
   items.some((item) => ("id" in item ? item.id === selectedItemId : isGroupSelected(item.items, selectedItemId)));
 
 const GroupItem = ({ ...props }: GroupItemProps) => {
-  const menuId = `menu-${props.label}`;
+  const groupMenuId = `group-menu-${props.label}`;
   const [isOpen, setIsOpen] = useState(false);
   const { selectedItemId } = useContext(ContextualMenuContext);
+  const selected = useMemo(
+    () => !isOpen && isGroupSelected(props.items, selectedItemId),
+    [isOpen, props.items, selectedItemId]
+  );
 
   return (
     <>
       <ItemAction
-        aria-controls={menuId}
+        aria-controls={groupMenuId}
         aria-expanded={isOpen ? true : undefined}
+        aria-selected={selected}
         collapseIcon={isOpen ? <DxcIcon icon="filled_expand_more" /> : <DxcIcon icon="filled_expand_more" />}
         onClick={() => {
           setIsOpen((isOpen) => !isOpen);
         }}
-        selected={!isOpen && isGroupSelected(props.items, selectedItemId)}
+        selected={selected}
         {...props}
       />
       {isOpen && (
-        <ItemsList id={menuId}>
+        <ItemsList id={groupMenuId}>
           {props.items.map((item) => (
             <MenuItem item={item} level={props.level + 1} />
           ))}
