f2545224854415d5291446e0da1774699d94d76d
Iván Gómez Pinta <44321109+GomezIvann@users.noreply.github.com>
2024-03-20 12:55:24 +0100

    Adding Contextual Menu definition

--- website/screens/components/contextual-menu/ContextualMenuPageLayout.tsx
+++ website/screens/components/contextual-menu/ContextualMenuPageLayout.tsx
@@ -1,12 +1,13 @@
-import {
-  DxcParagraph,
-  DxcFlex,
-} from "@dxc-technology/halstack-react";
+import { DxcParagraph, DxcFlex } from "@dxc-technology/halstack-react";
 import PageHeading from "@/common/PageHeading";
 import TabsPageHeading from "@/common/TabsPageLayout";
 import ComponentHeading from "@/common/ComponentHeading";
 
-const ContextualMenuPageHeading = ({ children }: { children: React.ReactNode }) => {
+const ContextualMenuPageHeading = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
   const tabs = [
     { label: "Code", path: "/components/contextual-menu" },
     { label: "Usage", path: "/components/contextual-menu/usage" },
@@ -19,7 +20,10 @@ const ContextualMenuPageHeading = ({ children }: { children: React.ReactNode }) 
         <DxcFlex direction="column" gap="2rem">
           <ComponentHeading name="Contextual Menu" />
           <DxcParagraph>
-            Pending definition.
+            The contextual menu is a powerful component that improves user
+            experience by allowing users to navigate through page-level content
+            or choose from a list of actions while complementing the general
+            disposition of the main content within the interface.
           </DxcParagraph>
           <TabsPageHeading tabs={tabs}></TabsPageHeading>
         </DxcFlex>
