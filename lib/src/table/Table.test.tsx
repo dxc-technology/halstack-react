838cbcaeee5610ac5c3bb9a74b47a6a827444c8d
Mil4n0r <morenocarmonaenrique@gmail.com>
2024-04-10 13:52:02 +0200

    Added actions type to prevent wrong inference

--- lib/src/table/Table.test.tsx
+++ lib/src/table/Table.test.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { act, render } from "@testing-library/react";
 import DxcTable from "./Table";
 import userEvent from "@testing-library/user-event";
+import { Actions } from "./types";
 
 (global as any).globalThis = global;
 (global as any).DOMRect = {
@@ -58,7 +59,7 @@ describe("Table component tests", () => {
   test("Table ActionsCell", async () => {
     const onSelectOption = jest.fn();
     const onClick = jest.fn();
-    const actions = [
+    const actions: Actions = [
       {
         title: "icon1",
         onClick: onSelectOption,
