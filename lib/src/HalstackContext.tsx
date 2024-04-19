0af7147a48c55d8af59a9c28e08da6e7a606d899
Jialecl <jialestrabajos@gmail.com>
2024-04-19 09:19:22 +0200

    Fonts css reverted due to issues with nextjs

--- lib/src/HalstackContext.tsx
+++ lib/src/HalstackContext.tsx
@@ -1,5 +1,6 @@
 import React, { useMemo } from "react";
 import Color from "color";
+import styled from "styled-components";
 import {
   AdvancedTheme,
   OpinionatedTheme,
@@ -383,11 +384,17 @@ const HalstackProvider = ({ theme, advancedTheme, labels, children }: HalstackPr
   const parsedLabels = useMemo(() => (labels ? parseLabels(labels) : defaultTranslatedComponentLabels), [labels]);
 
   return (
-    <HalstackContext.Provider value={parsedTheme}>
-      <HalstackLanguageContext.Provider value={parsedLabels}>{children}</HalstackLanguageContext.Provider>
-    </HalstackContext.Provider>
+    <Halstack>
+      <HalstackContext.Provider value={parsedTheme}>
+        <HalstackLanguageContext.Provider value={parsedLabels}>{children}</HalstackLanguageContext.Provider>
+      </HalstackContext.Provider>
+    </Halstack>
   );
 };
+
+const Halstack = styled.div`
+  @import url("https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap");
+`;
 
 export default HalstackContext;
 export { HalstackProvider, HalstackLanguageContext };
