69219812c7f217008d6a9d489daed25a29eced82
Mil4n0r <morenocarmonaenrique@gmail.com>
2024-05-02 09:39:43 +0200

    Removed unneeded type in Tooltip

--- lib/src/tooltip/types.tsx
+++ lib/src/tooltip/types.tsx
@@ -1,10 +1,8 @@
-type Position = "bottom" | "top" | "left" | "right";
-
 type Props = {
   /**
    * Preferred position for displaying the tooltip. It may adjust automatically based on available space.
    */
-  position?: Position;
+  position?: "bottom" | "top" | "left" | "right";
   /**
    * Text to be displayed inside the tooltip.
    */
