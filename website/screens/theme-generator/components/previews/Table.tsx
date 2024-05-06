9fde4b1673f9ac718401200e995607776f3f3f39
Jialecl <jialestrabajos@gmail.com>
2024-05-06 17:14:31 +0200

    Added tokens related to the actions cell to the documentation

--- website/screens/theme-generator/components/previews/Table.tsx
+++ website/screens/theme-generator/components/previews/Table.tsx
@@ -8,12 +8,14 @@ import {
 import Mode from "../Mode";
 import PreviewContainer from "./PreviewContainer";
 
-const iconSVG = (
-  <svg viewBox="0 0 24 24">
-    <path d="M0 0h24v24H0z" fill="none" />
-    <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
-  </svg>
-);
+const actions = [
+  {
+    icon: "delete",
+    title: "icon",
+    onClick: () => {},
+  },
+];
+
 const columns = [
   { displayValue: "Id", isSortable: false },
   { displayValue: "Name", isSortable: true },
@@ -25,7 +27,7 @@ const rows = [
     { displayValue: "001", sortValue: "001" },
     { displayValue: "Peter", sortValue: "Peter" },
     { displayValue: "Miami", sortValue: "Miami" },
-    { displayValue: <DxcButton mode="secondary" icon={iconSVG} /> },
+    { displayValue: <DxcResultsetTable.ActionsCell actions={actions} /> },
   ],
   [
     { displayValue: "002", sortValue: "002" },
@@ -37,19 +39,19 @@ const rows = [
     { displayValue: "003", sortValue: "003" },
     { displayValue: "Lana", sortValue: "Lana" },
     { displayValue: "Amsterdam", sortValue: "Amsterdam" },
-    { displayValue: <DxcButton mode="secondary" icon={iconSVG} /> },
+    { displayValue: <DxcResultsetTable.ActionsCell actions={actions} /> },
   ],
   [
     { displayValue: "004", sortValue: "004" },
     { displayValue: "Rick", sortValue: "Rick" },
     { displayValue: "London", sortValue: "London" },
-    { displayValue: <DxcButton mode="secondary" icon={iconSVG} /> },
+    { displayValue: <DxcResultsetTable.ActionsCell actions={actions} /> },
   ],
   [
     { displayValue: "005", sortValue: "005" },
     { displayValue: "Mark", sortValue: "Mark" },
     { displayValue: "Miami", sortValue: "Miami" },
-    { displayValue: <DxcButton mode="secondary" icon={iconSVG} /> },
+    { displayValue: <DxcResultsetTable.ActionsCell actions={actions} /> },
   ],
   [
     { displayValue: "006", sortValue: "006" },
