48f8179599378f7fb28e4b0572de4813a3c18f55
Mil4n0r <morenocarmonaenrique@gmail.com>
2024-05-07 10:42:00 +0200

    Removed await from hover/unhover

--- lib/src/tooltip/Tooltip.test.tsx
+++ lib/src/tooltip/Tooltip.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, waitFor } from "@testing-library/react";
+import { render, waitFor, screen, waitForElementToBeRemoved } from "@testing-library/react";
 import DxcTooltip from "./Tooltip";
 import DxcButton from "../button/Button";
 import userEvent from "@testing-library/user-event";
@@ -15,44 +15,19 @@ import userEvent from "@testing-library/user-event";
 };
 
 describe("Tooltip component tests", () => {
-  test("Tooltip does not render by default", async () => {
-    const { queryByRole } = render(
-      <DxcTooltip label="Tooltip Test">
-        <DxcButton label="Hoverable button" />
-      </DxcTooltip>
-    );
-    await waitFor(() => {
-      const tooltipElement = queryByRole("tooltip");
-      expect(tooltipElement).toBeFalsy();
-    });
-  });
-
-  test("Tooltip renders with correct label on hover", async () => {
-    const { getByText, getByRole } = render(
-      <DxcTooltip label="Tooltip Test">
-        <DxcButton label="Hoverable button" />
-      </DxcTooltip>
-    );
-    const triggerElement = getByText("Hoverable button");
-    userEvent.hover(triggerElement);
-    await waitFor(() => {
-      const tooltipContent = getByRole("tooltip", { name: "Tooltip Test" });
-      expect(tooltipContent).toBeTruthy();
-    });
-  });
-
-  test("Tooltip stops being rendered when hover is stopped", async () => {
+  test("Tooltip shows/unshows when hovering/unhovering", async () => {
     const { getByText, queryByRole } = render(
       <DxcTooltip label="">
         <DxcButton label="Hoverable button" />
       </DxcTooltip>
     );
     const triggerElement = getByText("Hoverable button");
+    expect(queryByRole("tooltip")).toBeFalsy();
     userEvent.hover(triggerElement);
+    expect(await screen.findByRole("tooltip", { name: "Tooltip Test" })).toBeTruthy();
     userEvent.unhover(triggerElement);
     await waitFor(() => {
-      const tooltipElement = queryByRole("tooltip");
-      expect(tooltipElement).toBeFalsy();
+      expect(queryByRole("tooltip")).toBeFalsy();
     });
   });
 });
