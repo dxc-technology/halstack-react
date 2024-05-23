e681c688d8ed58f2d20ade98067aeda82ca7c4fd
Iván Gómez Pinta <44321109+GomezIvann@users.noreply.github.com>
2024-05-23 11:29:24 +0200

    Updates to the bar chart component

--- lib/src/bar-chart/BarChart.stories.tsx
+++ lib/src/bar-chart/BarChart.stories.tsx
@@ -9,12 +9,85 @@ export default {
   component: DxcBarChart,
 };
 
+const series = [
+  {
+    title: "Series 1",
+    type: "bar",
+    data: [
+      { x: 0, y: 100000 },
+      { x: 1, y: 200000 },
+      { x: 2, y: 300000 },
+      { x: 3, y: 400000 },
+      { x: 4, y: 500000 },
+    ],
+  },
+  {
+    title: "Series 2",
+    type: "bar",
+    data: [
+      { x: 0, y: 50000 },
+      { x: 1, y: 100000 },
+      { x: 2, y: 150000 },
+      { x: 3, y: 200000 },
+      { x: 4, y: 250000 },
+    ],
+  },
+  {
+    title: "Series 3",
+    type: "bar",
+    data: [
+      { x: 0, y: 200000 },
+      { x: 1, y: 400000 },
+      { x: 2, y: 600000 },
+      { x: 3, y: 800000 },
+      { x: 4, y: 1000000 },
+    ],
+  },
+  {
+    title: "Series 4",
+    type: "bar",
+    data: [
+      { x: 0, y: 100000 },
+      { x: 1, y: 200000 },
+      { x: 2, y: 400000 },
+      { x: 3, y: 700000 },
+      { x: 4, y: 900000 },
+    ],
+  },
+  {
+    title: "Series 5",
+    type: "bar",
+    data: [
+      { x: 0, y: 50000 },
+      { x: 1, y: 500000 },
+      { x: 2, y: 800000 },
+      { x: 3, y: 900000 },
+      { x: 4, y: 100000 },
+    ],
+  },
+  {
+    title: "Series 6",
+    type: "bar",
+    data: [
+      { x: 0, y: 10000 },
+      { x: 1, y: 250000 },
+      { x: 2, y: 550000 },
+      { x: 3, y: 750000 },
+      { x: 4, y: 950000 },
+    ],
+  },
+  {
+    title: "Average",
+    type: "threshold",
+    y: 400000,
+  },
+];
+
 const BarChart = () => (
   <>
-    <Title title="Default" theme="light" level={4} />
+    <Title title="Basic bar chart" theme="light" level={4} />
     <ExampleContainer>
       <DxcBarChart
-        chartTitle="Bar Chart"
         series={[
           {
             title: "Series 1",
@@ -28,70 +101,62 @@ const BarChart = () => (
             ],
           },
           {
+            title: "Average",
+            type: "threshold",
+            y: 300000,
+          },
+        ]}
+        xDomain={[0, 1, 2, 3, 4]}
+        yDomain={[0, 700000]}
+      />
+    </ExampleContainer>
+    <Title title="Horizontal bar chart" theme="light" level={4} />
+    <ExampleContainer>
+      <DxcBarChart
+        series={[
+          {
+            title: "Series 1",
+            type: "bar",
+            data: [
+              { x: 0, y: 100000 },
+              { x: 1, y: 200000 },
+              { x: 2, y: 300000 },
+            ],
+          },
+          {
             title: "Series 2",
             type: "bar",
             data: [
               { x: 0, y: 50000 },
               { x: 1, y: 100000 },
               { x: 2, y: 150000 },
-              { x: 3, y: 200000 },
-              { x: 4, y: 250000 },
             ],
-          },
-          {
-            title: "Series 3",
-            type: "bar",
-            data: [
-              { x: 0, y: 200000 },
-              { x: 1, y: 400000 },
-              { x: 2, y: 600000 },
-              { x: 3, y: 800000 },
-              { x: 4, y: 1000000 },
-            ],
-          },
-          {
-            title: "Series 4",
-            type: "bar",
-            data: [
-              { x: 0, y: 100000 },
-              { x: 1, y: 200000 },
-              { x: 2, y: 400000 },
-              { x: 3, y: 700000 },
-              { x: 4, y: 900000 },
-            ],
-          },
-          {
-            title: "Series 5",
-            type: "bar",
-            data: [
-              { x: 0, y: 50000 },
-              { x: 1, y: 500000 },
-              { x: 2, y: 800000 },
-              { x: 3, y: 900000 },
-              { x: 4, y: 100000 },
-            ],
-          },
-          {
-            title: "Series 6",
-            type: "bar",
-            data: [
-              { x: 0, y: 10000 },
-              { x: 1, y: 250000 },
-              { x: 2, y: 550000 },
-              { x: 3, y: 750000 },
-              { x: 4, y: 950000 },
-            ],
-          },
-          {
-            title: "Average",
-            type: "threshold",
-            y: 400000,
           },
         ]}
+        xDomain={[0, 1, 2]}
+        yDomain={[0, 600000]}
+        horizontalBars
+      />
+    </ExampleContainer>
+    <Title title="Stacked bar chart" theme="light" level={4} />
+    <ExampleContainer>
+      <DxcBarChart series={series as any} xDomain={[0, 1, 2, 3, 4]} yDomain={[0, 5000000]} stackedBars />
+    </ExampleContainer>
+    <Title title="Stacked & horizontal bar chart" theme="light" level={4} />
+    <ExampleContainer>
+      <DxcBarChart series={series as any} xDomain={[0, 1, 2, 3, 4]} yDomain={[0, 5000000]} stackedBars horizontalBars />
+    </ExampleContainer>
+    <Title title="Complete bar chart" theme="light" level={4} />
+    <ExampleContainer>
+      <DxcBarChart
+        chartTitle="Bar Chart"
+        series={series as any}
         xDomain={[0, 1, 2, 3, 4]}
         yDomain={[0, 1000000]}
-        showFilter
+        xTitle="X Axis"
         yTitle="Y Axis"
+        showFilter
+        showLegend
         legendTitle="Legend"
       />
     </ExampleContainer>
@@ -206,8 +271,7 @@ const BarChart = () => (
 );
 
 export const Chromatic = BarChart.bind({});
-BarChart.play = async ({ canvasElement }) => {
+Chromatic.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement);
-  const filter = canvas.getAllByTitle("Clear selection")[1];
-  await userEvent.click(filter);
+  await userEvent.click(canvas.getAllByTitle("Clear selection")[1]);
 };
--- lib/src/bar-chart/BarChart.tsx
+++ lib/src/bar-chart/BarChart.tsx
@@ -1,33 +1,33 @@
-import React, { useEffect } from "react";
+import React, { memo } from "react";
 import { BarChart } from "@cloudscape-design/components";
 import { applyTheme } from "@cloudscape-design/components/theming";
 import { useState, useMemo, useCallback } from "react";
 import BarChartProps, { DataTypes, InsetWrapperProps } from "./types";
 import theme from "./theme";
 import styled, { css } from "styled-components";
-import { DxcSpinner, DxcTypography, DxcInset, DxcSelect, DxcButton, DxcFlex, DxcHeading } from "../main";
+import { DxcSpinner, DxcTypography, DxcInset, DxcSelect, DxcButton, DxcFlex, DxcHeading, DxcGrid } from "../main";
 import DxcIcon from "../icon/Icon";
 import CoreTokens from "../common/coreTokens";
 
+/**
+ * Cloudscape Design Components - Theming
+ * https://cloudscape.design/foundation/visual-foundation/theming/
+ */
 applyTheme({ theme });
 
-const EmptyStatus = () => (
-  <DxcFlex direction="column" alignItems="center" gap="0.25rem">
-    <DxcTypography fontWeight="600" color={CoreTokens.color_grey_800}>
-      No data available.
-    </DxcTypography>
-    <DxcTypography color={CoreTokens.color_grey_800}>There's not data available.</DxcTypography>
-  </DxcFlex>
-);
+const LoadingStatus = memo(() => (
+  <LoadingContainer>
+    <DxcSpinner mode="small" />
+    Loading data.
+  </LoadingContainer>
+));
 
-const NoMatchStatus = () => (
-  <DxcFlex direction="column" alignItems="center" gap="0.25rem">
-    <DxcTypography fontWeight="600" color={CoreTokens.color_grey_800}>
-      No matching data
-    </DxcTypography>
-    <DxcTypography color={CoreTokens.color_grey_800}>Try changing the filters or reloading the data.</DxcTypography>
-  </DxcFlex>
-);
+const StatusMessage = memo(({ title, description }: { title: string; description: string }) => (
+  <StatusMessageContainer>
+    <strong>{title}</strong>
+    {description}
+  </StatusMessageContainer>
+));
 
 const InsetWrapper = ({ condition, children }: InsetWrapperProps): JSX.Element => (
   <>{condition ? <DxcInset top="0.5rem">{children}</DxcInset> : children}</>
@@ -44,7 +44,7 @@ const DxcBarChart = <X extends DataTypes>({
   onRetry,
   series,
   showFilter,
-  showLegend = true,
+  showLegend,
   stackedBars,
   xDomain,
   xTitle,
@@ -64,24 +64,27 @@ const DxcBarChart = <X extends DataTypes>({
   );
 
   return loading ? (
-    <LoadingStatus>
-      <DxcSpinner mode="small" />
-      Loading data.
-    </LoadingStatus>
+    <LoadingStatus />
   ) : error ? (
     <ErrorStatus>
       <Label>
         <DxcIcon icon="cancel" />
         {error}
       </Label>
-      <DxcButton label="Retry" mode="secondary" onClick={() => onRetry()} />
+      <DxcButton
+        label="Retry"
+        mode="secondary"
+        onClick={() => {
+          onRetry();
+        }}
+      />
     </ErrorStatus>
   ) : (
-    <DxcFlex direction="column" gap="1rem">
+    <DxcGrid gap="1rem">
       {chartTitle && (
-        <DxcTypography textAlign="center">
-          <DxcHeading text={chartTitle} level={2} margin={{ bottom: "medium" }} />
-        </DxcTypography>
+        <DxcGrid.Item placeSelf="center">
+          <DxcHeading text={chartTitle} level={2} />
+        </DxcGrid.Item>
       )}
       {showFilter && (
         <DxcSelect
@@ -112,8 +115,10 @@ const DxcBarChart = <X extends DataTypes>({
           hideLegend={!showLegend}
           horizontalBars={horizontalBars}
           stackedBars={stackedBars}
-          empty={<EmptyStatus />}
-          noMatch={<NoMatchStatus />}
+          empty={<StatusMessage title="No data available" description="There's not data available." />}
+          noMatch={
+            <StatusMessage title="No matching data" description="Try changing the filters or reloading the data." />
+          }
           hideFilter
           onHighlightChange={(highlightedSeries) => {
             const highlightedSeriesTitle = highlightedSeries.detail.highlightedSeries?.title;
@@ -121,7 +126,7 @@ const DxcBarChart = <X extends DataTypes>({
           }}
         />
       </InsetWrapper>
-    </DxcFlex>
+    </DxcGrid>
   );
 };
 
@@ -134,12 +139,6 @@ const sharedStyles = css`
   min-height: 500px;
 `;
 
-const LoadingStatus = styled.span`
-  ${sharedStyles}
-  color: ${CoreTokens.color_grey_900};
-  gap: ${CoreTokens.spacing_8};
-`;
-
 const ErrorStatus = styled.span`
   ${sharedStyles}
   flex-direction: column;
@@ -147,14 +146,27 @@ const ErrorStatus = styled.span`
   color: ${CoreTokens.color_red_700};
 `;
 
-const Label = styled.div`
-  display: flex;
+const Label = styled.span`
+  display: inline-flex;
   align-items: center;
   gap: ${CoreTokens.spacing_4};
 
   > span {
     font-size: 20px;
   }
+`;
+
+const LoadingContainer = styled.span`
+  ${sharedStyles}
+  gap: ${CoreTokens.spacing_8};
+  color: ${CoreTokens.color_grey_900};
+`;
+
+const StatusMessageContainer = styled.span`
+  ${sharedStyles}
+  flex-direction: column;
+  gap: ${CoreTokens.spacing_4};
+  color: ${CoreTokens.color_grey_800};
 `;
 
 export default DxcBarChart;
