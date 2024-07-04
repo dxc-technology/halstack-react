5ee7850d7544294a3698aa6ffa96ce1b0dd83543
Mil4n0r <morenocarmonaenrique@gmail.com>
2024-07-04 11:29:55 +0200

    Fixed import in Icon accessibility test

--- packages/lib/src/icon/Icon.accessibility.test.tsx
+++ packages/lib/src/icon/Icon.accessibility.test.tsx
@@ -1,5 +1,5 @@
 import { render } from "@testing-library/react";
-import { axe } from "../../test/accessibility/axe-helper.js";
+import { axe } from "../../test/accessibility/axe-helper";
 import DxcIcon from "./Icon";
 
 describe("Icon component accessibility tests", () => {
