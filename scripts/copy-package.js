edab9397dd4848db60cd1d2f73b61d663e006b24
Iván Gómez Pinta <44321109+GomezIvann@users.noreply.github.com>
2024-07-05 13:27:24 +0200

    More updates to publish workflows

--- scripts/copy-package.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-fs.createReadStream('./package.json').pipe(fs.createWriteStream('../lib/dist/package.json'));
-console.log('✅ package.json copied');
--- scripts/copy-readme.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-fs.createReadStream('../../README.md').pipe(fs.createWriteStream('../lib/dist/README.md'));
-console.log('✅ README.md copied');
