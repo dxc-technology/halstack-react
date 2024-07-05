a03fa671397edc2579bbe5d60bc3f20fb1252d01
Mil4n0r <morenocarmonaenrique@gmail.com>
2024-07-05 14:48:20 +0200

    Testing husky

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,6 +3,6 @@ const micromatch = require("micromatch");
 module.exports = {
   "(apps|packages)/**/*.(ts|tsx|js)": (filenames) => {
     const filteredFilenames = micromatch.not(filenames, "**/packages/eslint-config/**");
-    return [`npx eslint --fix ${filteredFilenames.join(" ")}`, `npx prettier --write ${filteredFilenames.join(" ")}`];
+    return [`npx eslint --fix --max-warnings 0 ${filteredFilenames.join(" ")}`, `npx prettier --write ${filteredFilenames.join(" ")}`];
   },
 };
