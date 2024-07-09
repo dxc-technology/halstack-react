60758ec9ef21a1b16ccdfbe69e5ce7e82699bf99
Mil4n0r <morenocarmonaenrique@gmail.com>
2024-07-09 14:37:24 +0200

    Removed prettier from lint-staged flow to make review easier

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,8 @@
 module.exports = {
   "(apps|packages)/**/*.(ts|tsx|js)": (filenames) => {
-    return [`npx eslint --fix --max-warnings 0 ${filenames.join(" ")}`, `npx prettier --write ${filenames.join(" ")}`];
+    return [
+      `npx eslint --fix --max-warnings 0 ${filenames.join(" ")}`,
+      // `npx prettier --write ${filenames.join(" ")}`
+    ];
   },
 };
