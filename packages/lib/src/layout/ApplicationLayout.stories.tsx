686257c15b7256cf6b2c2f7fb0d666deedfb5cbc
Mil4n0r <morenocarmonaenrique@gmail.com>
2024-07-23 14:57:10 +0200

    Added tooltip visual tests to components that internally use them

--- packages/lib/src/layout/ApplicationLayout.stories.tsx
+++ packages/lib/src/layout/ApplicationLayout.stories.tsx
@@ -1,6 +1,7 @@
 import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 import Title from "../../.storybook/components/Title";
 import DxcApplicationLayout from "./ApplicationLayout";
+import { userEvent, within } from "@storybook/test";
 
 export default {
   title: "Application Layout",
@@ -159,3 +160,28 @@ export const ApplicationLayoutWithCustomFooter = () => (
     </DxcApplicationLayout>
   </>
 );
+
+const Tooltip = () => (
+  <>
+    <DxcApplicationLayout
+      sidenav={
+        <DxcApplicationLayout.SideNav>
+          <DxcApplicationLayout.SideNav.Section>
+            <p>SideNav Content</p>
+          </DxcApplicationLayout.SideNav.Section>
+        </DxcApplicationLayout.SideNav>
+      }
+    >
+      <DxcApplicationLayout.Main>
+        <p>Main Content</p>
+      </DxcApplicationLayout.Main>
+    </DxcApplicationLayout>
+  </>
+);
+
+export const ApplicationLayoutTooltip = Tooltip.bind({});
+ApplicationLayoutTooltip.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  const toggleVisibility = await canvas.findByRole("button");
+  await userEvent.hover(toggleVisibility);
+};
