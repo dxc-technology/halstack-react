d7bc9da02b4d6660f61b31403c29f5f9a29b71a4
Iván Gómez Pinta <44321109+GomezIvann@users.noreply.github.com>
2025-01-22 11:30:21 +0100

    Updating example

--- apps/website/screens/components/button/code/examples/basicUsage.ts
+++ apps/website/screens/components/button/code/examples/basicUsage.ts
@@ -1,14 +1,14 @@
-import { DxcButton, DxcInset, HalstackThemeContextProvider } from "@dxc-technology/halstack-react";
+import { DxcButton, DxcInset, HalstackThemeProvider } from "@dxc-technology/halstack-react";
 
 const code = `() => {
   return (
     <DxcInset space="2rem">
-      <HalstackThemeContextProvider value="theme-1">
+      <HalstackThemeProvider value="theme-1">
         <DxcButton label="Button 1" />
-      </HalstackThemeContextProvider>
-      <HalstackThemeContextProvider value="theme-2">
+      </HalstackThemeProvider>
+      <HalstackThemeProvider value="theme-2">
         <DxcButton label="Button 2" margin={{ left: "small" }} />
-      </HalstackThemeContextProvider>
+      </HalstackThemeProvider>
     </DxcInset>
   );
 }`;
@@ -16,7 +16,7 @@ const code = `() => {
 const scope = {
   DxcButton,
   DxcInset,
-  HalstackThemeContextProvider
+  HalstackThemeProvider
 };
 
 export default { code, scope };
