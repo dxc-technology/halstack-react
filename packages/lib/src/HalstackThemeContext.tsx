fd115ab4a0a8f5203bee93999e183eba13e11e15
Iván Gómez Pinta <44321109+GomezIvann@users.noreply.github.com>
2025-01-22 11:13:05 +0100

    HalstackThemeProvider renaming

--- packages/lib/src/HalstackThemeContext.tsx
+++ packages/lib/src/HalstackThemeContext.tsx
@@ -3,4 +3,4 @@ import { createContext } from "react";
 const HalstackThemeContext = createContext<string | undefined>(undefined);
 
 export default HalstackThemeContext;
-export const HalstackThemeContextProvider = HalstackThemeContext.Provider;
+export const HalstackThemeProvider = HalstackThemeContext.Provider;
