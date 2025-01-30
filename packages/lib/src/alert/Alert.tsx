f4dc2918299750e07803f8b9fe579b4159504584
Enrique Moreno <enrique.moreno@dxc.com>
2025-01-30 15:16:47 +0100

    Added tokens to DxcAlert

--- packages/lib/src/alert/Alert.tsx
+++ packages/lib/src/alert/Alert.tsx
@@ -18,20 +18,20 @@ const AlertContainer = styled.div<{
   display: flex;
   flex-direction: column;
   gap: ${CoreTokens.spacing_8};
-  ${(props) =>
-    (props.mode === "modal" || props.mode === "inline") && `border-radius: ${CoreTokens.border_radius_medium};`};
+  ${(props) => (props.mode === "modal" || props.mode === "inline") && `border-radius: var(--border-radius-s);`};
   padding: ${(props) =>
     props.mode === "modal"
-      ? CoreTokens.spacing_24
+      ? "var(--spacing-padding-l)"
       : props.mode === "inline"
-        ? CoreTokens.spacing_12
-        : `${CoreTokens.spacing_8} ${CoreTokens.spacing_12}`};
+        ? "var(--spacing-padding-s)"
+        : "var(--spacing-padding-xs) var(--spacing-padding-s)"};
+
   background-color: ${(props) =>
-    (props.mode === "modal" && props.theme.modalBackgroundColor) ||
-    (props.semantic === "info" && props.theme.infoBackgroundColor) ||
-    (props.semantic === "success" && props.theme.successBackgroundColor) ||
-    (props.semantic === "warning" && props.theme.warningBackgroundColor) ||
-    (props.semantic === "error" && props.theme.errorBackgroundColor)};
+    (props.mode === "modal" && "var(--color-absolutes-white)") ||
+    (props.semantic === "info" && "var(--color-bg-secondary-lighter)") ||
+    (props.semantic === "success" && "var(--color-bg-success-lighter)") ||
+    (props.semantic === "warning" && "var(--color-bg-warning-lighter)") ||
+    (props.semantic === "error" && "var(--color-bg-error-lighter)")};
   overflow: hidden;
 `;
 
@@ -39,28 +39,28 @@ const TitleContainer = styled.div<{ mode: AlertPropsType["mode"]; semantic: Aler
   flex-grow: 1;
   display: flex;
   align-items: center;
-  gap: ${CoreTokens.spacing_8};
+  gap: var(--spacing-gap-s);
   color: ${(props) =>
-    (props.semantic === "info" && props.theme.infoIconColor) ||
-    (props.semantic === "success" && props.theme.successIconColor) ||
-    (props.semantic === "warning" && props.theme.warningIconColor) ||
-    (props.semantic === "error" && props.theme.errorIconColor)};
-  font-size: ${(props) => props.theme.iconSize};
+    (props.semantic === "info" && "var(--color-bg-secondary-strong)") ||
+    (props.semantic === "success" && "var(--color-bg-success-strong)") ||
+    (props.semantic === "warning" && "var(--color-bg-warning-strong)") ||
+    (props.semantic === "error" && "var(--color-bg-error-strong)")};
+  font-size: var(--height-s);
   overflow: hidden;
 `;
 
 const typographyStyles = css`
-  font-family: ${(props) => props.theme.fontFamily};
-  font-size: ${(props) => props.theme.fontSize};
-  font-style: ${(props) => props.theme.fontStyle};
-  font-weight: ${(props) => props.theme.fontWeight};
-  color: ${(props) => props.theme.fontColor};
+  font-family: var(--typography-font-family);
+  font-style: normal;
+  color: var(--color-fg-neutral-dark);
+  font-weight: var(--typography-helper-text-regular);
+  line-height: normal;
 `;
 
 const Title = styled.span<{ mode: AlertPropsType["mode"] }>`
   ${typographyStyles}
-  ${(props) => props.mode === "modal" && `font-size: ${props.theme.modalTitleFontSize}`};
-  font-weight: ${(props) => props.theme.modalTitleFontWeight};
+  font-size: ${(props) => (props.mode === "modal" ? "var(--typography-title-xl)" : "var(--typography-title-s)")};
+  font-weight: var(--typography-title-bold);
 `;
 
 const Message = styled.div<{ mode: AlertPropsType["mode"] }>`
@@ -68,15 +68,17 @@ const Message = styled.div<{ mode: AlertPropsType["mode"] }>`
   white-space: ${(props) => props.mode === "banner" && "nowrap"};
   text-overflow: ${(props) => props.mode === "banner" && "ellipsis"};
   overflow: ${(props) => props.mode === "banner" && "hidden"};
-
+  font-size: var(--typography-helper-text-m);
   > strong {
-    font-weight: ${(props) => props.theme.modalTitleFontWeight};
+    font-weight: var(--typography-title-bold);
+    font-size: var(--typography-title-s);
   }
 `;
 
 const NavigationText = styled.span`
   ${typographyStyles}
   white-space: nowrap;
+  font-size: var(--typography-helper-text-s);
 `;
 
 const Actions = memo(
@@ -137,7 +139,6 @@ export default function DxcAlert({
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const id = useId();
-  const colorsTheme = useContext(HalstackContext);
   const translatedLabels = useContext(HalstackLanguageContext);
 
   const handleNextOnClick = () => {
@@ -158,90 +159,78 @@ export default function DxcAlert({
   }, [currentIndex, messages, handlePrevOnClick]);
 
   return (
-    <ThemeProvider theme={colorsTheme.alert}>
-      <ModalAlertWrapper condition={mode === "modal"} onClose={handleOnClose}>
-        <AlertContainer
-          role={mode === "modal" ? "alertdialog" : "alert"}
-          aria-modal={mode === "modal" ? true : undefined}
-          aria-labelledby={mode === "modal" ? `${id}-title` : undefined}
-          aria-describedby={mode === "modal" ? `${id}-message` : undefined}
-          semantic={semantic}
-          mode={mode}
-        >
-          <DxcFlex gap="0.75rem" alignItems="center">
-            <TitleContainer mode={mode} semantic={semantic}>
-              {getIcon(semantic)}
-              {mode === "banner" ? (
-                <Message mode={mode}>
-                  <strong>{title}</strong>
-                  {messages.length > 0 && <> - {messages[currentIndex]?.text}</>}
-                </Message>
-              ) : (
-                <Title id={`${id}-title`} mode={mode}>
-                  {title}
-                </Title>
-              )}
-            </TitleContainer>
-            {mode === "banner" && (
-              <Actions
-                semantic={semantic}
-                mode={mode}
-                primaryAction={primaryAction}
-                secondaryAction={secondaryAction}
-              />
+    <ModalAlertWrapper condition={mode === "modal"} onClose={handleOnClose}>
+      <AlertContainer
+        role={mode === "modal" ? "alertdialog" : "alert"}
+        aria-modal={mode === "modal" ? true : undefined}
+        aria-labelledby={mode === "modal" ? `${id}-title` : undefined}
+        aria-describedby={mode === "modal" ? `${id}-message` : undefined}
+        semantic={semantic}
+        mode={mode}
+      >
+        <DxcFlex gap="0.75rem" alignItems="center">
+          <TitleContainer mode={mode} semantic={semantic}>
+            {getIcon(semantic)}
+            {mode === "banner" ? (
+              <Message mode={mode}>
+                <strong>{title}</strong>
+                {messages.length > 0 && <> - {messages[currentIndex]?.text}</>}
+              </Message>
+            ) : (
+              <Title id={`${id}-title`} mode={mode}>
+                {title}
+              </Title>
             )}
-            {messages.length > 1 && (
-              <DxcFlex alignItems="center" gap="0.25rem">
-                <DxcActionIcon
-                  icon="chevron_left"
-                  title={translatedLabels.alert.previousMessageActionTitle}
-                  onClick={handlePrevOnClick}
-                  disabled={currentIndex === 0}
-                />
-                <NavigationText>
-                  {currentIndex + 1} of {messages.length}
-                </NavigationText>
-                <DxcActionIcon
-                  icon="chevron_right"
-                  title={translatedLabels.alert.nextMessageActionTitle}
-                  onClick={handleNextOnClick}
-                  disabled={currentIndex === messages.length - 1}
-                />
-              </DxcFlex>
-            )}
-            {closable && (
-              <DxcFlex gap="0.25rem">
-                {mode !== "modal" && <DxcDivider orientation="vertical" />}
-                <DxcActionIcon
-                  icon="close"
-                  title={
-                    messages.length > 1
-                      ? translatedLabels.alert.closeMessageActionTitle
-                      : translatedLabels.alert.closeAlertActionTitle
-                  }
-                  onClick={handleOnClose}
-                />
-              </DxcFlex>
-            )}
-          </DxcFlex>
-          {mode === "modal" && <DxcDivider />}
-          {mode !== "banner" && (
-            <>
-              {messages.length > 0 && (
-                <Message id={`${id}-message`} mode={mode}>
-                  {messages[currentIndex]?.text}
-                </Message>
-              )}
-              <Actions
-                semantic={semantic}
-                mode={mode}
-                primaryAction={primaryAction}
-                secondaryAction={secondaryAction}
-              />
-            </>
+          </TitleContainer>
+          {mode === "banner" && (
+            <Actions semantic={semantic} mode={mode} primaryAction={primaryAction} secondaryAction={secondaryAction} />
           )}
-        </AlertContainer>
-      </ModalAlertWrapper>
-    </ThemeProvider>
+          {messages.length > 1 && (
+            <DxcFlex alignItems="center" gap="0.25rem">
+              <DxcActionIcon
+                icon="chevron_left"
+                title={translatedLabels.alert.previousMessageActionTitle}
+                onClick={handlePrevOnClick}
+                disabled={currentIndex === 0}
+              />
+              <NavigationText>
+                {currentIndex + 1} of {messages.length}
+              </NavigationText>
+              <DxcActionIcon
+                icon="chevron_right"
+                title={translatedLabels.alert.nextMessageActionTitle}
+                onClick={handleNextOnClick}
+                disabled={currentIndex === messages.length - 1}
+              />
+            </DxcFlex>
+          )}
+          {closable && (
+            <DxcFlex gap="0.25rem">
+              {mode !== "modal" && <DxcDivider orientation="vertical" />}
+              <DxcActionIcon
+                icon="close"
+                title={
+                  messages.length > 1
+                    ? translatedLabels.alert.closeMessageActionTitle
+                    : translatedLabels.alert.closeAlertActionTitle
+                }
+                onClick={handleOnClose}
+              />
+            </DxcFlex>
+          )}
+        </DxcFlex>
+        {mode === "modal" && <DxcDivider />}
+        {mode !== "banner" && (
+          <>
+            {messages.length > 0 && (
+              <Message id={`${id}-message`} mode={mode}>
+                {messages[currentIndex]?.text}
+              </Message>
+            )}
+            <Actions semantic={semantic} mode={mode} primaryAction={primaryAction} secondaryAction={secondaryAction} />
+          </>
+        )}
+      </AlertContainer>
+    </ModalAlertWrapper>
   );
 }
