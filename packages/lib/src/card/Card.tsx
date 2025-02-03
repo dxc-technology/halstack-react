ac52f4c816b68fe98b8a04618e719d63f598305c
Enrique Moreno <enrique.moreno@dxc.com>
2025-02-03 16:53:19 +0100

    Reimplementation of the Card component with new CSS variables

--- packages/lib/src/card/Card.tsx
+++ packages/lib/src/card/Card.tsx
@@ -2,8 +2,82 @@ import { useContext, useState } from "react";
 import styled, { ThemeProvider } from "styled-components";
 import { spaces } from "../common/variables";
 import CardPropsType from "./types";
-import CoreTokens from "../common/coreTokens";
 import HalstackContext from "../HalstackContext";
+
+const Card = styled.div<{
+  hasAction: boolean;
+  margin: CardPropsType["margin"];
+  shadowDepth: 0 | 1 | 2;
+}>`
+  display: flex;
+  cursor: ${({ hasAction }) => (hasAction && "pointer") || "unset"};
+  outline: ${({ hasAction }) => !hasAction && "none"};
+  margin: ${({ margin }) => (margin && typeof margin !== "object" ? spaces[margin] : "0px")};
+  margin-top: ${({ margin }) => (margin && typeof margin === "object" && margin.top ? spaces[margin.top] : "")};
+  margin-right: ${({ margin }) => (margin && typeof margin === "object" && margin.right ? spaces[margin.right] : "")};
+  margin-bottom: ${({ margin }) =>
+    margin && typeof margin === "object" && margin.bottom ? spaces[margin.bottom] : ""};
+  margin-left: ${({ margin }) => (margin && typeof margin === "object" && margin.left ? spaces[margin.left] : "")};
+  text-decoration: none;
+  ${({ hasAction }) =>
+    hasAction &&
+    `:focus {
+      border-radius: var(--border-radius-s);
+      outline: var(--border-width-m) solid var(--border-color-secondary-medium);
+    }`}
+  box-shadow: ${({ shadowDepth }) =>
+    shadowDepth === 1
+      ? "var(--shadow-low-x-position) var(--shadow-low-y-position) var(--shadow-low-blur) var(--shadow-low-spread) var(--shadow-dark)"
+      : shadowDepth === 2
+        ? "var(--shadow-mid-x-position) var(--shadow-mid-y-position) var(--shadow-mid-blur) var(--shadow-mid-spread) var(--shadow-light)"
+        : "none"};
+`;
+
+const CardContainer = styled.div<{
+  hasAction: boolean;
+  imagePosition: CardPropsType["imagePosition"] | "none";
+}>`
+  display: flex;
+  flex-direction: ${(props) => (props.imagePosition === "after" ? "row-reverse" : "row")};
+  height: 220px;
+  width: 400px;
+  &:hover {
+    border-color: ${({ hasAction }) => (hasAction ? "" : "unset")};
+  }
+`;
+
+const TagImage = styled.img<{ imagePadding: CardPropsType["imagePadding"]; imageCover: CardPropsType["imageCover"] }>`
+  height: ${({ imagePadding }) =>
+    !imagePadding
+      ? "100%"
+      : typeof imagePadding !== "object" && `calc(100% - ${spaces[imagePadding]} - ${spaces[imagePadding]})`};
+  width: ${({ imagePadding }) =>
+    !imagePadding
+      ? "100%"
+      : typeof imagePadding !== "object" && `calc(100% - ${spaces[imagePadding]} - ${spaces[imagePadding]})`};
+  object-fit: ${({ imageCover }) => (imageCover ? "cover" : "contain")};
+`;
+
+const ImageContainer = styled.div<{ imageBgColor: CardPropsType["imageBgColor"] }>`
+  align-items: center;
+  background-color: ${({ imageBgColor }) => imageBgColor};
+  display: flex;
+  flex-shrink: 0;
+  height: 100%;
+  justify-content: center;
+  width: 35%;
+`;
+
+const CardContent = styled.div`
+  align-items: flex-start;
+  align-self: stretch;
+  display: flex;
+  flex-direction: column;
+  flex-shrink: 0;
+  gap: var(--spacing-gap-ml, 16px);
+  overflow: hidden;
+  padding: var(--spacing-padding-l);
+`;
 
 const DxcCard = ({
   imageSrc,
@@ -46,74 +120,5 @@ const DxcCard = ({
     </ThemeProvider>
   );
 };
-
-const Card = styled.div<{
-  hasAction: boolean;
-  margin: CardPropsType["margin"];
-  shadowDepth: 0 | 1 | 2;
-}>`
-  display: inline-flex;
-  cursor: ${({ hasAction }) => (hasAction && "pointer") || "unset"};
-  outline: ${({ hasAction }) => !hasAction && "none"};
-  margin: ${({ margin }) => (margin && typeof margin !== "object" ? spaces[margin] : "0px")};
-  margin-top: ${({ margin }) => (margin && typeof margin === "object" && margin.top ? spaces[margin.top] : "")};
-  margin-right: ${({ margin }) => (margin && typeof margin === "object" && margin.right ? spaces[margin.right] : "")};
-  margin-bottom: ${({ margin }) =>
-    margin && typeof margin === "object" && margin.bottom ? spaces[margin.bottom] : ""};
-  margin-left: ${({ margin }) => (margin && typeof margin === "object" && margin.left ? spaces[margin.left] : "")};
-  text-decoration: none;
-  ${({ hasAction }) =>
-    hasAction &&
-    `:focus {
-      outline: #0095ff auto 1px;
-    }`}
-
-  box-shadow: ${({ shadowDepth }) =>
-    shadowDepth === 1
-      ? `0px 3px 6px ${CoreTokens.color_grey_300_a}`
-      : shadowDepth === 2
-        ? `0px 3px 10px ${CoreTokens.color_grey_300_a}`
-        : "none"};
-`;
-
-const CardContainer = styled.div<{
-  hasAction: boolean;
-  imagePosition: CardPropsType["imagePosition"] | "none";
-}>`
-  display: inline-flex;
-  flex-direction: ${(props) => (props.imagePosition === "after" ? "row-reverse" : "row")};
-  height: ${(props) => props.theme.height};
-  width: ${(props) => props.theme.width};
-  &:hover {
-    border-color: ${({ hasAction }) => (hasAction ? "" : "unset")};
-  }
-`;
-
-const TagImage = styled.img<{ imagePadding: CardPropsType["imagePadding"]; imageCover: CardPropsType["imageCover"] }>`
-  height: ${({ imagePadding }) =>
-    !imagePadding
-      ? "100%"
-      : typeof imagePadding !== "object" && `calc(100% - ${spaces[imagePadding]} - ${spaces[imagePadding]})`};
-  width: ${({ imagePadding }) =>
-    !imagePadding
-      ? "100%"
-      : typeof imagePadding !== "object" && `calc(100% - ${spaces[imagePadding]} - ${spaces[imagePadding]})`};
-  object-fit: ${({ imageCover }) => (imageCover ? "cover" : "contain")};
-`;
-
-const ImageContainer = styled.div<{ imageBgColor: CardPropsType["imageBgColor"] }>`
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  flex-shrink: 0;
-  width: 35%;
-  height: 100%;
-  background-color: ${({ imageBgColor }) => imageBgColor};
-`;
-
-const CardContent = styled.div`
-  flex-grow: 1;
-  overflow: hidden;
-`;
 
 export default DxcCard;
