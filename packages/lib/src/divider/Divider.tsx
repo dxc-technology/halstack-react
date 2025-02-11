68886172342a51b4a2322481a46554729e14f699
Enrique Moreno <enrique.moreno@dxc.com>
2025-02-11 15:10:13 +0100

    Reimplementation of the Divider component with new CSS variables

--- packages/lib/src/divider/Divider.tsx
+++ packages/lib/src/divider/Divider.tsx
@@ -1,6 +1,25 @@
 import styled from "styled-components";
-import CoreTokens from "../common/coreTokens";
 import DividerPropsType from "./types";
+
+const StyledDivider = styled.hr<DividerPropsType>`
+  ${({ orientation, weight, color }) => `
+    border-color: ${
+      color === "lightGrey"
+        ? "var(--border-color-neutral-lighter)"
+        : color === "mediumGrey"
+          ? "var(--border-color-neutral-medium)"
+          : "var(--border-color-neutral-strongest)"
+    };
+    ${orientation === "horizontal" ? "width" : "min-height"}: 100%;
+    ${orientation === "horizontal" ? "height" : "width"}: 0px;
+    ${
+      orientation === "horizontal"
+        ? "border-width: " + (weight === "regular" ? "1px 0 0 0" : "2px 0 0 0")
+        : "border-width: " + (weight === "regular" ? "0 0 0 1px" : "0 0 0 2px")
+    };
+    margin: 0px;
+  `}
+`;
 
 const DxcDivider = ({
   orientation = "horizontal",
@@ -16,25 +35,5 @@ const DxcDivider = ({
     aria-hidden={decorative}
   />
 );
-
-const StyledDivider = styled.hr<DividerPropsType>`
-  ${({ orientation, weight, color }) => `
-    border-color: ${
-      color === "lightGrey"
-        ? CoreTokens.color_grey_200
-        : color === "mediumGrey"
-          ? CoreTokens.color_grey_400
-          : CoreTokens.color_grey_700
-    };
-    ${orientation === "horizontal" ? "width" : "min-height"}: 100%;
-    ${orientation === "horizontal" ? "height" : "width"}: 0px;
-    ${
-      orientation === "horizontal"
-        ? "border-width: " + (weight === "regular" ? "1px 0 0 0" : "2px 0 0 0")
-        : "border-width: " + (weight === "regular" ? "0 0 0 1px" : "0 0 0 2px")
-    };
-    margin: 0px;
-  `}
-`;
 
 export default DxcDivider;
