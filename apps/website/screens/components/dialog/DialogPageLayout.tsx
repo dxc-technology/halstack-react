d698c0e992482eb8b8220afd65c3b225053f098f
Enrique Moreno <enrique.moreno@dxc.com>
2025-02-18 13:49:10 +0100

    Fixed layout text

--- apps/website/screens/components/dialog/DialogPageLayout.tsx
+++ apps/website/screens/components/dialog/DialogPageLayout.tsx
@@ -17,8 +17,7 @@ const DialogPageHeading = ({ children }: { children: ReactNode }) => {
           <ComponentHeading name="Dialog" />
           <DxcParagraph>
             A modal dialog is a message box or child window that requires user interaction before returning to the
-            parent window. These boxes appear on top of the open parent window that is currently displayed on the
-            screen.
+            parent window.
           </DxcParagraph>
           <TabsPageHeading tabs={tabs} />
         </DxcFlex>
