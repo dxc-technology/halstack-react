ef21ed6686506b720797689b42b43e8a92351214
Enrique Moreno <enrique.moreno@dxc.com>
2025-02-28 13:16:51 +0100

    Added ActionIcon to Chip

--- packages/lib/src/chip/Chip.tsx
+++ packages/lib/src/chip/Chip.tsx
@@ -5,6 +5,7 @@ import { spaces } from "../common/variables";
 import DxcIcon from "../icon/Icon";
 import HalstackContext from "../HalstackContext";
 import ChipPropsType from "./types";
+import ActionIcon from "../action-icon/ActionIcon";
 
 const calculateWidth = (margin: ChipPropsType["margin"]) =>
   `calc(100% - ${getMargin(margin, "left")} - ${getMargin(margin, "right")})`;
@@ -44,25 +45,10 @@ const LabelContainer = styled.span<{ disabled: ChipPropsType["disabled"] }>`
 
 const IconContainer = styled.div<{
   disabled: ChipPropsType["disabled"];
-  interactive: boolean;
 }>`
   display: flex;
   border-radius: var(--border-radius-xs);
   color: ${(props) => (props.disabled ? "var(--color-fg-neutral-medium)" : "var(--color-fg-neutral-dark)")};
-  ${({ interactive }) => interactive && "cursor: pointer;"}
-
-  ${(props) =>
-    props.interactive &&
-    `
-      &:hover, &:active{
-        background-color: var(--color-bg-alpha-light);
-      }
-      &:focus,
-      &:focus-visible {
-        outline: var(--border-width-m, 2px) solid var(--border-color-secondary-medium);
-      }
-    `}
-
   font-size: var(--height-s);
   svg {
     width: 24px;
@@ -85,31 +71,35 @@ const DxcChip = ({
   return (
     <ThemeProvider theme={colorsTheme.chip}>
       <Chip disabled={disabled} margin={margin}>
-        {prefixIcon && (
-          <IconContainer
-            role={typeof onClickPrefix === "function" ? "button" : undefined}
-            aria-label={typeof onClickPrefix === "function" ? "Prefix Action" : undefined}
-            disabled={disabled}
-            interactive={typeof onClickPrefix === "function" && !disabled}
-            tabIndex={typeof onClickPrefix === "function" && !disabled ? tabIndex : -1}
-            onClick={() => onClickPrefix && !disabled && onClickPrefix()}
-          >
-            {typeof prefixIcon === "string" ? <DxcIcon icon={prefixIcon} /> : prefixIcon}
-          </IconContainer>
-        )}
+        {prefixIcon &&
+          (typeof onClickPrefix === "function" ? (
+            <ActionIcon
+              disabled={disabled}
+              icon={prefixIcon}
+              onClick={onClickPrefix}
+              tabIndex={tabIndex}
+              title="Prefix Action"
+            />
+          ) : (
+            <IconContainer disabled={disabled}>
+              {typeof prefixIcon === "string" ? <DxcIcon icon={prefixIcon} /> : prefixIcon}
+            </IconContainer>
+          ))}
         {label && <LabelContainer disabled={disabled}>{label}</LabelContainer>}
-        {suffixIcon && (
-          <IconContainer
-            role={typeof onClickSuffix === "function" ? "button" : undefined}
-            aria-label={typeof onClickSuffix === "function" ? "Suffix Action" : undefined}
-            disabled={disabled}
-            interactive={typeof onClickSuffix === "function" && !disabled}
-            tabIndex={typeof onClickSuffix === "function" && !disabled ? tabIndex : -1}
-            onClick={() => !disabled && onClickSuffix?.()}
-          >
-            {typeof suffixIcon === "string" ? <DxcIcon icon={suffixIcon} /> : suffixIcon}
-          </IconContainer>
-        )}
+        {suffixIcon &&
+          (typeof onClickSuffix ? (
+            <ActionIcon
+              disabled={disabled}
+              icon={suffixIcon}
+              onClick={onClickSuffix}
+              tabIndex={tabIndex}
+              title="Suffix Action"
+            />
+          ) : (
+            <IconContainer disabled={disabled}>
+              {typeof suffixIcon === "string" ? <DxcIcon icon={suffixIcon} /> : suffixIcon}
+            </IconContainer>
+          ))}
       </Chip>
     </ThemeProvider>
   );
