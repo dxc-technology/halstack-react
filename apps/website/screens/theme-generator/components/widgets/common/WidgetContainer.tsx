99b175285800eefa0a486a7eb525fd5ba9adc5fd
Iván Gómez Pinta <44321109+GomezIvann@users.noreply.github.com>
2025-03-12 17:47:05 +0100

    Removing Theme generator and theming doc pages

--- apps/website/screens/theme-generator/components/widgets/common/WidgetContainer.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const WidgetContainer = styled.span`
-  display: grid;
-  grid-template: 1fr / minmax(0, 50%) minmax(0, 50%);
-  column-gap: 0.25rem;
-`;
-
-export default WidgetContainer;
--- apps/website/screens/utilities/halstack-provider/HalstackProviderPage.tsx
+++ apps/website/screens/utilities/halstack-provider/HalstackProviderPage.tsx
@@ -2,8 +2,6 @@ import DocFooter from "@/common/DocFooter";
 import Example from "@/common/example/Example";
 import QuickNavContainer from "@/common/QuickNavContainer";
 import QuickNavContainerLayout from "@/common/QuickNavContainerLayout";
-import opinionatedTheme from "./examples/opinionatedTheme";
-import advancedTheme from "./examples/advancedTheme";
 import customTranslations from "./examples/customTranslations";
 import {
   DxcFlex,
@@ -31,22 +29,6 @@ const sections = [
           <th>Default</th>
         </tr>
         <tr>
-          <td>theme</td>
-          <td>
-            <TableCode>OpinionatedTheme</TableCode>
-          </td>
-          <td>Object with a given structure, specified below, for defining the opinionated theme.</td>
-          <td>-</td>
-        </tr>
-        <tr>
-          <td>advancedTheme</td>
-          <td>
-            <TableCode>AdvancedTheme</TableCode>
-          </td>
-          <td>Object with a given structure, specified below, for defining the advanced theme.</td>
-          <td>-</td>
-        </tr>
-        <tr>
           <td>labels</td>
           <td>
             <TableCode>TranslatedLabels</TableCode>
@@ -55,83 +37,6 @@ const sections = [
           <td>-</td>
         </tr>
       </DxcTable>
-    ),
-  },
-  {
-    title: "Opinionated theme",
-    content: (
-      <>
-        <DxcParagraph>
-          As explained on the{" "}
-          <Link href="/principles/themes/#different-theming-strategies-opinionated-theme" passHref legacyBehavior>
-            <DxcLink>Themes</DxcLink>
-          </Link>{" "}
-          page, you can apply the opinionated theming strategy to customize the components.
-        </DxcParagraph>
-        <DxcAlert
-          title="Opinionated theme"
-          semantic="info"
-          message={{
-            text: (
-              <>
-                Remember that you can use the{" "}
-                <Link href="/theme-generator/opinionated-theme" passHref legacyBehavior>
-                  <DxcLink>opinionated theme generator</DxcLink>
-                </Link>{" "}
-                to help you create themes.
-              </>
-            ),
-          }}
-          closable={false}
-        />
-        <DxcParagraph>
-          Below is an example of customizing the colours of a <Code>DxcAccordion</Code> and a <Code>DxcTextInput</Code>:
-        </DxcParagraph>
-        <Example example={opinionatedTheme} defaultIsVisible />
-        <DxcParagraph>
-          We create a <Code>customTheme</Code> object with as many components as we want and their respective values.
-          Then we pass this object to the Halstack Provider, which wraps our components, through its <Code>theme</Code>{" "}
-          property.
-        </DxcParagraph>
-      </>
-    ),
-  },
-  {
-    title: "Advanced theme",
-    content: (
-      <>
-        <DxcParagraph>
-          Advanced theming is the option to choose when further customization is required. To find out which use cases
-          are valid for this strategy, you can refer to the{" "}
-          <Link href="/principles/themes/#different-theming-strategies-advanced-theme" passHref legacyBehavior>
-            <DxcLink>Themes</DxcLink>
-          </Link>{" "}
-          page.
-        </DxcParagraph>
-        <DxcAlert
-          title="Advanced theme"
-          semantic="info"
-          message={{
-            text: (
-              <>
-                Remember that you can use the{" "}
-                <Link href="/theme-generator/advanced-theme" passHref legacyBehavior>
-                  <DxcLink>advanced theme generator</DxcLink>
-                </Link>{" "}
-                to help you create themes.
-              </>
-            ),
-          }}
-          closable={false}
-        />
-
-        <Example example={advancedTheme} defaultIsVisible />
-        <DxcParagraph>
-          In the example above we have customized some of the <Code>DxcAccordion</Code> tokens. As you can see, it is
-          not necessary to pass all the tokens of the component, only those that you want to change their value with
-          respect to the default theme.
-        </DxcParagraph>
-      </>
     ),
   },
   {
