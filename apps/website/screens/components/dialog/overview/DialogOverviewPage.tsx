eba9dc18b3113c52ad273dd843b83155b8c85e56
Enrique Moreno <enrique.moreno@dxc.com>
2025-03-13 11:47:48 +0100

    Removed return statement

--- apps/website/screens/components/dialog/overview/DialogOverviewPage.tsx
+++ apps/website/screens/components/dialog/overview/DialogOverviewPage.tsx
@@ -139,15 +139,13 @@ const sections = [
   },
 ];
 
-const DialogOverviewPage = () => {
-  return (
-    <DxcFlex direction="column" gap="4rem">
-      <QuickNavContainerLayout>
-        <QuickNavContainer sections={sections} startHeadingLevel={2} />
-      </QuickNavContainerLayout>
-      <DocFooter githubLink="https://github.com/dxc-technology/halstack-react/blob/master/apps/website/screens/components/dialog/overview/DialogOverviewPage.tsx" />
-    </DxcFlex>
-  );
-};
+const DialogOverviewPage = () => (
+  <DxcFlex direction="column" gap="4rem">
+    <QuickNavContainerLayout>
+      <QuickNavContainer sections={sections} startHeadingLevel={2} />
+    </QuickNavContainerLayout>
+    <DocFooter githubLink="https://github.com/dxc-technology/halstack-react/blob/master/apps/website/screens/components/dialog/overview/DialogOverviewPage.tsx" />
+  </DxcFlex>
+);
 
 export default DialogOverviewPage;
