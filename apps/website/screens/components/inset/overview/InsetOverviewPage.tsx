483f54cb4d9505fefa95c5a48b417197d8ff2390
Iván Gómez Pinta <44321109+GomezIvann@users.noreply.github.com>
2025-03-31 17:49:10 +0200

    Overview page updated

--- apps/website/screens/components/inset/overview/InsetOverviewPage.tsx
+++ apps/website/screens/components/inset/overview/InsetOverviewPage.tsx
@@ -1,8 +1,9 @@
-import { DxcFlex, DxcParagraph } from "@dxc-technology/halstack-react";
+import { DxcBulletedList, DxcFlex, DxcLink, DxcParagraph } from "@dxc-technology/halstack-react";
 import QuickNavContainerLayout from "@/common/QuickNavContainerLayout";
 import QuickNavContainer from "@/common/QuickNavContainer";
 import DocFooter from "@/common/DocFooter";
 import Code from "@/common/Code";
+import Link from "next/link";
 
 const sections = [
   {
@@ -10,29 +11,56 @@ const sections = [
     content: (
       <>
         <DxcParagraph>
-          Inset properties refer to the scale of the spacing used outside a container. The unit commonly used for the
-          inset scale is REM, which is a measurement unit that refers to the font-size of the root element of a
-          document.
+          The inset component is a container that provides spacing around its content. It can be used to create visual
+          separation between elements in a layout.
         </DxcParagraph>
         <DxcParagraph>
-          The following values of REM are commonly used in the design system: <Code>0</Code>, <Code>0.125</Code>,{" "}
-          <Code>0.25</Code>, <Code>0.5</Code>, <Code>1</Code>, <Code>1.5</Code>, <Code>2</Code>, <Code>3</Code>,{" "}
-          <Code>4</Code>, and <Code>5</Code>.
+          The inset component can be customized with different spacing options, allowing for flexible design choices. It
+          is typically used in conjunction with other components to create a cohesive layout.
         </DxcParagraph>
       </>
     ),
   },
+  {
+    title: "Best practices",
+    content: (
+      <DxcBulletedList>
+        <DxcBulletedList.Item>
+          Use the inset component to create visual separation between elements in a layout.
+        </DxcBulletedList.Item>
+        <DxcBulletedList.Item>
+          Choose appropriate spacing values to ensure a balanced and visually appealing design.
+        </DxcBulletedList.Item>
+        <DxcBulletedList.Item>
+          Avoid excessive use of the inset component, as it may lead to a cluttered layout.
+        </DxcBulletedList.Item>
+        <DxcBulletedList.Item>
+          Use this component in conjunction with other layout components like{" "}
+          <Link href="/components/flex" passHref legacyBehavior>
+            <DxcLink>flex</DxcLink>
+          </Link>{" "}
+          and{" "}
+          <Link href="/components/grid" passHref legacyBehavior>
+            <DxcLink>grid</DxcLink>
+          </Link>{" "}
+          components to create consistent and semantic layouts.
+        </DxcBulletedList.Item>
+        <DxcBulletedList.Item>
+          Whenever possible, <strong>try to use the design tokens</strong> provided by the Halstack Design System, even
+          though the component allows any value.
+        </DxcBulletedList.Item>
+      </DxcBulletedList>
+    ),
+  },
 ];
 
-const InsetOverviewPage = () => {
-  return (
-    <DxcFlex direction="column" gap="4rem">
-      <QuickNavContainerLayout>
-        <QuickNavContainer sections={sections} startHeadingLevel={2}></QuickNavContainer>
-      </QuickNavContainerLayout>
-      <DocFooter githubLink="https://github.com/dxc-technology/halstack-react/blob/master/apps/website/screens/components/inset/overview/InsetOverviewPage.tsx" />
-    </DxcFlex>
-  );
-};
+const InsetOverviewPage = () => (
+  <DxcFlex direction="column" gap="4rem">
+    <QuickNavContainerLayout>
+      <QuickNavContainer sections={sections} startHeadingLevel={2} />
+    </QuickNavContainerLayout>
+    <DocFooter githubLink="https://github.com/dxc-technology/halstack-react/blob/master/apps/website/screens/components/inset/overview/InsetOverviewPage.tsx" />
+  </DxcFlex>
+);
 
 export default InsetOverviewPage;
