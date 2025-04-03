4e1c3f19be76cbd4cde086c044d9837d81d0c294
Enrique Moreno <enrique.moreno@dxc.com>
2025-04-03 10:56:40 +0200

    Fixed margin values

--- packages/lib/src/bleed/Bleed.tsx
+++ packages/lib/src/bleed/Bleed.tsx
@@ -1,17 +1,17 @@
 import BleedPropsType from "./types";
 import DxcContainer from "../container/Container";
 
+const getNegativeValue = (value?: string) => (value ? `-${value}` : null);
+
 export default function DxcBleed({ space, horizontal, vertical, top, right, bottom, left, children }: BleedPropsType) {
   return (
     <DxcContainer
-      margin={
-        space || {
-          bottom: (bottom || vertical) ?? "0rem",
-          left: (left || horizontal) ?? "0rem",
-          right: (right || horizontal) ?? "0rem",
-          top: (top || vertical) ?? "0rem",
-        }
-      }
+      margin={{
+        top: getNegativeValue(top) ?? getNegativeValue(vertical) ?? getNegativeValue(space) ?? "0rem",
+        right: getNegativeValue(right) ?? getNegativeValue(horizontal) ?? getNegativeValue(space) ?? "0rem",
+        bottom: getNegativeValue(bottom) ?? getNegativeValue(vertical) ?? getNegativeValue(space) ?? "0rem",
+        left: getNegativeValue(left) ?? getNegativeValue(horizontal) ?? getNegativeValue(space) ?? "0rem",
+      }}
     >
       {children}
     </DxcContainer>
