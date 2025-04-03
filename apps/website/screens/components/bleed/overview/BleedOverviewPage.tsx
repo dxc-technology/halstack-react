bf9bff7480c759f29d378199778ea8b1bfd53270
Enrique Moreno <enrique.moreno@dxc.com>
2025-04-03 11:45:31 +0200

    Added documentation

--- apps/website/screens/components/bleed/overview/BleedOverviewPage.tsx
+++ apps/website/screens/components/bleed/overview/BleedOverviewPage.tsx
@@ -1,7 +1,8 @@
-import { DxcFlex, DxcParagraph } from "@dxc-technology/halstack-react";
+import { DxcBulletedList, DxcFlex, DxcLink, DxcParagraph } from "@dxc-technology/halstack-react";
 import QuickNavContainerLayout from "@/common/QuickNavContainerLayout";
 import QuickNavContainer from "@/common/QuickNavContainer";
 import DocFooter from "@/common/DocFooter";
+import Link from "next/link";
 import Code from "@/common/Code";
 
 const sections = [
@@ -10,29 +11,58 @@ const sections = [
     content: (
       <>
         <DxcParagraph>
-          Bleed properties refer to the scale of the spacing used outside a container. The unit commonly used for the
-          bleed scale is REM, which is a measurement unit that refers to the font-size of the root element of a
-          document.
-        </DxcParagraph>
-        <DxcParagraph>
-          The following values of REM are commonly used in the design system: <Code>0</Code>, <Code>0.125</Code>,{" "}
-          <Code>0.25</Code>, <Code>0.5</Code>, <Code>1</Code>, <Code>1.5</Code>, <Code>2</Code>, <Code>3</Code>,{" "}
-          <Code>4</Code>, and <Code>5</Code>.
+          The Bleed component is a container that applies negative margins around its content, allowing elements to
+          extend beyond their usual boundaries. This can be useful for aligning content seamlessly and creating dynamic,
+          edge-to-edge layouts. It provides customizable spacing options, offering greater design flexibility while
+          maintaining visual coherence.
         </DxcParagraph>
       </>
     ),
   },
+  {
+    title: "Best practices",
+    content: (
+      <DxcBulletedList>
+        <DxcBulletedList.Item>
+          <strong>Enhance visual flow:</strong> use the bleed component to create smooth transitions between sections, removing
+          unwanted spacing restrictions.
+        </DxcBulletedList.Item>
+        <DxcBulletedList.Item>
+          <strong>Maintain balance and aesthetics:</strong> choose appropriate spacing values to ensure a
+          well-proportioned and visually appealing design.
+        </DxcBulletedList.Item>
+        <DxcBulletedList.Item>
+          <strong>Avoid unnecessary clutter:</strong> excessive use of the Inset component may lead to a fragmented or
+          overcrowded layout.
+        </DxcBulletedList.Item>
+        <DxcBulletedList.Item>
+          <strong>Ensure consistency with design tokens:</strong> whenever possible, use the design tokens provided by
+          the Halstack Design System, to maintain visual and functional consistency across applications, even though the
+          component allows custom values.
+        </DxcBulletedList.Item>
+        <DxcBulletedList.Item>
+          <strong>Combine with other layout techniques:</strong> use this component alongside{" "}
+          <Link href="/components/flex" passHref legacyBehavior>
+            <DxcLink>flex</DxcLink>
+          </Link>{" "}
+          and{" "}
+          <Link href="/components/grid" passHref legacyBehavior>
+            <DxcLink>grid</DxcLink>
+          </Link>{" "}
+          components to create consistent and semantic layouts.
+        </DxcBulletedList.Item>
+      </DxcBulletedList>
+    ),
+  },
 ];
 
-const BleedOverviewPage = () => {
-  return (
-    <DxcFlex direction="column" gap="4rem">
-      <QuickNavContainerLayout>
-        <QuickNavContainer sections={sections} startHeadingLevel={2} />
-      </QuickNavContainerLayout>
-      <DocFooter githubLink="https://github.com/dxc-technology/halstack-react/blob/master/apps/website/screens/components/bleed/overview/BleedOverviewPage.tsx" />
-    </DxcFlex>
-  );
-};
+const BleedOverviewPage = () => (
+  <DxcFlex direction="column" gap="4rem">
+    <QuickNavContainerLayout>
+      <QuickNavContainer sections={sections} startHeadingLevel={2} />
+    </QuickNavContainerLayout>
+    <DocFooter githubLink="https://github.com/dxc-technology/halstack-react/blob/master/apps/website/screens/components/bleed/overview/BleedOverviewPage.tsx" />
+  </DxcFlex>
+);
 
 export default BleedOverviewPage;
