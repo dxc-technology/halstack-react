5b596e06992a2ea2f0f9d17197c3188470537af6
Jialecl <jialestrabajos@gmail.com>
2025-04-14 14:56:30 +0200

    changed height to line-height and moved styled components outside

--- packages/lib/src/quick-nav/QuickNav.tsx
+++ packages/lib/src/quick-nav/QuickNav.tsx
@@ -5,57 +5,58 @@ import DxcHeading from "../heading/Heading";
 import { HalstackLanguageContext } from "../HalstackContext";
 import QuickNavTypes from "./types";
 
+const translatedLabels = useContext(HalstackLanguageContext);
+
+const QuickNavContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: var(--spacing-gap-m);
+  padding: var(--spacing-padding-xs) var(--spacing-padding-m);
+  border-left: var(--border-width-m) var(--border-style-default) var(--border-color-neutral-medium);
+`;
+
+const ListColumn = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: var(--spacing-gap-s);
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+`;
+
+const ListSecondColumn = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: var(--spacing-gap-xs);
+  margin-top: var(--spacing-gap-xs);
+  padding: var(--spacing-padding-none) var(--spacing-padding-xs);
+  list-style-type: none;
+`;
+
+const Link = styled.a`
+  text-decoration: none;
+  font-family: var(--typography-font-family);
+  font-size: var(--typography-label-m);
+  font-weight: var(--typography-label-regular);
+  color: var(--color-fg-neutral-stronger);
+  display: block;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+  line-height: var(--height-s);
+  width: fit-content;
+  max-width: 100%;
+
+  &:hover {
+    color: var(--color-fg-primary-strong);
+  }
+  &:focus {
+    border-radius: var(--border-radius-xs);
+    outline: var(--border-width-m) var(--border-style-default) var(--border-color-secondary-medium);
+  }
+`;
+
 const DxcQuickNav = ({ title, links }: QuickNavTypes): JSX.Element => {
-  const translatedLabels = useContext(HalstackLanguageContext);
-
-  const QuickNavContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: var(--spacing-gap-m);
-    padding: var(--spacing-padding-xs) var(--spacing-padding-m);
-    border-left: var(--border-width-m) var(--border-style-default) var(--border-color-neutral-medium);
-  `;
-
-  const ListColumn = styled.ul`
-    display: flex;
-    flex-direction: column;
-    gap: var(--spacing-gap-s);
-    margin: 0;
-    padding: 0;
-    list-style-type: none;
-  `;
-
-  const ListSecondColumn = styled.ul`
-    display: flex;
-    flex-direction: column;
-    gap: var(--spacing-gap-xs);
-    margin-top: var(--spacing-gap-xs);
-    padding: var(--spacing-padding-none) var(--spacing-padding-xs);
-    list-style-type: none;
-  `;
-
-  const Link = styled.a`
-    text-decoration: none;
-    font-family: var(--typography-font-family);
-    font-size: var(--typography-label-m);
-    font-weight: var(--typography-label-regular);
-    color: var(--color-fg-neutral-stronger);
-    display: block;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    overflow: hidden;
-    height: var(--height-s);
-    width: fit-content;
-    max-width: 100%;
-
-    &:hover {
-      color: var(--color-fg-primary-strong);
-    }
-    &:focus {
-      border-radius: var(--border-radius-xs);
-      outline: var(--border-width-m) var(--border-style-default) var(--border-color-secondary-medium);
-    }
-  `;
   return (
     <QuickNavContainer>
       <DxcHeading level={4} text={title || translatedLabels.quickNav.contentTitle} />
