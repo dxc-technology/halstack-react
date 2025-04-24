1764f72f1d0a7ba0edc9c3856a2cdb748cff7a82
Jialecl <jialestrabajos@gmail.com>
2025-04-24 10:33:42 +0200

    Another try to fix the chromatic error

--- packages/lib/src/file-input/FileInput.stories.tsx
+++ packages/lib/src/file-input/FileInput.stories.tsx
@@ -600,7 +600,6 @@ export const Chromatic: Story = {
   render: FileInput,
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
-    const longError = canvas.getAllByText("This error message is a multiline paragraph for testing.")[0];
-    longError && (await userEvent.hover(longError));
+    await userEvent.hover(canvas.getByText("This error message is a multiline paragraph for testing."));
   },
 };
