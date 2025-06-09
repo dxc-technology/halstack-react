a50b90f2e983eb5f3700044e6ece772cb7cde644
Enrique Moreno <enrique.moreno@dxc.com>
2025-06-09 16:39:32 +0200

    Reimplemented typography and documentation using tokens

--- packages/lib/src/typography/types.ts
+++ packages/lib/src/typography/types.ts
@@ -23,12 +23,12 @@ export type Props = {
   children: ReactNode;
   color?: string;
   display?: "inline" | "block";
-  fontFamily?: "Open Sans, sans-serif" | "Source Code Pro, monospace";
-  fontSize?: "0.75rem" | "0.875rem" | "1rem" | "1.25rem" | "1.5rem" | "2rem" | "3rem" | "3.75rem";
+  fontFamily?: string;
+  fontSize?: string;
   fontStyle?: "italic" | "normal";
-  fontWeight?: "300" | "400" | "600" | "700";
-  letterSpacing?: "-0.025em" | "-0.0125em" | "0em" | "0.025em" | "0.05em" | "0.1em";
-  lineHeight?: "1em" | "1.25em" | "1.365em" | "1.5em" | "1.715em" | "2em";
+  fontWeight?: string;
+  letterSpacing?: string;
+  lineHeight?: string;
   textAlign?: "left" | "center" | "right";
   textDecoration?: "none" | "underline" | "line-through";
   textOverflow?: "clip" | "ellipsis" | "unset";
