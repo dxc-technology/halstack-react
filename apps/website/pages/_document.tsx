9d31d6e13b4cb394931c7336c9bd5c63e1af9401
Enrique Moreno Carmona <33158831+Mil4n0r@users.noreply.github.com>
2025-07-04 14:05:22 +0200

    Revert "Added fix for FOUC problem"

--- apps/website/pages/_document.tsx
+++ apps/website/pages/_document.tsx
@@ -1,9 +1,38 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 import createEmotionServer from "@emotion/server/create-instance";
-import createEmotionCache from "./createEmotionCache";
-import * as React from "react";
+import React from "react";
+import createCache from "@emotion/cache";
 
 export default class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const originalRenderPage = ctx.renderPage;
+
+    const cache = createCache({ key: "css", prepend: true });
+    const { extractCriticalToChunks } = createEmotionServer(cache);
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App: any) =>
+          function EnhanceApp(props: any) {
+            return <App emotionCache={cache} {...props} />;
+          },
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+    const emotionStyles = extractCriticalToChunks(initialProps.html);
+    const emotionStyleTags = emotionStyles.styles.map((style) => (
+      <style
+        data-emotion={`${style.key} ${style.ids.join(" ")}`}
+        key={style.key}
+        dangerouslySetInnerHTML={{ __html: style.css }}
+      />
+    ));
+
+    return {
+      ...initialProps,
+      styles: [...React.Children.toArray(initialProps.styles), ...emotionStyleTags],
+    };
+  }
   render() {
     return (
       <Html lang="en">
@@ -16,32 +45,3 @@ export default class MyDocument extends Document {
     );
   }
 }
-
-MyDocument.getInitialProps = async (ctx) => {
-  const originalRenderPage = ctx.renderPage;
-
-  const cache = createEmotionCache();
-  const { extractCriticalToChunks, constructStyleTagsFromChunks } = createEmotionServer(cache);
-
-  ctx.renderPage = () =>
-    originalRenderPage({
-      enhanceApp: (App: any) =>
-        function EnhanceApp(props) {
-          return <App emotionCache={cache} {...props} />;
-        },
-    });
-
-  const initialProps = await Document.getInitialProps(ctx);
-  const emotionChunks = extractCriticalToChunks(initialProps.html);
-  const emotionStyleTags = constructStyleTagsFromChunks(emotionChunks);
-
-  return {
-    ...initialProps,
-    styles: (
-      <>
-        {initialProps.styles}
-        <style data-emotion={`css`} dangerouslySetInnerHTML={{ __html: emotionStyleTags }} />
-      </>
-    ),
-  };
-};
