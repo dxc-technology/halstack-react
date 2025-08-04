3308ca9bca57a3a33d77b7ce4d5aa74a6eb5443a
Enrique Moreno <morenocarmonaenrique@gmail.com>
2025-08-04 14:52:47 +0200

    Changed env variable settings for vitest

--- packages/lib/vitest.config.ts
+++ packages/lib/vitest.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
         plugins: [
           // The plugin will run tests for the stories defined in your Storybook config
           // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-          storybookTest({ configDir: path.join(dirname, ".storybook") }),
+          storybookTest({ configDir: path.join(dirname, ".storybook"), storybookUrl: process.env.SB_URL }),
         ],
         test: {
           name: "storybook",
