0ca540de0568d58e782360650245c12e121869bc
Mil4n0r <morenocarmonaenrique@gmail.com>
2025-10-01 08:10:58 +0200

    Added missing styles to datagrid

--- packages/lib/src/data-grid/DataGrid.tsx
+++ packages/lib/src/data-grid/DataGrid.tsx
@@ -2,6 +2,7 @@ import { useEffect, useMemo, useState } from "react";
 import DataGrid, { SortColumn } from "react-data-grid";
 import styled from "@emotion/styled";
 import DataGridPropsType, { HierarchyGridRow, GridRow, ExpandableGridRow } from "./types";
+import "react-data-grid/lib/styles.css";
 import {
   convertToRDGColumns,
   rowKeyGetter,
