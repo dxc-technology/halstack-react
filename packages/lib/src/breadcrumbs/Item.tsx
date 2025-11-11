8788088f4cec833b48de24eac9f3964784e39aaa
PelayoFelgueroso <pfelguerosogalguera@gmail.com>
2025-11-11 13:08:24 +0100

    Fi based on comments

--- packages/lib/src/breadcrumbs/Item.tsx
+++ packages/lib/src/breadcrumbs/Item.tsx
@@ -48,10 +48,9 @@ const Item = ({ isCurrentPage = false, href, label, onClick }: ItemPropsType) =>
   const currentItemRef = useRef<HTMLSpanElement | null>(null);
 
   const handleOnClick = (event: MouseEvent<HTMLAnchorElement>) => {
-    if (onClick) {
-      event.preventDefault();
-      if (href) onClick?.(href);
-    }
+    if (!onClick) return;
+    event.preventDefault();
+    if (href) onClick(href);
   };
 
   const handleOnMouseEnter = (event: MouseEvent<HTMLAnchorElement>) => {
