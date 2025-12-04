4f19b8d9a2dff23a1429b98188c2120ab4e503d4
PelayoFelgueroso <pfelguerosogalguera@gmail.com>
2025-12-04 12:45:23 +0100

    Fix inheritColor typo

--- apps/website/screens/components/spinner/code/SpinnerCodePage.tsx
+++ apps/website/screens/components/spinner/code/SpinnerCodePage.tsx
@@ -39,7 +39,7 @@ const sections = [
             <td>
               <DxcFlex direction="column" gap="var(--spacing-gap-xs)" alignItems="baseline">
                 <StatusBadge status="new" />
-                inheritColot
+                inheritColor
               </DxcFlex>
             </td>
             <td>
