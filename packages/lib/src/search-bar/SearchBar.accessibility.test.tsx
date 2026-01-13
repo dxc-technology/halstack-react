e737d7f86d8348c8b81ab46cf8a97c276d7b53c7
Pelayo Felgueroso <pfelguerosogalguera@gmail.com>
2026-01-13 12:09:16 +0100

    Resolve comments

--- packages/lib/src/search-bar/SearchBar.accessibility.test.tsx
+++ packages/lib/src/search-bar/SearchBar.accessibility.test.tsx
@@ -1,6 +1,5 @@
 import { render } from "@testing-library/react";
 import DxcSearchBar from "./SearchBar";
-import DxcSearchBarTrigger from "./SearchBarTrigger";
 import { axe } from "../../test/accessibility/axe-helper";
 
 describe("SearchBar component accessibility tests", () => {
@@ -38,20 +37,6 @@ describe("SearchBar component accessibility tests", () => {
         onCancel={() => {}}
       />
     );
-    const results = await axe(container);
-    expect(results.violations).toHaveLength(0);
-  });
-});
-
-describe("SearchBarTrigger component accessibility tests", () => {
-  it("Should not have basic accessibility issues", async () => {
-    const { container } = render(<DxcSearchBarTrigger />);
-    const results = await axe(container);
-    expect(results.violations).toHaveLength(0);
-  });
-
-  it("Should not have basic accessibility issues with onTriggerClick", async () => {
-    const { container } = render(<DxcSearchBarTrigger onTriggerClick={() => {}} />);
     const results = await axe(container);
     expect(results.violations).toHaveLength(0);
   });
