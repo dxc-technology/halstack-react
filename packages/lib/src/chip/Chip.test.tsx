28f72b87cac88690748261f015f261284660d1b0
Pelayo Felgueroso <pfelguerosogalguera@gmail.com>
2026-01-15 16:13:22 +0100

    Add tests

--- packages/lib/src/chip/Chip.test.tsx
+++ packages/lib/src/chip/Chip.test.tsx
@@ -6,16 +6,24 @@ describe("Chip component tests", () => {
     const { getByText } = render(<DxcChip label="Chip" />);
     expect(getByText("Chip")).toBeTruthy();
   });
-  test("Calls correct function when clicking on prefix icon", () => {
-    const onClick = jest.fn();
-    const { getByText, getByRole } = render(<DxcChip label="Chip" prefixIcon="nutrition" onClickPrefix={onClick} />);
-    expect(getByText("Chip")).toBeTruthy();
-    fireEvent.click(getByRole("button"));
-    expect(onClick).toHaveBeenCalled();
+  test("Chip renders correctly with prefix icon", () => {
+    const { getByRole } = render(<DxcChip label="Chip" prefix="nutrition" />);
+    const avatar = getByRole("img", { hidden: true });
+    expect(avatar).toBeTruthy();
   });
-  test("Calls correct function when clicking on suffix icon", () => {
+  test("Chip renders correctly with avatar", () => {
+    const { getByRole } = render(<DxcChip label="Chip" prefix={{ color: "primary" }} />);
+    const avatar = getByRole("img", { hidden: true });
+    expect(avatar).toBeTruthy();
+  });
+  test("Chip doesn't render avatar when size is small", () => {
+    const { queryByRole } = render(<DxcChip label="Chip" prefix={{ color: "primary" }} size="small" />);
+    const avatar = queryByRole("img", { hidden: true });
+    expect(avatar).not.toBeTruthy();
+  });
+  test("Calls correct function when clicking on action icon", () => {
     const onClick = jest.fn();
-    const { getByText, getByRole } = render(<DxcChip label="Chip" suffixIcon="nutrition" onClickSuffix={onClick} />);
+    const { getByText, getByRole } = render(<DxcChip label="Chip" action={{ icon: "nutrition", onClick: onClick }} />);
     expect(getByText("Chip")).toBeTruthy();
     fireEvent.click(getByRole("button"));
     expect(onClick).toHaveBeenCalled();
