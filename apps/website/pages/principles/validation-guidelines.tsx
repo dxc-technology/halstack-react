d3bd83363ca4cca6459fafe258ab9ad355252bc0
PelayoFelgueroso <pfelguerosogalguera@gmail.com>
2026-02-10 10:45:57 +0100

    Add Phone Number Validation Guidelines page

---
+++ apps/website/pages/principles/validation-guidelines.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import ValidationGuidelinesPage from "screens/principles/validation-guidelines/ValidationGuidelinesPage";
+
+const ValidationGuidelines = () => (
+  <>
+    <Head>
+      <title>Validation Guidelines — Halstack Design System</title>
+    </Head>
+    <ValidationGuidelinesPage />
+  </>
+);
+
+export default ValidationGuidelines;
