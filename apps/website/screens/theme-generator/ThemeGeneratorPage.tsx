87bc14afe2712ba85c95f7f43418b9511cfbc58d
Jialecl <jialestrabajos@gmail.com>
2026-02-25 14:03:18 +0100

    Matching design values

--- apps/website/screens/theme-generator/ThemeGeneratorPage.tsx
+++ apps/website/screens/theme-generator/ThemeGeneratorPage.tsx
@@ -5,24 +5,26 @@ const ThemeGeneratorPage = () => {
   return (
     <DxcContainer
       width="100%"
-      padding="var(--spacing-padding-xxl)"
+      padding="56px"
       boxSizing="border-box"
       background={{
-        image: "url(/theme-generator-landing-bg.jpg)",
-        position: "0px 500px",
+        image: "url(/theme-generator-landing-bg.png)",
+        position: "0px 50%",
+        size: "cover",
       }}
-      minHeight="328px"
       height="fit-content"
       borderRadius="0% 0% 640px 0%"
     >
-      <DxcFlex direction="column" fullHeight justifyContent="center" gap="var(--spacing-gap-m)">
-        <DxcHeading text="Welcome to Halstack Theme Generators" />
-        <DxcContainer maxWidth="60%">
-          <DxcParagraph>
-            Create and explore your brand within Halstack. Configure your core colors, upload your logo variants, and
-            see in real time how your theme works across components, layouts, and real product scenarios.
-          </DxcParagraph>
-        </DxcContainer>
+      <DxcFlex direction="column" justifyContent="center" gap="var(--spacing-gap-ml)">
+        <DxcFlex direction="column" justifyContent="center" gap="var(--spacing-gap-m)">
+          <DxcHeading text="Welcome to Halstack Theme Generators" />
+          <DxcContainer maxWidth="60%">
+            <DxcParagraph>
+              Create and explore your brand within Halstack. Configure your core colors, upload your logo variants, and
+              see in real time how your theme works across components, layouts, and real product scenarios.
+            </DxcParagraph>
+          </DxcContainer>
+        </DxcFlex>
         <Link href="" passHref legacyBehavior>
           <DxcLink icon="arrow_forward" iconPosition="after">
             Start your theme
