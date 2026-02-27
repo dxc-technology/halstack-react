587a6316e656a939910a6027bdb006fda9fd543e
Jialecl <jialestrabajos@gmail.com>
2026-02-27 09:29:47 +0100

    Changes based on feedback
    - Minor style changes to match the design more closely, such as
    typography size and divider color.
    - Added React.Fragment with key to fix the warning about missing keys.
    - Changing LandingCard to LandingCards to avoid repeating each card.

--- apps/website/screens/theme-generator/components/LandingSteps.tsx
+++ apps/website/screens/theme-generator/components/LandingSteps.tsx
@@ -1,23 +1,14 @@
-import {
-  DxcContainer,
-  DxcFlex,
-  DxcHeading,
-  DxcTypography,
-  DxcAvatar,
-  DxcDivider,
-  DxcInset,
-} from "@dxc-technology/halstack-react";
+import { DxcContainer, DxcFlex, DxcTypography, DxcAvatar, DxcDivider, DxcInset } from "@dxc-technology/halstack-react";
+import React from "react";
 
 const LandingStep = ({ index, title, description }: { index: number; title: string; description: string }) => {
   return (
     <DxcContainer width="224px">
       <DxcFlex direction="column" alignItems="center" gap="var(--spacing-gap-l)">
-        <DxcFlex>
-          <DxcAvatar label={`${index}`} color="primary" size="large" />
-        </DxcFlex>
+        <DxcAvatar label={`${index}`} color="primary" size="large" />
         <DxcFlex direction="column" alignItems="center" gap="var(--spacing-gap-xs)">
-          <DxcHeading text={title} level={5} />
-          <DxcTypography textAlign="center" fontSize="var(--typography-body-s)">
+          <DxcTypography fontWeight="var(--typography-title-bold)">{title}</DxcTypography>
+          <DxcTypography textAlign="center" fontSize="var(--typography-body-m)">
             {description}
           </DxcTypography>
         </DxcFlex>
@@ -31,16 +22,16 @@ const LandingSteps = ({ steps }: { steps: { title: string; description: string }
     <DxcInset bottom="var(--spacing-gap-xxl)">
       <DxcFlex gap="var(--spacing-gap-l)" wrap="wrap" justifyContent="center">
         {steps.map((step, index) => (
-          <>
-            <LandingStep key={index} index={index + 1} title={step.title} description={step.description} />
+          <React.Fragment key={index}>
+            <LandingStep index={index + 1} title={step.title} description={step.description} />
             {index !== steps.length - 1 && (
               <DxcContainer width="48px">
                 <DxcFlex fullHeight alignItems="center" justifyContent="center">
-                  <DxcDivider />
+                  <DxcDivider color="lightGrey" />
                 </DxcFlex>
               </DxcContainer>
             )}
-          </>
+          </React.Fragment>
         ))}
       </DxcFlex>
     </DxcInset>
