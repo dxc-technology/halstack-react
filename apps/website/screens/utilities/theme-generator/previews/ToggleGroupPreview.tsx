c282499220b46b571da0ac3c2c1a645dc8c31ec3
Mil4n0r <morenocarmonaenrique@gmail.com>
2026-02-27 14:49:39 +0100

    Improved layout and added missing examples

--- apps/website/screens/utilities/theme-generator/previews/ToggleGroupPreview.tsx
+++ apps/website/screens/utilities/theme-generator/previews/ToggleGroupPreview.tsx
@@ -4,16 +4,16 @@ const ToggleGroupPreview = () => {
   const optionsWithIcons = [
     {
       value: 1,
-      label: "Facebook",
+      label: "Toggle option 1",
       icon: "filled_thumb_up",
     },
     {
       value: 2,
-      label: "Linkedin",
-      icon: "filled_work",
+      label: "Toggle option 2",
+      icon: "filled_thumb_down",
     },
   ];
-  return <DxcToggleGroup options={optionsWithIcons} />;
+  return <DxcToggleGroup options={optionsWithIcons} defaultValue={1} />;
 };
 
 export default ToggleGroupPreview;
