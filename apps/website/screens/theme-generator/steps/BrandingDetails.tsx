0b3eda4d9278e10936132eebc02bc2a75e3db2d5
PelayoFelgueroso <pfelguerosogalguera@gmail.com>
2026-03-06 09:04:56 +0100

    Change folder to branding again

--- apps/website/screens/theme-generator/steps/BrandingDetails.tsx
+++ apps/website/screens/theme-generator/steps/BrandingDetails.tsx
@@ -1,9 +1,9 @@
 import { DxcContainer, DxcFlex } from "@dxc-technology/halstack-react";
 import { FileData } from "../../../../../packages/lib/src/file-input/types";
 import { Colors, Logos } from "../types";
-import BrandingLogoGrid from "../components/BrandingDetails/BrandingLogoGrid";
+import BrandingLogoGrid from "../components/branding/BrandingLogoGrid";
 import { brandingIcon, infoIcon, paletteIcon } from "./icons";
-import BrandingColorGrid from "../components/BrandingDetails/BrandingColorGrid";
+import BrandingColorGrid from "../components/branding/BrandingColorGrid";
 
 const coreColors = {
   id: "core",
