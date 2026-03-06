fdc638fa73521b9876282264b355ceb01a1af472
Mil4n0r <morenocarmonaenrique@gmail.com>
2026-03-06 12:36:02 +0100

    Added partially applied theme to preview and inline styles prop

--- apps/website/screens/theme-generator/ThemeGeneratorPreviewPage.tsx
+++ apps/website/screens/theme-generator/ThemeGeneratorPreviewPage.tsx
@@ -1,9 +1,221 @@
+// import {
+//   DxcButton,
+//   DxcContainer,
+//   DxcFlex,
+//   DxcSelect,
+//   DxcToggleGroup,
+//   HalstackProvider,
+// } from "@dxc-technology/halstack-react";
+// import { useMemo, useState } from "react";
+// import componentsList from "../common/componentsList.json";
+// import { componentsRegistry, examplesRegistry } from "screens/utilities/theme-generator/componentsRegistry";
+// import { ListOptionType } from "../../../../packages/lib/src/select/types";
+// import styled from "@emotion/styled";
+
+// // JSON Structure type, this should go in the json file or somewhere else
+
+// type ComponentItem = {
+//   label: string;
+//   icon?: string;
+//   path?: string;
+//   status?: string;
+//   links?: ComponentItem[];
+// };
+
+// /////////////////////////////////////
+
+// const exampleOptions = [
+//   {
+//     label: "Application example",
+//     value: "/examples/application",
+//     icon: "settings",
+//   },
+//   {
+//     label: "Dashboard example",
+//     value: "/examples/dashboard",
+//     icon: "dashboard",
+//   },
+//   {
+//     label: "Form example",
+//     value: "/examples/form",
+//     icon: "description",
+//   },
+//   {
+//     label: "Login example",
+//     value: "/examples/login",
+//     icon: "login",
+//   },
+// ];
+
+// const componentsExceptions = [
+//   "/components/application-layout",
+//   "/components/bleed",
+//   "/components/bulleted-list",
+//   "/components/card",
+//   "/components/container",
+//   "/components/dialog",
+//   "/components/flex",
+//   "/components/footer",
+//   "/components/grid",
+//   "/components/header",
+//   "/components/heading",
+//   "/components/image",
+//   "/components/inset",
+//   "/components/paragraph",
+//   "/components/popover",
+//   "/components/sidenav",
+//   "/components/typography",
+// ];
+
+// const mapToSelectGroups = (data: ComponentItem[]) => {
+//   const collectOptions = (items: ComponentItem[]): ListOptionType[] => {
+//     return items.flatMap((item) => {
+//       const current: ListOptionType[] =
+//         item.path && !componentsExceptions.includes(item.path)
+//           ? [
+//               {
+//                 label: item.label,
+//                 value: item.path,
+//                 icon: item.icon,
+//               },
+//             ]
+//           : [];
+
+//       const nested = item.links ? collectOptions(item.links) : [];
+
+//       return [...current, ...nested];
+//     });
+//   };
+
+//   return data.map((category) => ({
+//     label: category.label,
+//     options: collectOptions(category.links ?? []),
+//   }));
+// };
+
+// const ThemeGeneratorPreviewPage = ({ tokens }: { tokens: Record<string, string> }) => {
+//   const [mode, setMode] = useState<"components" | "examples">("components");
+
+//   const [selectedComponents, setSelectedComponents] = useState<string[]>([]);
+//   const [selectedExample, setSelectedExample] = useState<string>("");
+
+//   const componentOptions = useMemo(() => {
+//     return mapToSelectGroups(componentsList as ComponentItem[]);
+//   }, []);
+
+//   const displayedPreview = useMemo(() => {
+//     if (mode === "components") {
+//       return selectedComponents.map((component) => {
+//         const ComponentPreview = componentsRegistry[component as keyof typeof componentsRegistry];
+//         return ComponentPreview ? <ComponentPreview key={component} /> : null;
+//       });
+//     }
+
+//     if (mode === "examples") {
+//       const ExamplePreview = examplesRegistry[selectedExample as keyof typeof examplesRegistry];
+//       return ExamplePreview ? <ExamplePreview key={selectedExample} /> : null;
+//     }
+
+//     return null;
+//   }, [mode, selectedComponents, selectedExample]);
+
+//   return (
+//     <DxcContainer width="100%" height="100%">
+//       <DxcFlex direction="column" gap="var(--spacing-gap-s)" fullHeight>
+//         <DxcFlex direction="row" justifyContent="space-between" alignItems="center">
+//           <DxcToggleGroup
+//             options={[
+//               { label: "Components", icon: "category", value: 1 },
+//               { label: "Layout examples", icon: "dashboard", value: 2 },
+//             ]}
+//             value={mode === "components" ? 1 : 2}
+//             onChange={(value: number) => setMode(value === 1 ? "components" : "examples")}
+//           />
+
+//           {mode === "components" && (
+//             <DxcSelect
+//               placeholder="Select components"
+//               options={componentOptions}
+//               multiple
+//               value={selectedComponents}
+//               onChange={({ value }) => {
+//                 setSelectedComponents(value);
+//               }}
+//               enableSelectAll
+//             />
+//           )}
+
+//           {mode === "examples" && (
+//             <DxcSelect
+//               placeholder="Select examples"
+//               options={exampleOptions}
+//               value={selectedExample}
+//               onChange={({ value }) => {
+//                 setSelectedExample(value);
+//               }}
+//             />
+//           )}
+//         </DxcFlex>
+//         {/* TODO: Turn this into a separate componente called PreviewArea or similar? */}
+//         <HalstackProvider opinionatedTheme={tokens}>
+//           <DxcContainer
+//             borderRadius="var(--border-radius-l)"
+//             border={{
+//               width: "var(--border-width-s)",
+//               color: "var(--border-color-neutral-medium)",
+//               style: "var(--border-style-default)",
+//             }}
+//             background={{ color: "var(--color-bg-neutral-lightest)" }}
+//             padding="var(--spacing-padding-s)"
+//             height="100%"
+//           >
+//             <DxcFlex direction="column" gap="var(--spacing-gap-l)" fullHeight>
+//               <DxcFlex justifyContent="flex-end">
+//                 <DxcButton
+//                   icon="filled_delete"
+//                   size={{ height: "medium" }}
+//                   title="Delete selection"
+//                   onClick={() => {
+//                     if (mode === "components") {
+//                       setSelectedComponents([]);
+//                     } else {
+//                       setSelectedExample("");
+//                     }
+//                   }}
+//                   mode="secondary"
+//                   semantic="error"
+//                   disabled={mode === "components" ? selectedComponents.length === 0 : !selectedExample}
+//                 />
+//               </DxcFlex>
+//               <CustomPreviewArea>{displayedPreview}</CustomPreviewArea>
+//             </DxcFlex>
+//           </DxcContainer>
+//         </HalstackProvider>
+//       </DxcFlex>
+//     </DxcContainer>
+//   );
+// };
+
+// // TODO: this is just a quick solution to make the preview area scrollable when the content is too big, I don't know what other approach that doesn't
+// // involve adding custom styles could be used. (fullHeight does not do anything here either)
+// const CustomPreviewArea = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: var(--spacing-gap-l);
+//   flex: 1 1 0;
+//   overflow: auto;
+//   align-items: flex-start;
+// `;
+
+// export default ThemeGeneratorPreviewPage;
+
 import {
   DxcButton,
   DxcContainer,
   DxcFlex,
   DxcSelect,
   DxcToggleGroup,
+  DxcTypography,
   HalstackProvider,
 } from "@dxc-technology/halstack-react";
 import { useMemo, useState } from "react";
@@ -157,40 +369,53 @@ const ThemeGeneratorPreviewPage = ({ tokens }: { tokens: Record<string, string> 
           )}
         </DxcFlex>
         {/* TODO: Turn this into a separate componente called PreviewArea or similar? */}
-        <HalstackProvider opinionatedTheme={tokens}>
-          <DxcContainer
-            borderRadius="var(--border-radius-l)"
-            border={{
-              width: "var(--border-width-s)",
-              color: "var(--border-color-neutral-medium)",
-              style: "var(--border-style-default)",
-            }}
-            background={{ color: "var(--color-bg-neutral-lightest)" }}
-            padding="var(--spacing-padding-s)"
-            height="100%"
-          >
-            <DxcFlex direction="column" gap="var(--spacing-gap-l)" fullHeight>
-              <DxcFlex justifyContent="flex-end">
-                <DxcButton
-                  icon="filled_delete"
-                  size={{ height: "medium" }}
-                  title="Delete selection"
-                  onClick={() => {
-                    if (mode === "components") {
-                      setSelectedComponents([]);
-                    } else {
-                      setSelectedExample("");
-                    }
-                  }}
-                  mode="secondary"
-                  semantic="error"
-                  disabled={mode === "components" ? selectedComponents.length === 0 : !selectedExample}
-                />
+        <DxcContainer
+          borderRadius="var(--border-radius-l)"
+          border={{
+            width: "var(--border-width-s)",
+            color: "var(--border-color-neutral-medium)",
+            style: "var(--border-style-default)",
+          }}
+          background={{ color: "var(--color-bg-neutral-lightest)" }}
+          padding="var(--spacing-padding-s)"
+          height="100%"
+        >
+          {(mode === "components" && selectedComponents.length > 0) || (mode === "examples" && !!selectedExample) ? (
+            <HalstackProvider opinionatedTheme={tokens} style={{ height: "100%" }}>
+              <DxcFlex direction="column" gap="var(--spacing-gap-l)" fullHeight>
+                <DxcFlex justifyContent="flex-end">
+                  <DxcButton
+                    icon="filled_delete"
+                    size={{ height: "medium" }}
+                    title="Delete selection"
+                    onClick={() => {
+                      if (mode === "components") {
+                        setSelectedComponents([]);
+                      } else {
+                        setSelectedExample("");
+                      }
+                    }}
+                    mode="secondary"
+                    semantic="error"
+                    disabled={mode === "components" ? selectedComponents.length === 0 : !selectedExample}
+                  />
+                </DxcFlex>
+                <CustomPreviewArea>{displayedPreview}</CustomPreviewArea>
               </DxcFlex>
-              <CustomPreviewArea>{displayedPreview}</CustomPreviewArea>
+            </HalstackProvider>
+          ) : (
+            <DxcFlex alignItems="center" justifyContent="center" fullHeight>
+              <DxcTypography
+                color="var(--color-fg-neutral-dark)"
+                fontFamily="var(--typography-font-family)"
+                fontSize="var(--typography-body-s)"
+                fontWeight="var(--typography-body-regular)"
+              >
+                Select {mode === "components" ? "a component" : "an example"} to preview
+              </DxcTypography>
             </DxcFlex>
-          </DxcContainer>
-        </HalstackProvider>
+          )}
+        </DxcContainer>
       </DxcFlex>
     </DxcContainer>
   );
