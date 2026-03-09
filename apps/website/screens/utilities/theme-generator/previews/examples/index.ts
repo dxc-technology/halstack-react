21414511792c9640ea69739535d6d02cf5bffc23
Raquel Arrojo López <10975289+raquelarrojo@users.noreply.github.com>
2026-03-09 16:55:12 +0100

    Merge pull request #2417 from dxc-technology/Mil4n0r/theme-generator-preview
    [doc] Theme generator Preview (Step 2)

--- apps/website/screens/utilities/theme-generator/previews/examples/index.ts
+++ apps/website/screens/utilities/theme-generator/previews/examples/index.ts
@@ -1,4 +1,3 @@
 export { default as ApplicationPreview } from "./ApplicationPreview";
 export { default as DashboardPreview } from "./DashboardPreview";
 export { default as FormPreview } from "./FormPreview";
-export { default as LoginPreview } from "./LoginPreview";
