dbbef903c9b12e87c11177db31aa858bdb78e074
PelayoFelgueroso <pfelguerosogalguera@gmail.com>
2026-03-17 17:28:54 +0100

    Move /screens/utilities/theme-generator/previews to screens/theme-generator/previews

--- apps/website/screens/utilities/theme-generator/previews/components/CardPreview.tsx
+++
@@ -1,7 +0,0 @@
-import { DxcCard } from "@dxc-technology/halstack-react";
-
-const CardPreview = () => {
-  return <DxcCard>Card</DxcCard>;
-};
-
-export default CardPreview;
--- apps/website/screens/utilities/theme-generator/previews/components/TypographyPreview.tsx
+++
@@ -1,7 +0,0 @@
-import { DxcTypography } from "@dxc-technology/halstack-react";
-
-const TypographyPreview = () => {
-  return <DxcTypography>Typography</DxcTypography>;
-};
-
-export default TypographyPreview;
