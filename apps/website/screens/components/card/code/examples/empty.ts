cfd8a4e57c5de64facc9d59556083833c5f69d81
Jialecl <jialestrabajos@gmail.com>
2026-05-22 13:00:28 +0200

    removed unnecessary content from examples

--- apps/website/screens/components/card/code/examples/empty.ts
+++ apps/website/screens/components/card/code/examples/empty.ts
@@ -12,13 +12,6 @@ const code = `() => {
   return (
     <DxcInset space="var(--spacing-padding-xl)">
       <DxcCard image={image} isEmpty emptySize={{ width: "460px", height: "350px" }}>
-        <DxcContainer maxWidth="445px">
-          <DxcParagraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at pretium mi. Sed ac mi purus. Donec
-            mattis luctus nisi, vitae scelerisque metus. Praesent in justo vitae quam mollis sollicitudin. Cras at consequat
-            libero.
-          </DxcParagraph>
-        </DxcContainer>
       </DxcCard>
     </DxcInset>
   );
