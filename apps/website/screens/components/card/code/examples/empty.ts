af21b52214aba5290a77bb0eb79deac5ec158dd9
Jialecl <jialestrabajos@gmail.com>
2026-05-22 12:42:21 +0200

    2 additional examples added to card doc page

---
+++ apps/website/screens/components/card/code/examples/empty.ts
@@ -0,0 +1,34 @@
+import { DxcInset, DxcCard, DxcParagraph, DxcContainer } from "@dxc-technology/halstack-react";
+
+const code = `() => {
+  const image = {
+    alt: "Example image",
+    width: "445px",
+    height: "100%",
+    objectFit: "cover",
+    src: "https://picsum.photos/id/11/1920/1080",
+  };
+
+  return (
+    <DxcInset space="var(--spacing-padding-xl)">
+      <DxcCard image={image} isEmpty emptySize={{ width: "460px", height: "350px" }}>
+        <DxcContainer maxWidth="445px">
+          <DxcParagraph>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at pretium mi. Sed ac mi purus. Donec
+            mattis luctus nisi, vitae scelerisque metus. Praesent in justo vitae quam mollis sollicitudin. Cras at consequat
+            libero.
+          </DxcParagraph>
+        </DxcContainer>
+      </DxcCard>
+    </DxcInset>
+  );
+}`;
+
+const scope = {
+  DxcCard,
+  DxcInset,
+  DxcContainer,
+  DxcParagraph,
+};
+
+export default { code, scope };
