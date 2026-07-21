2c85af6b3a1dcc7410d23052143f86db8e54d07f
PelayoFelgueroso <pfelguerosogalguera@gmail.com>
2026-07-21 10:34:13 +0200

    Remove unused functions in utils

--- packages/lib/src/message-input/utils.ts
+++ packages/lib/src/message-input/utils.ts
@@ -1,63 +1,12 @@
 import MessageInputPropsType from "./types";
 import { css } from "@emotion/react";
 
-export const makeCancelable = (promise: Promise<string[]>) => {
-  let hasCanceled_ = false;
-  const wrappedPromise = new Promise<string[]>((resolve, reject) => {
-    promise.then(
-      (val) => {
-        if (hasCanceled_) {
-          reject(new Error("Is canceled"));
-        } else {
-          resolve(val);
-        }
-      },
-      (promiseError) => {
-        if (hasCanceled_) {
-          reject(new Error("Is canceled"));
-        } else if (promiseError instanceof Error) {
-          reject(promiseError);
-        } else {
-          reject(new Error(String(promiseError)));
-        }
-      }
-    );
-  });
-  return {
-    promise: wrappedPromise,
-    cancel() {
-      hasCanceled_ = true;
-    },
-  };
-};
-
 export const isLengthIncorrect = (
   value: string,
   minLength: MessageInputPropsType["minLength"],
   maxLength: MessageInputPropsType["maxLength"]
 ) =>
   value != null && ((minLength != null && value.length < minLength) || (maxLength != null && value.length > maxLength));
-
-export const isNumberIncorrect = (
-  value: number,
-  minNumber: MessageInputPropsType["minLength"],
-  maxNumber: MessageInputPropsType["maxLength"]
-) => (minNumber != null && value < minNumber) || (maxNumber != null && value > maxNumber);
-
-export const transformSpecialChars = (str: string) => {
-  const specialCharsRegex = /[\\*()[\]{}+?/]/;
-  let value = str;
-  if (specialCharsRegex.test(value)) {
-    const regexAsString = specialCharsRegex.toString().split("");
-    const uniqueSpecialChars = regexAsString.filter((item, index) => regexAsString.indexOf(item) === index);
-    uniqueSpecialChars.forEach((specialChar) => {
-      if (str.includes(specialChar)) {
-        value = value.replace(specialChar, `\\${specialChar}`);
-      }
-    });
-  }
-  return value;
-};
 
 export const inputStylesByStatePromptInput = (disabled: boolean, error: boolean, focus?: boolean) => css`
   background-color: ${disabled ? `var(--color-bg-neutral-lightest)` : `transparent`};
