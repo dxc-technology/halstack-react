0fc77c9bc9722c24d88d7212eee22accbe8984e1
jialecl <jialestrabajos@gmail.com>
2026-07-24 09:21:27 +0200

    Improved localization example

--- apps/website/screens/guidelines/localization/examples/localization.tsx
+++ apps/website/screens/guidelines/localization/examples/localization.tsx
@@ -1,21 +1,35 @@
-import { DxcDateInput, HalstackProvider, DxcInset } from "@dxc-technology/halstack-react";
+import { DxcDateInput, DxcTimeInput, HalstackProvider, DxcInset, DxcFlex } from "@dxc-technology/halstack-react";
 
 const code = `() => {
 
+const labels = {
+  formFields: {
+    optionalLabel: "(Freiwillig)",
+  },
+};
+
   return (
-    <HalstackProvider localeTag="de-DE">
+    <HalstackProvider localeTag="de-DE" labels={labels}>
       <DxcInset space="var(--spacing-padding-xl)">
-        <DxcDateInput
-          label="Date"
-          placeholder="Select a date"
-          optional
-        />
+        <DxcFlex direction="column" gap="2rem">
+          <DxcDateInput
+            label="Date"
+            placeholder="Select a date"
+            optional
+          />
+          <DxcTimeInput
+            label="Time"
+            optional
+          />
+        </DxcFlex>
       </DxcInset>
     </HalstackProvider>
   );
 }`;
 
 const scope = {
+  DxcFlex,
+  DxcTimeInput,
   DxcDateInput,
   HalstackProvider,
   DxcInset,
